8d09f4f35ed6ac9c4e1e02b3d36d88c9b72d1035
youvovk <wer3wollf@gmail.com>
2019-09-06 07:54:48 +0300

    unuse filed were deleted

---
+++ src/components/article/article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ArticleContent from '../article__content/article__content';
+
+function Article() {
+  return (
+    <article className="article">
+      <ArticleContent />
+    </article>
+  );
+}
+
+export default Article;
---
+++ src/components/navigation/navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a
+        className="navigation__link"
+        href="https://www.facebook.com/yuriy3vovk"
+      >
+        About
+      </a>
+      <a
+        className="navigation__link"
+        href="https://www.facebook.com/yuriy3vovk"
+      >
+        Services
+      </a>
+      <a
+        className="navigation__link"
+        href="https://www.facebook.com/yuriy3vovk"
+      >
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+export default Navigation;
