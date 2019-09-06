28e94fbccefa9a7865cb2d15b918c416169cdf7a
KseniiaMazan <ksusamazan@gmail.com>
2019-09-06 13:12:11 +0300

    solution

---
+++ src/components/Header/Navigation/Navigation.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../../generalCSS.css';
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a className="navigation__link" href="https://www.instagram.com/ksusha_mazan/">
+        About
+      </a>
+      <a className="navigation__link" href="https://www.instagram.com/ksusha_mazan/">
+        Services
+      </a>
+      <a className="navigation__link" href="https://www.instagram.com/ksusha_mazan/">
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+export default Navigation;
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../../generalCSS.css';
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
+
+export default Welcome;
