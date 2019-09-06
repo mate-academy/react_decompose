0a4632d9b585d4b9d45724202d0f62b1446257ce
KseniiaMazan <ksusamazan@gmail.com>
2019-09-06 13:32:32 +0300

    readme changes

---
+++ build/precache-manifest.641d43b52007701bb422768c1155ba6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad6f4e341ae6d2227e334cdefe0d35d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "76881e13c7cc4529b460",
+    "url": "./static/css/main.887c4bb8.chunk.css"
+  },
+  {
+    "revision": "bf0439faa0bd921c0f73",
+    "url": "./static/js/2.be3010fa.chunk.js"
+  },
+  {
+    "revision": "76881e13c7cc4529b460",
+    "url": "./static/js/main.73d971f1.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
