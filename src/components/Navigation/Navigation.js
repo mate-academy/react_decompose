3ab27b803c1b1a0947020ba958a0ed91c27faaae
KosBelozyorov <KosBelozyorov@gmail.com>
2019-09-06 17:13:38 +0300

    Solution

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Navigation.css';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#">About</a>
+    <a className="navigation__link" href="#">Services</a>
+    <a className="navigation__link" href="#">Contact</a>
+  </nav>
+);
+
+export default Navigation;
