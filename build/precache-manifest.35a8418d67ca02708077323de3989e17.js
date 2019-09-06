c9149a7c8c20ada7739e94a9afeb7629b6e22113
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-09-06 14:53:11 +0300

    Solution added

---
+++ build/precache-manifest.35a8418d67ca02708077323de3989e17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec196b1a6f8cbbba1d2e87b147d75339",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e16e93b1a87df3a75e8",
+    "url": "./static/css/main.6ae9d9e0.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "1e16e93b1a87df3a75e8",
+    "url": "./static/js/main.9c9b2cc9.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a className="navigation__link" href="https://www.google.com/">
+        About
+      </a>
+      <a className="navigation__link" href="https://www.google.com/">
+        Services
+      </a>
+      <a className="navigation__link" href="https://www.google.com/">
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+export default Navigation;
