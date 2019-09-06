f88f6bbc275123b0f6e422fe8993216218194018
ArnyMac <max@MacBook-Air-ArnyMac.local>
2019-09-06 17:57:49 +0300

    solution

---
+++ build/precache-manifest.d5793c4cc3397561e82a659c67366d3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "202ccb928773d93f66735b3510dc3c13",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b73ae3d82b86188ef949",
+    "url": "./static/css/main.18f7a239.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "b73ae3d82b86188ef949",
+    "url": "./static/js/main.3a8a0d3e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
