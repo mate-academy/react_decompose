0bac36c193c12f4e0eb43f6e315f4c5b1d8f78c0
igaryok <igor.sobko@gmail.com>
2019-09-06 20:28:04 +0300

    create project

---
+++ build/precache-manifest.2e829bfb816fe6c6888aacfe4119c27f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59275d57f8de89e9fb177ddc946fc680",
+    "url": "./index.html"
+  },
+  {
+    "revision": "31fd1076d62b56431003",
+    "url": "./static/css/main.d2de78a3.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "31fd1076d62b56431003",
+    "url": "./static/js/main.07297956.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
