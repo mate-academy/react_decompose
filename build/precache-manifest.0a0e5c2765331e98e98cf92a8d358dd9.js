85d05fb083c9a701f29ec34ae3511a56f3fd72a2
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-09-08 01:30:43 +0300

    solution

---
+++ build/precache-manifest.0a0e5c2765331e98e98cf92a8d358dd9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181f2ec3bdea0ea4cc1725d939ea04f3",
+    "url": "/react_decompose/index.html"
+  },
+  {
+    "revision": "af005b1395b3be8d1d71",
+    "url": "/react_decompose/static/css/main.29599215.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "/react_decompose/static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "af005b1395b3be8d1d71",
+    "url": "/react_decompose/static/js/main.ebf3b2ef.chunk.js"
+  },
+  {
+    "revision": "e966956942911a68aecd",
+    "url": "/react_decompose/static/js/runtime~main.f15862fb.js"
+  }
+]);
