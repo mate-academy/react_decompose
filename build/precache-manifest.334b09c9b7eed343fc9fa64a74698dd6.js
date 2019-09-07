7d3d3900bd21a470783bf2d1f6a00031395a63fc
yevhen-sahach <antitop04@gmail.com>
2019-09-07 12:50:49 +0300

    first commit

---
+++ build/precache-manifest.334b09c9b7eed343fc9fa64a74698dd6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90f5584d38967301a172a24514e8f5ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ee3745f0ca319bdb1dd6",
+    "url": "./static/css/main.3820d94b.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "ee3745f0ca319bdb1dd6",
+    "url": "./static/js/main.9405d7d5.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
