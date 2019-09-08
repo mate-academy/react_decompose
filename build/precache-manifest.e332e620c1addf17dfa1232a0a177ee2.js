4d052af19f4f4757ed28f0f169a6fa3db704045c
Alex Andrutskii <alex.andrutskii@gmail.com>
2019-09-09 02:43:12 +0300

    fix

---
+++ build/precache-manifest.e332e620c1addf17dfa1232a0a177ee2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48f28d5e08c86c248968b46864846721",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e9d986096db3b7e1eaa1",
+    "url": "./static/css/main.4289e892.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "e9d986096db3b7e1eaa1",
+    "url": "./static/js/main.f8add27c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
