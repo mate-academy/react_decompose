7ec86eb3f57ed870706e011373940fffb0cc8b44
vladyslav223 <vladyslav223@gmail.com>
2019-09-08 17:09:40 +0300

    first react

---
+++ src/components/StickyHeader/StickyHeader.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './StickyHeader.scss';
+
+
+
+
+const StickyHeader = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default StickyHeader;
