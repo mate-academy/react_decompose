ebf09ce6bd7550f1d111159fdd5b786c2fc3c554
Serhii-P <serhii.pyskun@gmail.com>
2019-09-09 20:15:22 +0300

    'Solution'

---
+++ build/precache-manifest.37c70293fe62e77cbe81dfa909c30791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03d87162915d49e90b40f3c65a74019",
+    "url": "./index.html"
+  },
+  {
+    "revision": "122bdba0cb71989347bb",
+    "url": "./static/css/main.5b25b7ba.chunk.css"
+  },
+  {
+    "revision": "b5c5beb43fab1ff7f479",
+    "url": "./static/js/2.ec295295.chunk.js"
+  },
+  {
+    "revision": "122bdba0cb71989347bb",
+    "url": "./static/js/main.d94bcbef.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Navigation.css';
+
+let navList = ['About', 'Services', 'Contact'];
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+{navList.map(
+  item => <a className="navigation__link" href="# ">{item}</a>
+)}
+ </nav>
+)};
+
+export default Navigation;
