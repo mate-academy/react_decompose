5a7bea8c599adce36d7429ae711390964f80fe57
Rafler <amuronyk@gmail.com>
2019-09-10 14:30:04 +0300

    fixing comments

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
 
-function App() {
-  return (
-    <>
-      <Welcome />
-      <Header />
-      <Article />
-      </>
-  );
-}
+const App = () => (
+  <>
+    <Welcome />
+    <Header />
+    <Article />
+  </>
+);
 
 export default App;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,16 @@
 import React from "react";
 import './Header.css'
 
-function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <nav className="navigation">
-        <a className="navigation__link" href="#">About</a>
-        <a className="navigation__link" href="#">Services</a>
-        <a className="navigation__link" href="#">Contact</a>
-      </nav >
-    </header >
-  );
-}
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <nav className="navigation">
+      <a className="navigation__link" href="#">About</a>
+      <a className="navigation__link" href="#">Services</a>
+      <a className="navigation__link" href="#">Contact</a>
+    </nav >
+  </header >
+);
+
 
 export default Header;
