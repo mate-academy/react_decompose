57606ea25a96c37abd13ec52bc91373357230436
Sijey <Popov040391@gmail.com>
2019-10-25 13:54:37 +0300

    Solution

---
+++ build/precache-manifest.dc23bfe6e30d0846f1f02dbb40bf20cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2f3539011be0029b298d77b2b11ed49",
+    "url": "/react_decompose/index.html"
+  },
+  {
+    "revision": "5917956a022f8ede820a",
+    "url": "/react_decompose/static/css/main.474d1e22.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "/react_decompose/static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "5917956a022f8ede820a",
+    "url": "/react_decompose/static/js/main.7557e88c.chunk.js"
+  },
+  {
+    "revision": "e966956942911a68aecd",
+    "url": "/react_decompose/static/js/runtime~main.f15862fb.js"
+  }
+]);
