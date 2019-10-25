e93dbbd250472f860b87a0239d4ca3c93e21f60c
Andrii <farikpite@gmail.com>
2019-10-25 19:24:35 +0300

    All separated

---
+++ src/components/js/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/Header.css';
+import '../css/Navigation.css';
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <h1 className="header__title">Site Name</h1>
+        <nav className="navigation">
+          <a className="navigation__link" href="#">About</a>
+          <a className="navigation__link" href="#">Services</a>
+          <a className="navigation__link" href="#">Contact</a>
+        </nav>
+      </header>
+    </>
+  );
+}
+
+export default Header;
