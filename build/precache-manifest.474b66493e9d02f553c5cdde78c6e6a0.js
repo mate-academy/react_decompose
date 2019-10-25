702c203772938bf3e2e64fa4c538e32a3093ec0e
Volodymyr_Roiuk <heppivova@gmail.com>
2019-10-25 13:14:52 +0300

    'Solution'

---
+++ build/precache-manifest.474b66493e9d02f553c5cdde78c6e6a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0c1797bb78a11aba4a9759844b8b3f5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "25cb64074caf980b3525",
+    "url": "./static/css/main.45fcd0f9.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "./static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "25cb64074caf980b3525",
+    "url": "./static/js/main.f06c6eb6.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
