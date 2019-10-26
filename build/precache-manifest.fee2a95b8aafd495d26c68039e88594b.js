9a39888329d88fa102fb72252aa9c18d5f6009f7
Rudenko Artem <artem.rud@bk.ru>
2019-10-26 17:23:58 +0300

    Solution

---
+++ build/precache-manifest.fee2a95b8aafd495d26c68039e88594b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160d6672e2618b53a6044cbd24df838d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32ae35da549f613ecfc5",
+    "url": "./static/css/main.dea559f1.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "./static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "32ae35da549f613ecfc5",
+    "url": "./static/js/main.bc8768bf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
