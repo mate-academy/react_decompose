c3e11c84c332b723e08541228861e37f586f1280
anisimovv <kirill.anisimovv@gmail.com>
2019-10-28 01:24:29 +0300

    Solution

---
+++ build/precache-manifest.75bfc83e47918783cfc80c1ca453bf64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "146b49e5d7f8b459ddcb46a192bdb623",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2b696b1ea11b7845f437",
+    "url": "./static/css/main.53aad29a.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "./static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "2b696b1ea11b7845f437",
+    "url": "./static/js/main.96ff4586.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
