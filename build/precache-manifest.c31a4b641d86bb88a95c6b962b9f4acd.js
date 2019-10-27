1691c7f6e3b8fe83e1b1288d294fd90eaf1e114b
Dmitriymush <powder322@gmail.com>
2019-10-27 21:23:15 +0200

    solution

---
+++ build/precache-manifest.c31a4b641d86bb88a95c6b962b9f4acd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d044c27e0ecb3c67e050dd9e39960e72",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fa9e329adb115cc359e2",
+    "url": "./static/css/main.586d5b4c.chunk.css"
+  },
+  {
+    "revision": "6df4285f02b988ca3bf5",
+    "url": "./static/js/2.fdcae455.chunk.js"
+  },
+  {
+    "revision": "fa9e329adb115cc359e2",
+    "url": "./static/js/main.01998ecf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/components/js/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(props) {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <nav className="navigation">
+        <a className="navigation__link" href="#">About</a>
+        <a className="navigation__link" href="#">Services</a>
+        <a className="navigation__link" href="#">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
