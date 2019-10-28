2d6b9ced6c63aaed8582359fbc082c2dc9d21726
Dmitriymush <54553744+Dmitriymush@users.noreply.github.com>
2019-10-28 09:59:24 +0200

    Update src/components/js/Section.js

--- src/components/js/Section.js
+++ src/components/js/Section.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 function Section(props) {
   return (
-    <>
       <section className="welcome">
         <span className="welcome__text">Sticky Header!</span>
       </section>
