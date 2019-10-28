1fd8f671047d191603f36de1a6fc95b38f9df40c
YuraBobeshko <gorobech55@gmail.com>
2019-10-28 17:03:52 +0200

    commit message

---
+++ build/precache-manifest.14d9ce984450decf4c340de16d374b96.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64d9e702c362439ae6893d65ee446333",
+    "url": "./index.html"
+  },
+  {
+    "revision": "21015a0bb0aa117122f1",
+    "url": "./static/css/main.dbd112cc.chunk.css"
+  },
+  {
+    "revision": "6aae36475cc7eb3a3485",
+    "url": "./static/js/2.7efea42a.chunk.js"
+  },
+  {
+    "revision": "21015a0bb0aa117122f1",
+    "url": "./static/js/main.4beb650c.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
---
+++ src/Section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import './Section.css';
+
+export default function Section () {
+  return (
+          <section className="welcome">
+            <span className="welcome__text">Sticky Header!</span>
+          </section>
+        );
+}
