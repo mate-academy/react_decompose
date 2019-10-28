4ffb592181becf3d94c8e3a3b56969f377365e04
lubremets <lubremets@gmail.com>
2019-10-28 14:37:40 +0200

    sol

---
+++ src/components/Section.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
