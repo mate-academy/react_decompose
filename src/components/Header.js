8f1360ee700bed6f17e381c53f0c201a45e78b96
lubremets <lubremets@gmail.com>
2019-10-29 11:04:50 +0200

    solution

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from 'react';
+
+export default function() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <nav className="navigation">
+        <a className="navigation__link" href="#">About</a>
+        <a className="navigation__link" href="#">Services</a>
+        <a className="navigation__link" href="#">Contact</a>
+      </nav>
+    </header>
+  );
+}
