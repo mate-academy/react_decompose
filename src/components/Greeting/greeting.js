49cf588ec6adcdd5c6532cb168836357fe2cfb24
Leon <FeelineKing@gmail.com>
2019-10-31 00:59:00 +0200

    Solution

---
+++ src/components/Greeting/greeting.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './greeting.css';
+
+const Greeting = () => (
+  <section className="greeting">
+    <span className="greeting__text">Sticky Header!</span>
+  </section>
+)
+
+export default Greeting;
---
+++ src/components/Navigation/navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navigation.css';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#">About</a>
+    <a className="navigation__link" href="#">Services</a>
+    <a className="navigation__link" href="#">Contact</a>
+  </nav>
+)
+
+export default Navigation;
