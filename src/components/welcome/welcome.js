4fce9761febd95d529f45bdc605d87433828f034
Alex-8-8 <46635183+Alex-8-8@users.noreply.github.com>
2020-02-11 23:35:16 +0200

    react decompose

---
+++ src/components/welcome/welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './welcome.css';
+
+export default function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
