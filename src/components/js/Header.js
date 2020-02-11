eec0ee4113ac650131bcc6298e53021b3fb90f46
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-02-11 18:21:08 +0200

    Decompose implement

---
+++ src/components/js/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Header.css';
+
+const Header = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Header;
