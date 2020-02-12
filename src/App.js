0de9954c6ef394aec40c315f6f2cd466799e79b7
dima <tynindima@gmail.com>
2020-02-12 13:00:08 +0200

    Deletad empty spaces, rename folder

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/welcome/Welcome';
-import Header from './components/header/Header';
-import Article from './components/article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 function App() {
   return (
     <main className="app">
       <Welcome />
-,
       <Header />
-,
       <Article />
-,
     </main>
   );
 }
