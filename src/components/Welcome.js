e2cb234be7d1d613800c525a3c86f42d5a5da992
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-02-12 13:24:29 +0200

    solution

---
+++ src/components/Welcome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
+
+export default Welcome;
