827db7f2f2d7c88c8212343c55b7ae5128760619
Roma <romakoder@gmail.com>
2020-02-12 11:50:17 +0200

    fixed comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/header/Header';
-import Welcome from './components/welcome/Welcome';
-import Article from './components/article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import Article from './components/Article/Article';
 import './App.css';
 
 function App() {
