4504ad09f59a1e6a86484b52f0c6e2e48ee06199
Olexander <kravchenko.sania@gmail.com>
2020-02-13 10:40:30 +0200

    attempt 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Header from './components/header/Header';
-import Article from './components/Article/Article';
+import Article from './components/article/Article';
 
 function App() {
   return (
