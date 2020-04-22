6cc07732dde5dac375af9993c21b82e191c8e293
crash2bash <sachagurov@gmail.com>
2020-04-22 10:18:07 +0300

    solution

---
+++ src/components/Main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Welcome from './welcome/Welcome';
+import Header from './header/Header';
+import Article from './article/Article';
+
+function Main() {
+  return (
+    <main className="app">
+      <Welcome />
+      <Header />
+      <Article />
+    </main>
+  );
+}
+
+export default Main;
