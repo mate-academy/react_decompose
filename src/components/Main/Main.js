053cea473e4433449f969cfa776d2d49f50f15f3
Kostya <oliinyk.kostya@gmail.com>
2020-04-22 16:41:29 +0300

    solution

---
+++ src/components/Main/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Article from '../Article/Article';
+import Section from '../Section/Section';
+
+function Main() {
+  return (
+    <main className="app">
+      <Section />
+      <Article />
+    </main>
+  );
+}
+
+export default Main;
