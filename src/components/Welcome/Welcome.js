23767ec61cc454d2e3051a86de5168c3c5a344de
WincherZ <wincher2016@gmail.com>
2020-04-22 15:40:56 +0300

    some changes

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Welcome.module.css';
+
+const Welcome = () => (
+  <section className={styles.welcome}>
+    <span className={styles.welcome__text}>Sticky Header!</span>
+  </section>
+);
+
+export default Welcome;
