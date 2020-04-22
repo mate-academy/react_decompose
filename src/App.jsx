39f5bed0bcd565c8588dc3c23feda5ab9ceae3c8
Kira Marks <serafita22@gmail.com>
2020-04-22 15:12:48 +0300

    solution

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './components/css/main.css';
+import Welcome from './components/js/Welcome';
+import Header from './components/js/Header';
+import Article from './components/js/Article';
+
+function App() {
+  return (
+    <main className="app">
+      <Welcome />
+      <Header />
+      <Article />
+    </main>
+  );
+}
+
+export default App;
