7101035d22060c2d566720a076ee0ad8312f31f8
Kostya <oliinyk.kostya@gmail.com>
2020-04-23 14:13:09 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-
-import Header from './components/Header/Header';
 import Main from './components/Main/Main';
 
 function App() {
   return (
     <div>
-      <Header />
+
       <Main />
     </div>
   );
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Article from '../Article/Article';
 import Section from '../Section/Section';
+import Header from '../Header/Header';
 
 function Main() {
   return (
     <main className="app">
+      <Header />
       <Section />
       <Article />
     </main>
