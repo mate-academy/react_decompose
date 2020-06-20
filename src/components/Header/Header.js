d9566ce65aa72dcc142f24e9d298766b6bfde0d9
solaryasha <solaryasha@gmail.com>
2020-06-20 22:59:24 +0300

    Decomposing App.js

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Header.module.css';
+import { Navigation } from '../Navigation/Navigation';
+
+export const Header = () => (
+  <header className={styles.header}>
+    <h1 className={styles.header__title}>Site Name</h1>
+    <Navigation />
+  </header>
+);
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Welcome.module.css';
+
+export const Welcome = () => (
+  <section className={styles.welcome}>
+    <span className={styles.welcome__text}>Sticky Header!</span>
+  </section>
+);
