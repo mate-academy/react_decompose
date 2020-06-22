c58601475ab1eee2a61facfb8aaac60df5fc4bfe
nazmeln <nazik100378@gmail.com>
2020-06-22 21:21:27 +0300

    solved

---
+++ src/Section/Section.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Section = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Section;
