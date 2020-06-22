6ef6194a90786ab61e7ca3bae82d8f39aa9c6f2f
Denys <gryshko_denys@gmail.com>
2020-06-22 19:51:36 +0300

    solution with components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './components/Welcome.css';
-import './components/Article.css';
-import './components/Header.css';
-
 import App from './App';
 
 ReactDOM.render(
