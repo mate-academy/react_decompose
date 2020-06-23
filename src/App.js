7fe100dd5990328378c40077f3803f3047338a0a
LimonovInsp <bis.limonov@gmail.com>
2020-06-23 12:25:17 +0300

    add arrow functions

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Welcome from './components/Welcome';
 import Header from './components/Header';
 import Article from './components/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './styles/Paragraph.css';
 import PropTypes from 'prop-types';
 
-function Paragraph({ text }) {
-  return (
-    <p className="article__paragraph">{text}</p>
-  );
-}
+const Paragraph = ({ text }) => (
+  <p className="article__paragraph">{text}</p>
+);
 
 Paragraph.propTypes = {
   text: PropTypes.string.isRequired,
