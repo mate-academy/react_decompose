8552d8736ace319b101ed0bcaa3dc22c468debe3
tyooma <flying.alien.godzilla@gmail.com>
2020-06-23 19:09:53 +0300

    Add folders + files

---
+++ src/components/section/Section.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Section.css';
+
+export const Section = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
