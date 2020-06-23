cc71ca9c0adad1cfd958f0ad3ea154f2efad1919
nazmeln <nazik100378@gmail.com>
2020-06-23 13:05:21 +0300

    fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './Section/Section.css';
+import './Header/Header.css';
+import './Article/Article.css';
 
 ReactDOM.render(
   <App />,
