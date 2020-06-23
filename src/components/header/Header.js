031c1bd2fe6b8926508a84a94b7664f636127cbd
Campari229 <KennyKoin@gmail.com>
2020-06-23 23:42:14 +0300

    small fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,11 +2,9 @@ import React from 'react';
 import './Header.css';
 import { HeaderNavigation } from './Header-nav';
 
-const Header = () => (
+export const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <HeaderNavigation />
   </header>
 );
-
-export { Header };
