ca3d1fd7605fa73276450589cce930488529f8c7
German <germandolia@gmail.com>
2020-06-23 12:43:20 +0300

    export fix

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,11 +3,9 @@ import React from 'react';
 import './Header.css';
 import { Navigation } from '../navigation/Navigation';
 
-const Header = () => (
+export const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
   </header>
 );
-
-export { Header };
