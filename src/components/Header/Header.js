e2d7878e40c6b15dacb5c36efdf0abb3ba31e25b
Andrii Badekha <badekha@gmail.com>
2020-08-30 18:35:23 +0300

    solution

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+import { Navigation } from '../Navigation/Navigation';
+
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
