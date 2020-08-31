df11035cc0279d10de2bbae67dd06b3a25b21cc2
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-31 14:44:32 +0300

    React first task

---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => (
+  <>
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  </>
+);
+
+export default Header;
