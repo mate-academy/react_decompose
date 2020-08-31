e55c0ba7ea16cc2647a722b335ff9417a4beb875
Maxym Chernyavskiy <masyatya@gmail.com>
2020-08-31 15:03:33 +0300

    add files

---
+++ src/components/welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './welcome.scss';
+
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Welcome;
