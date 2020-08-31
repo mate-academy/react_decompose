bebc72ba1e9826aa563430e1777ab487114f288b
KatrinaSavvova <kat.savvek@gmail.com>
2020-08-31 17:06:40 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Article from './components/Article';
-import Welcome from './components/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
