6d6f266c4612f1e201bb4b3f6e16e8141927cf17
oivannikov <alex0502ivannikov@gmail.com>
2020-08-31 19:05:30 +0300

    fixed code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import './App.css';
 import Main from './components/main';
-import Header from './components/header';
-import Content from './components/content';
-import Welcome from './components/welcome';
 
 function App() {
   return (
-    <>
-      <Main />
-      <Welcome />
-      <Header />
-      <Content />
-    </>
+    <Main />
   );
 }
 
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import Header from './header';
+import Content from './content';
+import Welcome from './welcome';
 
 const Main = () => (
   <main className="app">
-    
+    <Welcome />
+    <Header />
+    <Content />
   </main>
 );
 
