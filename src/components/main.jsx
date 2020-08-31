7be7d9195f2e9b365019fe1976a2aba0df432485
oivannikov <alex0502ivannikov@gmail.com>
2020-08-31 13:48:27 +0300

    solution

---
+++ src/components/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Main = () => (
+  <main className="app">
+    
+  </main>
+);
+
+export default Main;
---
+++ src/components/welcome.jsx
@@ -0,0 +1,11 @@
+import React  from 'react';
+
+import '../styles/welcome.css';
+
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+)
+
+export default Welcome; 
