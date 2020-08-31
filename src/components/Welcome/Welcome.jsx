4620285537bbd1193eddd986c8023490d9a5fc01
oggyrock <steelo.sun@gmail.com>
2020-08-31 15:59:17 +0300

    added forgotten files)

---
+++ src/components/Welcome/Welcome.jsx
@@ -0,0 +1,10 @@
+import React from '../../../node_modules/react';
+import './Welcome.scss';
+
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Welcome;
