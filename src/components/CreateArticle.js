a2331b613fa3679157502cb279da8d08b6a41c4d
Petro Mandziuk <64119700+Petro-JS-Developer@users.noreply.github.com>
2020-09-18 12:40:11 +0300

    add files

---
+++ src/components/CreateArticle.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function CreateArticle() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae,
+        fermentum ac quam. Aliquam pretium tristique nibh quis iaculis. In et
+        cursus ex, eu aliquet ex. Proin facilisis lacus sit amet sapien
+        ultrices, ut vehicula arcu lobortis. Vivamus mollis ipsum ut
+        hendrerit molestie. Morbi lacinia, sapien eu dictum dignissim, tellus
+        tortor congue magna, sit amet bibendum libero nisi id massa.
+      </p>
+      <h2 className="article__subtitle">Headline</h2>
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor.
+        Cras congue, massa nec sagittis mollis, dui felis ultrices magna,
+        tincidunt finibus lorem quam in sem. Morbi odio turpis, pulvinar sit
+        amet vulputate quis, ultricies eu libero. Donec ac maximus neque, nec
+        maximus nibh. Morbi rhoncus convallis urna, accumsan porta lorem
+        hendrerit in. Cras eget nisl dui. Morbi faucibus nisi eget ipsum
+        semper vulputate. Mauris nec tincidunt lectus. Aenean ac mi consequat
+        velit dignissim consectetur. Fusce placerat ac ipsum ac eleifend.
+        Aenean quis faucibus ex.
+      </p>
+      <h3 className="article__subSubTitle">Headline</h3>
+      <p className="article__paragraph">
+        Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
+        facilisi. In at elit id leo tristique condimentum. Donec at est
+        nulla. Mauris egestas magna ut laoreet pretium. Sed ultrices suscipit
+        vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Fusce id sapien eros. Vivamus viverra ultricies gravida. Nam urna
+        nibh, blandit a vulputate at, vehicula non nulla. Aenean ut nulla
+        leo. Praesent in ullamcorper est.
+      </p>
+    </article>
+  );
+}
+
+export default CreateArticle;
---
+++ src/components/CreateSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function CreateSection() {
+  return (
+    <section className="welcome">
+      <h1> Hello I`m your new site</h1>
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
+
+export default CreateSection;
