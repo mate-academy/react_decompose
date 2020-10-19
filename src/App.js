7765ddf867f4ecea0307c067d1e576decf32d73f
Yuriy Putsan <putsan57@gmail.com>
2020-10-19 16:39:55 +0300

    fix files structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Welcome from './components/Welcome';
-import Header from './components/Header';
-import Article from './components/Article';
+import Welcome from './components/welcome/Welcome';
+import Header from './components/header/Header';
+import Article from './components/article/Article';
 import './App.css';
 
 function App() {
