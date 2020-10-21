bbc04acdf3d9916bf8a7755ac742ca191f40cb5c
dmitriy <dmitriy.bondarenko.it@gmail.com>
2020-10-21 16:01:48 +0300

    message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/header';
-import Article from './components/article/article';
-import Welcome from './components/section_welcome/welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
