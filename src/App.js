bb2dcb6e140bec2c9228822c4c118e07b1d53bb0
Sasha Larin <sashalarin200@gmail.com>
2021-01-05 20:15:03 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Section from './components/Section';
-import Article from './components/Article';
+import Header from './components/Header/Header';
+import Section from './components/Section/Section';
+import Article from './components/Article/Article';
 
 function App() {
   return (
