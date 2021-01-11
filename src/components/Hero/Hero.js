4e49d87c19dfe767a7ddf9363b81e3c529ad96b4
5Mountains <V.Piatyhor@gmail.com>
2021-01-12 00:05:02 +0200

    solution

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Hero.css';
+
+function Hero() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
+
+export default Hero;
