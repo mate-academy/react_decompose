431797cc9225c664805e849895cbe963d6cb6a06
Roman Matsuk <the.show.of.ice.man@gmail.com>
2021-01-12 13:36:24 +0200

    Solution

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Main from './Main';
 
 ReactDOM.render(
-  <App />,
+  <Main />,
   document.getElementById('root'),
 );
