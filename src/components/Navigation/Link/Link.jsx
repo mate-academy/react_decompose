c2832677534c892f7daba8b194c8e078638d55be
Layrovell <marina95prodan@gmail.com>
2021-01-13 13:19:48 +0200

    fixed

--- src/components/Navigation/Link/Link.jsx
+++ src/components/Navigation/Link/Link.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const Item = ({ href, text }) => (
-  <a className="navigation__link" href={href}>
+export const Link = ({ href, text }) => (
+  <a
+    className="navigation__link"
+    href={href}
+  >
     {text}
   </a>
 );
 
-Item.propTypes = {
+Link.propTypes = {
   href: PropTypes.string.isRequired,
   text: PropTypes.string.isRequired,
 };
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Navigation.css';
-import { Item } from './Item/Item';
+import { Link } from './Link/Link';
 import NavItems from '../../data/navItems.json';
 
 export const Navigation = () => (
   <nav className="navigation">
-    {NavItems.map(el => (
-      <Item key={el.id} href={el.href} text={el.text} />
+    {NavItems.map(link => (
+      <Link
+        key={link.id}
+        href={link.href}
+        text={link.text}
+      />
     ))}
   </nav>
 );
