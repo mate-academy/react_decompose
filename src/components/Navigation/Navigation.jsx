07fd7d94e692a6bf59de2bbc0f63bd85c411958f
Denys Zabudskyi <denys_zabudskyi@govitall.com>
2021-03-05 13:34:29 +0200

    RD001 - First commit

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Navigation.css';
+
+const Navigation = () => (
+  <>
+    <a className="navigation__link" href="#about">About</a>
+    <a className="navigation__link" href="#services">Services</a>
+    <a className="navigation__link" href="#contact">Contact</a>
+  </>
+);
+
+export default Navigation;
