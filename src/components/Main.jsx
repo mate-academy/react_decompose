1a36c6845e4a597aa110bf298643c21374f6548f
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-14 12:52:42 +0200

    add nav section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Section from './Section';
-import Header from './Header';
+import Header from './Header/Header';
 import Article from './Article';
 
 const Main = () => (
