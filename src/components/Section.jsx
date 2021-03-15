618ca19803d71c33fe00521397544d3346b8f36b
Olha Briukhanova <44871883+oaleksandrovva@users.noreply.github.com>
2021-03-15 09:32:13 +0200

    Delete Section.jsx

--- src/components/Section.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Section = () => (
-  <section className="welcome">
-    <span className="welcome__text">Sticky Header!</span>
-  </section>
-);
-
-export default Section;
