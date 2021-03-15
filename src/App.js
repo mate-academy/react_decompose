3c3fffef5be88c1d7a7a45382849bc4afd33a248
Vladyslav Semyrekov <semyrekov.vladyslav@gmail.com>
2021-03-15 16:46:20 +0200

    small fixes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/welcome/Welcome';
 import Header from './components/header/Header';
-import Nav from './components/nav/Nav';
 import Article from './components/article/Article';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <main className="app">
       <Welcome />
       <Header />
-      <Nav />
       <Article />
     </main>
   );
