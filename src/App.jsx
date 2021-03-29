1a6c3231f5786afe8cd79c5d5b97090f3f5bab22
Taras Chaban <tarekchaban@gmail.com>
2021-03-29 17:38:18 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Welcome from './Welcome';
-import Header from './Header';
-import Article from './Article';
+import Welcome from './components/Welcome';
+import Header from './components/Header';
+import Article from './components/Article';
 
 import './App.css';
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
+import Navigation from './Navigation';
 
 import './Header.css';
 
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    <nav className="navigation">
-      <a className="navigation__link" href="#about">About</a>
-      <a className="navigation__link" href="#services">Services</a>
-      <a className="navigation__link" href="#contact">Contact</a>
-    </nav>
+    <Navigation />
   </header>
 );
 
