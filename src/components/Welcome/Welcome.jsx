0d0aade2e90007e59904baa819a8001e4676aa67
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-30 09:33:59 +0300

    changed file names

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import './Welcome.css';
+import './welcome.css';
 
-const Welcome = () => (
+export const Welcome = () => (
   <section className="welcome">
     <span className="welcome__text">Sticky Header!</span>
   </section>
 );
-
-export default Welcome;
