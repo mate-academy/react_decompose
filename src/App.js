caba3d5313913aecbc8c6ab7f212da1f9c864b80
Oleg Hluhk <glug.oleg@gmail.com>
2021-03-30 13:56:44 +0300

    fix comment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import Header from './components/header/Header';
-import Welcom from './components/welcom/Welcom';
+import Welcom from './components/welcome/Welcome';
 import Article from './components/article/Article';
 
 import './App.scss';
 
 const App = () => (
   <main className="app">
-    <>
-      <Welcom />
-      <Header />
-      <Article />
-    </>
+    <Welcom />
+    <Header />
+    <Article />
   </main>
 );
 
