5a61d808345849cb7e7ebbf69e9352032bc01636
Taras Shchadylo <tarasdidg@gmail.com>
2021-04-20 16:59:35 +0300

    add components

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+import Welcome from './components/welcome/Welcome';
+import Header from './components/header/Header';
+import Article from './components/article/Article';
+
+function App() {
+  return (
+    <main className="app">
+      <Welcome />
+      <Header />
+      <Article />
+    </main>
+  );
+}
+
+export default App;
