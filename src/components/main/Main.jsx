cb9de77d0a21febb5b572b9bcb16d4169840c5cc
scarlettjohanssons <wolverinelop@gmail.com>
2021-05-17 13:59:47 +0300

    add

---
+++ src/components/main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Welcome from '../welcome/Welcome';
+import Header from '../header/Header';
+import Article from '../article/Article';
+
+const Main = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
+
+export default Main;
