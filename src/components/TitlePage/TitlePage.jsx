3998642e17461f96e1e6c27162040e54d49017d2
Dima Batiuk <batiuk20@gmail.com>
2021-05-18 00:02:26 +0300

    solution

---
+++ src/components/TitlePage/TitlePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './TitlePage.css';
+
+const TitlePage = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default TitlePage;
