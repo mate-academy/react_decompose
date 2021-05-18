bb5bf1fbd4cb62a8a3be691558f3a9d3ecb28a02
amithos <alex.theutos@gmail.com>
2021-05-18 16:46:15 +0300

    the Main component was removed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 import './App.css';
 
-import Main from './components/Main/Main';
-
 const App = () => (
-  <Main />
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
 );
 
 export default App;
