3e2e5750b53799427bf727698663942029fa3da1
Oleh Senkiv <o.senkiv.v@gmail.com>
2021-05-18 16:54:35 +0300

    Solution-001

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss';
+import Navigation from '../navigation/Navigation';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
