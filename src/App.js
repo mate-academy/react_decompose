94399e262b2f45d03a7b5442afadf6678084f4ec
Leonid Vegera <leonid.vegera@gmail.com>
2021-05-18 13:00:56 +0300

    Added all files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
