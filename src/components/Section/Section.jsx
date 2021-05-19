3566fc0da6d3f3930f489205cb8c6fded9d1d9fb
herkoss <dmytroherko@icloud.com>
2021-05-19 12:17:09 +0300

    Folders added v2

---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Section.css';
+
+const Section = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Section;
