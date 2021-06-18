4b72822f09ab8a1ade8875d3553ac0e88c882215
Kirill Osypov <Kirill.Osypov@gmail.com>
2021-06-18 15:20:57 +0300

    Solution

---
+++ src/components/Article.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Paragraph1 from './Paragraphs/Paragragh1';
+import Paragraph2 from './Paragraphs/Paragraph2';
+import Paragraph3 from './Paragraphs/Paragraph3';
+import Paragraph4 from './Paragraphs/Paragraph4';
+import Paragraph5 from './Paragraphs/Paragraph5';
+
+function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
+      <Paragraph1 />
+      <Paragraph2 />
+      <Paragraph3 />
+      <Paragraph4 />
+      <Paragraph5 />
+    </article>
+  );
+}
+
+export default Article;
---
+++ src/components/Paragraphs/Paragraph4.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Paragraph4() {
+  return (
+    <p className="article__paragraph">
+      Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
+      facilisi. In at elit id leo tristique condimentum. Donec at est
+      nulla. Mauris egestas magna ut laoreet pretium. Sed ultrices suscipit
+      vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+      Fusce id sapien eros. Vivamus viverra ultricies gravida. Nam urna
+      nibh, blandit a vulputate at, vehicula non nulla. Aenean ut nulla
+      leo. Praesent in ullamcorper est.
+    </p>
+  );
+}
+
+export default Paragraph4;
