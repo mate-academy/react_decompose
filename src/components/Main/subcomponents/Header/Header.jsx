d4b11bf350de0ee5eb56faaf3abdd2ec0b976a3e
Oleksii Lymonov <alex25_92@ukr.net>
2021-07-12 12:01:32 +0300

    third_try

--- src/components/Main/subcomponents/Header/Header.jsx
+++ src/components/Main/subcomponents/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Navigation from './subcomponents/Navigation';
+import Navigation from './subcomponents/Navigation/Navigation';
 import './header.css';
 
 const Header = () => (
