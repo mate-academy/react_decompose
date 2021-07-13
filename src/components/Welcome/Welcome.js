fa3a4e8b384081f9afd6e43c33a402904c8d89df
Stas Pechenyi <spechenuy@gmail.com>
2021-07-13 16:11:28 +0300

    README file was updated

---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <main className="app">
+      <section className="welcome">
+        <span className="welcome__text">Sticky Header!</span>
+      </section>
+    </main>
+  );
+}
+
+export default Welcome;
