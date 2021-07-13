ec06f13ed4a638dc55e1a1985f508996e0f6ad53
Bishko <yurik322412@gmail.com>
2021-07-13 19:16:19 +0300

    first react app

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+import './header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Nav />
+  </header>
+);
+
+export default Header;
