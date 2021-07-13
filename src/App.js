d376790440462708ff4c653c1a18eee4299f6d58
David Dohru <tronsmartmy@gmail.com>
2021-07-13 19:53:25 +0300

    fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import SectionWelcome from './components/Section__Welcome/Section__Welcome';
-import Article from './components/Article/Article';
+import Article from './components/Article';
 
 function App() {
   return (
