c03c3861b66b7b4a313f99950069154588dd258c
John <jozaman96@gmail.com>
2021-07-14 12:45:49 +0300

    capitalized folders

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Article from './components/Aticle/Article';
+import Article from './components/Article/Article';
 
 function App() {
   return (
