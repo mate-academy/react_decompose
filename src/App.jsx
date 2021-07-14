5d319e3668741943005a6e962b2659468ebb9af3
Bishko <yurik322412@gmail.com>
2021-07-14 11:00:32 +0300

    add sub folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/Welcome';
-import Header from './components/Header';
-import Article from './components/Article';
+import Welcome from './components/welcome/Welcome';
+import Header from './components/header/Header';
+import Article from './components/article/Article';
 
 function App() {
   return (
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Nav from './Nav';
+import Nav from '../nav/Nav';
 
 import './header.css';
 
