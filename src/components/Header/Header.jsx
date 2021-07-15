10f4fabb400e7ea006173d2218aac41d54c5902e
Yaroslav <gso218695@gmail.com>
2021-07-15 21:39:34 +0300

    test

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+
+import './Header.css';
+
+const Header = () => (
+  <>
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Nav />
+    </header>
+  </>
+);
+
+export default Header;
