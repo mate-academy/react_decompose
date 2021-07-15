dcb9829e2076667ba8048577ae74272e85c6cd9b
Oleh Pidverbetskyi <PidverbetskiyOleg1@gmail.com>
2021-07-15 18:22:09 +0300

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/header/header';
-import Welcome from './components/Welcome/welcome';
-import Article from './components/article/article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import Article from './components/Article/Article';
 
 function App() {
   return (
