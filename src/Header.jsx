0bc934a9156fdb81aeefd273d48685c596e6c5f0
Artem <temaqwerty15@gmail.com>
2021-07-15 18:08:23 +0300

    fin

---
+++ src/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Nav from './Nav';
+
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Nav />
+  </header>
+);
+
+export default Header;
---
+++ src/Section.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Welcome.css';
+
+const Section = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Section;
