469ab7acdb3bfa0384fad14224d4fdb4f2f94d1b
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-07-15 17:49:29 +0300

    deleted Main.jsx component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
-import Main from './components/Main';
+import WelcomSection from './components/WelcomeSection/WelcomeSection';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 const App = () => (
-  <Main />
+  <main className="app">
+    <WelcomSection />
+    <Header />
+    <Article />
+  </main>
 );
 
 export default App;
