b011841b7bb674cd3eb77c51a637f94e6ada2e5a
Михаил <mihay0113@gmail.com>
2021-07-15 16:13:54 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
-import Section from './components/Section';
+import Welcome from './components/Welcome';
 import Article from './components/Article';
 
 function App() {
   return (
     <main className="app">
-      <Section />
+      <Welcome />
       <Header />
       <Article />
     </main>
