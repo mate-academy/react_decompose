9162c578624be94f4de5c7b7a32144b63d57b7d6
Tetiana Yakovchuk <tanyalife294@gmail.com>
2021-07-15 12:28:05 +0300

    solution

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Nav from '../Nav/Nav';
+import './header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Welcome from '../Welcome/Welcome';
+import Header from '../Header/Header';
+import Article from '../Article/Article';
+
+function Main() {
+  return (
+    <>
+      <Welcome />
+      <Header />
+      <Article />
+    </>
+  );
+}
+
+export default Main;
