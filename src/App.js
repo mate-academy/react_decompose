f409e5a5675c90a27c180528c548c471ff6f7a7a
Artem <temaqwerty15@gmail.com>
2021-07-16 13:15:24 +0300

    fin fix

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Article from './Article';
-import Header from './Header';
-import Section from './Section';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Section from './components/Section/Section';
 
 function App() {
   return (
