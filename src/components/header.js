3aaa18ba1bbde3e08a118b4751a02a7c5c379d4b
Yaslik1033 <55955258+Yaslik1033@users.noreply.github.com>
2021-07-29 12:43:58 +0300

    some message

---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+  <>
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">About</a>
+        <a className="navigation__link" href="#services">Services</a>
+        <a className="navigation__link" href="#contact">Contact</a>
+      </nav>
+    </header>
+  </>);
+}
+
+export default Header;
