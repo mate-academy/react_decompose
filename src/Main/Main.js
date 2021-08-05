07acc154ee8b1aac7d8cc8c918f97fb6f5b3ca3a
Yulia <shabelna28@ukr.net>
2021-08-05 22:14:39 +0300

    Sol

---
+++ src/Main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Section from "../Section/Section";
+import Header from '../Header/Header';
+import Article from '../Article/Article';
+
+const Main = () => (
+    <main className="app">
+      <Section />
+      <Header />
+      <Article />
+    </main>
+);
+
+export default Main;
