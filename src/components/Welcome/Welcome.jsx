82ccfcaac847775f9c2e06df155dabfc3c62cc17
NastyaVovk <winc4ester@gmail.com>
2021-08-25 14:56:36 +0300

    initial

--- src/components/Welcome/Welcome.jsx
+++
@@ -1,6 +0,0 @@
-// Don't forget to import the React library
-// Import a css file containig weclome styles
-
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
