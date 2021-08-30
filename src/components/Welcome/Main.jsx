469e7f170254a6ec3bb6aa34eeac75e06fa92007
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-08-30 11:34:22 +0300

    Switch to branch develop

--- src/components/Welcome/Main.jsx
+++ src/components/Welcome/Main.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 
-import Welcome from './Welcome';
 import Header from './Header';
 import Article from './Article';
 
 const Main = () => (
   <main className="app">
-    <Welcome />
     <Header />
     <Article />
   </main>
