6c257c25441fe5fdc027db5c33d7a8cec9e3cfd5
unknown <hado.roman.ihorovych@gmail.com>
2021-08-30 12:35:42 +0300

    added some fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Welcome from './Welcome';
-import Header from './Header';
-import Article from './Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 function App() {
   return (
