42911cc8c7dae334f604e316f977acb12327ffb4
Andrii Serhovetc <serhovetc88@gmail.com>
2021-08-30 11:51:04 +0300

    fix after review

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,10 @@ import './Header.css';
 import Nav from './Nav/Nav';
 
 const Header = () => (
-  <>
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <Nav />
-    </header>
-  </>
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Nav />
+  </header>
 );
 
 export default Header;
