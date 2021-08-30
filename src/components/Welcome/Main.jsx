58c53eb76d2e54ed3960056b2410be75cccbd016
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-08-30 11:32:21 +0300

    React_decompose solved

---
+++ src/components/Welcome/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Welcome from './Welcome';
+import Header from './Header';
+import Article from './Article';
+
+const Main = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
+
+export default Main;
