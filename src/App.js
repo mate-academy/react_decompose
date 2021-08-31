b76af7680e117f9c5dfbde48644d9c0948c50616
Andriy Nikorych <andrei,nikorich@gmail.com>
2021-08-31 17:19:07 +0300

    fixed code review comments

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Article from './components/Welcome/Article';
-import Header from './components/Welcome/Header';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 
 function App() {
