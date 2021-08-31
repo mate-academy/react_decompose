9ec76dc3a8e1d3850a1901f2c48f2727a61e4a48
Roman Yacenko <ryacenko3@gmail.com>
2021-08-31 09:41:45 +0300

    fixed solution

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Welcome from './Welcome/Welcome';
+import Header from './Header/Header';
+import Article from './Article/Article';
+
+const Main = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
+
+export default Main;
