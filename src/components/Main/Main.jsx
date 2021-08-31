59c374e073dcacf726458f5fcda59b5c44d70b32
Roman Yacenko <ryacenko3@gmail.com>
2021-08-31 12:31:42 +0300

    fixed location of files

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Welcome from './Welcome/Welcome';
-import Header from './Header/Header';
+import Welcome from '../Welcome/Welcome';
+import Header from '../Header/Header';
 import Article from './Article/Article';
 
 const Main = () => (
