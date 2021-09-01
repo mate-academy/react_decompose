83096b581856d91b41606af62e858500e88828c2
Oleksandr Makohon <sashamakogon94@gmail.com>
2021-09-01 07:37:22 +0300

    fix link

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,7 +3,8 @@
 
 // Create a Welcome function returning the HTML of welcome block
 
-// Add a default export statement for Welcome component to use it in the other files
+// Add a default export statement
+// for Welcome component to use it in the other files
 
 import React from 'react';
 import './Welcome.css';
