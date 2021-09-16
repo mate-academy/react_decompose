4b27f10cf1dfd5091af06e3a6456809b8dd52e2a
Roman Minchenko <minchenko.poma@gmail.com>
2021-09-16 12:16:40 +0300

    react-decompose

---
+++ src/components/Article/Article.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Paragraph from '../Paragraph/Paragraph';
+
+import './Article.css';
+
+const Article = () => (
+  <article className="article">
+    <h1 className="article__title">Headline</h1>
+
+    <Paragraph />
+
+  </article>
+);
+
+export default Article;
