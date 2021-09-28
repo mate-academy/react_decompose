5de3823a5a7a80b0921687111a1abe26fa306f39
vpolets <vpolets@gmail.com>
2021-09-28 13:46:57 +0300

    Update repo. Added cypress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
 module.exports = {
-  extends: '@mate-academy/eslint-config-react',
+  extends: ['@mate-academy/eslint-config-react-typescript', 'plugin:cypress/recommended'],
+  // extends: '@mate-academy/eslint-config-react',
 };
