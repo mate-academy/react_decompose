71017c13a7e0b9bdd834f02c563374b640dc7ae7
caelum29 <caelum29@gmail.com>
2021-10-07 16:38:25 +0300

    solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,10 @@
-// Don't forget to import the React library
-// Import a css file containig weclome styles
+import './Welcome.scss';
+import React from 'react';
 
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
+export default function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
