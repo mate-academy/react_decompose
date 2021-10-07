ea644ce05fcc63a236281dbbee73e6558a18f66d
Oleksandr Krasnoshchokov <krasnoshchokovoleksandr@gmail.com>
2021-10-07 13:36:25 +0300

    Done

---
+++ src/blocks/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default Header;
+
+function Header() {
+  function HeaderTitle() {
+    return (
+      <h1 className="header__title">Site Name</h1>
+    );
+  }
+
+  function Nav() {
+    return (
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">About</a>
+        <a className="navigation__link" href="#services">Services</a>
+        <a className="navigation__link" href="#contact">Contact</a>
+      </nav>
+    );
+  }
+
+  return (
+    <header className="header">
+      <HeaderTitle />
+      <Nav />
+    </header>
+  );
+}
---
+++ src/blocks/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default Welcome;
+
+function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
