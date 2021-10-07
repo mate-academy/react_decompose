a12f4ece8d67783f87fadafc16aa470973e87286
Dima Anosov <anosov.work@gmail.com>
2021-10-07 15:25:48 +0300

    React decompose Solution 2

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,3 @@
-// Don't forget to import the React library
-// Import a css file containig weclome styles
-// Create a Welcome function returning the HTML of welcome block
 import React from 'react';
 import './Welcome.css';
 
