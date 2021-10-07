62c89cd6f3031ba543c7742fe4894454f8c5ec9a
Denis Demyanok <DenisRusis@ex.ua>
2021-10-07 17:53:58 +0300

    Some Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Article from '../Article/Article';
-import Header from '../Header/Header';
-import Welcome from '../Welcome/Welcome';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
