16e76fbdf2598fbc5db99f1e4033e0aa85bdcfbf
Alex <alex.krynytskyi@gmail.com>
2021-10-25 11:37:46 +0300

    small fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,17 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
   rules: {
-    'import/no-extraneous-dependencies': ['error', {
-      devDependencies: false,
-      optionalDependencies: false,
-      peerDependencies: false,
-    }],
+    'react/jsx-filename-extension': [
+      2,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: false,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
   },
 };
