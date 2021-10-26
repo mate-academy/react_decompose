49dce4f296048feaf77c0ea4ee02b7cb59f3b0ea
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-10-26 09:04:39 +0300

    SOLUTION-2

---
+++ src/components/Article/ArticleTitle/ArticleTitle.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const ArticleTitle = () => <h1 className="article__title">Headline</h1>;
+
+export default ArticleTitle;
---
+++ src/components/Header/HeaderTitle/HeaderTitle.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import './HeaderTitle.css';
+
+const HeaderTitle = () => <h1 className="header__title">Site Name</h1>;
+
+export default HeaderTitle;
