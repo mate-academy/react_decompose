26448c9addf73b231bb9296426516bf2bbe83105
Volodymyr Taranin <jaffplay@gmail.com>
2021-11-03 12:14:59 +0200

    welcome added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
-import Article from './components/Article/Article';
+
+import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
     <main className="app">
+      <Welcome />
       <Header />
       <Article />
     </main>
