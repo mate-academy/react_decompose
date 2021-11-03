33e8e2645435756642976a9207f9cf1bad16fc7c
Taras Lynets <linetstg@gmail.com>
2021-11-03 09:16:14 +0200

    update2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import Nav from '../Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    <Nav />
+    <Navigation />
   </header>
 );
 
