dbb1e4083123ad965ef4967f72cb1954d3693d92
Anton Karpena <antoniokarpena@gmail.com>
2021-11-03 09:49:51 +0200

    Initial commit

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,10 @@
-// Don't forget to import the React library
-// Import a css file containig weclome styles
+import React from 'react';
+import './Welcome.css';
 
-// Create a Welcome function returning the HTML of welcome block
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
 
-// Add a default export statement for Welcome component to use it in the other files
+export default Welcome;
