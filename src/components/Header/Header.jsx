815f381964d1a5d4ab17593c379346713b2f034f
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-21 22:10:21 +0200

    Commit components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+import { Navigation } from '../Navigation/Navigation';
+
+export function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
