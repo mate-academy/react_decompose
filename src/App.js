073304b35c02eb8a95206bf24bd8fa869f53fed3
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-21 19:35:17 +0200

    Change file structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 
-import Welcome from './components/Welcome/Welcome';
-import Header from './components/Header/Header';
-import Article from './components/Article/Article';
+import { Welcome } from './components/Welcome';
+import { Header } from './components/Header';
+import { Article } from './components/Article';
 
 function App() {
   return (
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './Welcome.css';
 
-function Welcome() {
+export function Welcome() {
   return (
     <section className="welcome">
       <span className="welcome__text">Sticky Header!</span>
     </section>
   );
 }
-
-export default Welcome;
