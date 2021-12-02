6b6cae1ab7a8143535bf0180ef20afa0c9384fe3
Oleksiy Pochkayev <oleksiypochkayev@gmail.com>
2021-12-02 17:30:33 +0200

    ?

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Banner from './Banner';
 import Article from './Article';
-import './styles/App.css';
+import './styles/App';
 
 function App() {
   return (
