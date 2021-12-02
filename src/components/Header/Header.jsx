f71e8903acb000502a48165e7bc2e2d6ab4ab9e1
Olya <olya.shyian.fed@gmail.com>
2021-12-02 16:25:27 +0200

    Solution

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import './Header.css';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
