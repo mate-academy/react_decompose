2e2a2a473dd1f4a936a0be70c84c1200074ebc70
lanebx <havchikos@gmail.com>
2021-12-02 12:00:58 +0200

    new Header block

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+import Navigation from '../Navigation/Navigation';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
