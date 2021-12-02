0188aeb0a4fe5a5780361acb24c1e0526ddc9aef
Lera Permiakova <vv.permiakova@gmail.com>
2021-12-02 23:00:46 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
-// Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
