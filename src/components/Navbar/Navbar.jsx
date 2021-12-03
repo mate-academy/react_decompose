51dd40ed4c58a8389949eb2ac2f47a4c7591b3dd
Ivan Stanko <ivan_stanko@ukr.net>
2021-12-03 11:55:33 +0200

    аÑfixed bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,9 +3,15 @@ import './Navbar.css';
 
 const Navbar = () => (
   <nav className="navigation">
-    <a className="navigation__link" href="#about">About</a>
-    <a className="navigation__link" href="#services">Services</a>
-    <a className="navigation__link" href="#contact">Contact</a>
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
   </nav>
 );
 
