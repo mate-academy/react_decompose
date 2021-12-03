f77abe3d92daac8380b62ce0725a50629b4b756b
Sviatoslav Myhal <svitilda19@gmail.com>
2021-12-03 15:01:08 +0200

    solution

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Banner.css';
+
+function Banner() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
+
+export default Banner;
