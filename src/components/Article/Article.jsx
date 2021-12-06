0c8b3e39ddb6443bbdffe894765cdd8d45f50c78
Volodymyr Soltys <soltus.vovan@gmail.com>
2021-12-06 11:55:46 +0200

    Fixes after code review

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Article.scss';
+import './Article.css';
 
 export function Article() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Navigation } from '../Navigation/Navigation';
-import './Header.scss';
+import './Header.css';
 
 export function Header() {
   return (
