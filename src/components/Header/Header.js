5858f6bd39a371fe9ce7ecb24b77894a208792c3
Mariia Kovach <mariiakovach123@gmail.com>
2021-12-07 14:05:48 +0200

    double navigation issue was fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,11 +6,6 @@ const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
-    <nav className="navigation">
-      <a className="navigation__link" href="#about">About</a>
-      <a className="navigation__link" href="#services">Services</a>
-      <a className="navigation__link" href="#contact">Contact</a>
-    </nav>
   </header>
 );
 
