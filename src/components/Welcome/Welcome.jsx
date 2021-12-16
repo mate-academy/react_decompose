0819fb5527e53ec35401ed1c4da26756b6939445
Oleh Khashchevskyi <hashchevskyj@gmail.com>
2021-12-16 18:50:03 +0200

    fixed code

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Welcome.scss';
 
-export default () => (
+const Welcome = () => (
   <section className="welcome">
     <span className="welcome__text">Sticky Header!</span>
   </section>
 );
+
+export default Welcome;
