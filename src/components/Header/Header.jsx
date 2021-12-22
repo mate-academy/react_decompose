af4eb659f3e6db956d236ab8eccbd75fb47b7e58
Dmytro Prontenko <dprontenko@gmail.com>
2021-12-22 20:01:51 +0200

    Solution_2

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+  </header>
+);
+
+export default Header;
