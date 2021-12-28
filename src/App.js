6626c20d1857f11ce4e29accde2b16f112de4a7a
Oleksandr Leshchenko <bulochkin72@gmail.com>
2021-12-28 02:08:48 +0200

    commit added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
+import './App.css';
 
 function App() {
   return (
