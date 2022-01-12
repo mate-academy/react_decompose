5d724147824d1efaa0f299e586298fd867fb14a5
Yevhenii Rozdorozhnii <rozdorozhnii@ukr.net>
2022-01-12 21:14:40 +0200

    solution_v1.0

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Navigation from '../Navigation/Navigation';
+import './Header.scss';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
