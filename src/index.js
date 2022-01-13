478ded6b8d309638e837ad65429cd42155cbba47
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-01-13 09:59:06 +0200

    Solution

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './components/Welcome/Welcome.css';
+
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
