3ba27bd80089935b10ad98a1e36b10a43ff45a84
AlexAzalor <87265793+AlexAzalor@users.noreply.github.com>
2022-01-13 11:29:27 +0200

    Fixed

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import './components/Welcome/Welcome.css';
-
 ReactDOM.render(
   <App />,
   document.getElementById('root'),
