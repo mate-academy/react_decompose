43223d12070f181aa866231d5e2cd41ea73e29f6
Yulia Koshkina <julyasav4uk@gmail.com>
2022-01-13 15:49:07 +0200

    create components folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import './App.css';
-import Welcome from './Welcome';
-import Header from './Header';
-import Article from './Article';
-// Move each BEM block to a separate component (file) and import them here
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 function App() {
   return (
