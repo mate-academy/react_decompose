678d0a1d82380f3b89ea4a7e4a43d4bc04c9092d
Aleksandra Mileant <aleksandramileant1@gmail.com>
2022-01-13 17:25:17 +0100

    fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './header/header';
-import Navigation from './navigation/navigation';
 import Article from './article/article';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
