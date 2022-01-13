85baf989c0e7359724c82f1fd75818de7ef64c8c
Artem <tema.maksimovich@gmail.com>
2022-01-13 17:34:22 +0200

    Task is done

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: ['@mate-academy/eslint-config-react', 'plugin:cypress/recommended'],
   rules: {
+    'linebreak-style': ['error', 'windows'],
     'import/no-extraneous-dependencies': ['error', {
       devDependencies: false,
       optionalDependencies: false,
