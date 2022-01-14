cbe2025853d0dc2c6826ab74009f1e7ab56dcd61
Ernesto Fernandez <ernesto.fernandez.1980@gmail.com>
2022-01-14 02:56:26 +0200

    solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import Main from './components/Main/Main';
-
-// Move each BEM block to a separate component (file) and import them here
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import './App.css';
 
 function App() {
-  return <Main />;
+  return (
+    <main className="app">
+      <Welcome />
+      <Header />
+      <Article />
+    </main>
+  );
 }
 
 export default App;
