c5e65cfaeb89b67754151f0c026d08228b0b3ff9
Dorosh13 <dorosh.d.13@gmail.com>
2022-02-10 18:00:40 +0200

    created decompose

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Navigation from './Navigation/Navigation';
+
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
