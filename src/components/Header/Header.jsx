a82a1375145f32ad4cd6d7762122274b6333d082
pogrebnyakp <pogrebnyakpavel@gmail.com>
2022-02-10 16:28:08 +0200

    add file

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+import './Header.css';
+import Nav from '../Nav/Nav';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
