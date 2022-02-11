be42f25b2543fe2705a40e7a1cf92ee6ce4b6b3d
Misha Novikov <novikov1807@gmail.com>
2022-02-11 09:18:16 +0200

    added files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+
+import './App.css';
+
+function App() {
+  return (
+    <main className="app">
+      <Welcome />
+      <Header />
+      <Article />
+    </main>
+  );
+}
+
+export default App;
