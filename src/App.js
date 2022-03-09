47aca9009873b10857d253a870ede00acf987022
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-09 12:40:33 +0200

    error fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
-import Article from './components/Article/Article.tsx';
+import Article from './components/Article/Article';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
