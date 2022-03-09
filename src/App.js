c3f33908e6bdbdb879309b971c40127ded73f247
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-09 12:58:23 +0200

    article added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
