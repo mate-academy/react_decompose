99e02d0f76270f046d5c7d858ecece3bd49476ab
Mykhailo Molnar <mmolnar@atlassian.com>
2022-03-09 12:22:15 +0200

    fixed errors with deploymnet

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react';
 
 import './Welcome.css';
 
 function Welcome() {
   return (
     <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
+      <span className="welcome__text">Sticky Header!</span>
     </section>
   );
 }
