a8f800b1548ab2e7961f2f81f1f0ecaf783fed22
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2022-03-15 17:47:05 +0200

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import './App.css';
 import Welcome from './components/Welcome/Welcome';
-import './components/Welcome/Welcome.css';
 import Header from './components/Header/Header';
-import './components/Header/Header.css';
 import Article from './components/Article/Article';
-import './components/Article/Article.css';
 
 // Move each BEM block to a separate component (file) and import them here
 
