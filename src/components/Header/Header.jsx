ed77118d8a9f30e549fc9d66f9358212edc11fc3
krolich500grn <edu.izaytsev@gmail.com>
2022-03-19 22:05:24 +0200

    solution decompose

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Navigation } from '../Navigation/Navigation';
+
+import './Header.scss';
+
+export function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
