7cd50d072c991e9c8ad4912b5fc0131148a8aa25
Roma Boreiko <roma.boreiko@gmail.com>
2022-03-23 19:28:15 +0200

    Solution

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable import/no-named-as-default */
+import React from 'react';
+
+import Navigation from '../Navigation/Navigation';
+
+import './Header.css';
+
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
