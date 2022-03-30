36644ee328ffaf4fb7e47e56c466a4d7d493d3d0
KristinaTar <kristina.tarashchuk@gmail.com>
2022-03-30 18:39:56 +0300

    xxx

---
+++ src/components/App/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Welcome from '../Welcome/Welcome';
+import Header from '../Header/Header';
+import Article from '../Article/Article';
+
+function App() {
+  return (
+    <>
+      <main className="app">
+        <Welcome />
+        <Header />
+        <Article />
+
+      </main>
+    </>
+  );
+}
+
+export default App;
