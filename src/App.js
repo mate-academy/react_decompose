7d080fbea0a3a39ae401639fe7a720284912a65b
Max Nikiforov <maxxnikiforov@gmail.com>
2022-04-26 22:56:10 +0700

    1 folder delated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
-import Welcome from './components/Welcome/Welcome/Welcome';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
