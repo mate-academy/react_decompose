fd40322c650812b41493d0d2f0e5e3d623eaddca
Maksym Yaremenko <restartermord123@ukr.net>
2022-04-27 18:08:56 +0300

    Folder fixes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/Welcome/Welcome/Welcome';
-import Header from './components/Welcome/Header/Header';
-import Article from './components/Welcome/Article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
