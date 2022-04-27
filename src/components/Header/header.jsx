0ee0f812dd8f33ce6e1ea744557f1ada7c8946a9
SeKaM0 <polozhentsew02@gmail.com>
2022-04-27 18:13:16 +0300

    Fix mistakes

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -5,6 +5,7 @@ import Nav from '../Navigation/nav';
 function Header() {
   return (
     <header className="header">
+      <h1 className="header__title">Site Name</h1>
       <Nav />
     </header>
   );
--- src/components/Navigation/nav.jsx
+++ src/components/Navigation/nav.jsx
@@ -3,14 +3,11 @@ import './nav.css';
 
 function Nav() {
   return (
-    <>
-      <h1 className="header__title">Site Name</h1>
-      <nav className="navigation">
-        <a className="navigation__link" href="#about">About</a>
-        <a className="navigation__link" href="#services">Services</a>
-        <a className="navigation__link" href="#contact">Contact</a>
-      </nav>
-    </>
+    <nav className="navigation">
+      <a className="navigation__link" href="#about">About</a>
+      <a className="navigation__link" href="#services">Services</a>
+      <a className="navigation__link" href="#contact">Contact</a>
+    </nav>
   );
 }
 
