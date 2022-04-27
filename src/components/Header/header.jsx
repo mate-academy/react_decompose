356f84803b76ce208ba4c8466ea335d13a6dd2de
SeKaM0 <polozhentsew02@gmail.com>
2022-04-27 16:03:15 +0300

    Finish Task

---
+++ src/components/Header/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './header.css';
+import Nav from '../Navigation/nav';
+
+function Header() {
+  return (
+    <header className="header">
+      <Nav />
+    </header>
+  );
+}
+
+export default Header;
