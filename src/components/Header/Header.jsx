02ae2e648c0d7fd0c71c55e13a9125ddec27c1ed
Ivan Boiko <ivan.boiko.code@gmail.com>
2022-04-29 00:58:22 +0300

    react-decompose

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
