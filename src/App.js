0abc06fcb0efa6a2a78652799d07146d4468e4da
yurii837 <gorogotskiy@gmail.com>
2022-04-29 11:37:19 +0300

    rename invalid components, remove comments

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/wellcome/Welcome';
+import Welcome from './components/welcome/Welcome';
 import Article from './components/article/Aticle';
 import Header from './components/header/Header';
-
-// Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
     <main className="app">
       <Welcome />
-      <Article />
       <Header />
+      <Article />
     </main>
   );
 }
