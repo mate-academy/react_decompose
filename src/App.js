be3aa4722e0982907d83549b14474db04b09bcd7
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-05-18 15:08:23 +0300

    Navigation component add

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <main className="app">
       <Welcome />
       <Header />
+      <Navigation />
       <Article />
     </main>
   );
