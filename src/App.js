29cabe6ab232572b5f4363518a8e6fb02733a919
Zubov Illia <contactduodiamonds@gmail.com>
2022-05-23 18:41:15 +0300

    deleted usless style imports css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header/Header';
-import './components/Header/Header.css';
 import Welcome from './components/Welcome/Welcome';
-import './components/Welcome/Welcome.css';
 import Article from './components/Article/Article';
-import './components/Article/Article.css';
 
 function App() {
   return (
