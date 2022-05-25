554edcae904270117f763bb23cde376f47c967aa
Valerii Derkach <valerij.derkach@gmail.com>
2022-05-25 11:47:46 +0300

    navigation folder added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,16 +6,13 @@
 // Add a default export statement for Header component to use it in the other files
 import React from 'react';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <nav className="navigation">
-        <a className="navigation__link" href="#about">About</a>
-        <a className="navigation__link" href="#services">Services</a>
-        <a className="navigation__link" href="#contact">Contact</a>
-      </nav>
+      <Navigation />
     </header>
   );
 }
