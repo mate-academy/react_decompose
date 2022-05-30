c55ae8b07fe59ed51ecd8a02d2f36e3fef46f36e
Victor Hrygorash <viktor12041991@i.ua>
2022-05-30 19:48:37 +0300

    split header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
@@ -8,14 +9,7 @@ function App() {
       <section className="welcome">
         <span className="welcome__text">Sticky Header!</span>
       </section>
-      <header className="header">
-        <h1 className="header__title">Site Name</h1>
-        <nav className="navigation">
-          <a className="navigation__link" href="#about">About</a>
-          <a className="navigation__link" href="#services">Services</a>
-          <a className="navigation__link" href="#contact">Contact</a>
-        </nav>
-      </header>
+      <Header />
       <article className="article">
         <h1 className="article__title">Headline</h1>
         <p className="article__paragraph">
