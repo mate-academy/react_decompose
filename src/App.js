a98a5266a6ecfe7eac627fde456a4b5dc4617de6
Karina Prozner <prozner.karina@gmail.com>
2022-05-30 15:37:07 +0200

    okay, now everything should work

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Article from './Article';
-import Welcome from './Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
     <main className="app">
-      <Header />
-      <Article />
       <Welcome />
+
+      <Header />
+
+      <Article />
     </main>
   );
 }
