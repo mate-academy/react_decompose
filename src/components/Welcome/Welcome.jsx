9f42ae52c9dba0a703c8351bc2ea15f6728f7bca
Olya Bushovska <OlyaBushovska767@gmail.com>
2022-06-09 00:38:44 +0300

    Solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
+import './Welcome.css';
+
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Welcome;
 // Don't forget to import the React library
 // Import a css file containig weclome styles
 
