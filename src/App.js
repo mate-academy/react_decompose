f385fffbf6f9277c83cdfbda7088c5bdae0c8db5
Yaroslav Baiduk <byaa99@gmail.com>
2022-07-01 00:44:56 +0300

    Love React <3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Article from './components/Article';
-import Header from './components/Header';
-import Welcome from './components/Welcome';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 import './App.css';
 
 function App() {
