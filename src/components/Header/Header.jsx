0cc373c8bf0b87ea95a9fd45b78238a1484c4998
SAMAEV <andreysamaev@gmail.com>
2022-06-30 17:10:46 +0300

    reacting

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,3 +4,20 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <>
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">About</a>
+        <a className="navigation__link" href="#services">Services</a>
+        <a className="navigation__link" href="#contact">Contact</a>
+      </nav>
+    </header>
+  </>
+);
+
+export default Header;
