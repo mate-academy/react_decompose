1d70eb29893dcbf97897bf9f8628bbcd4d24dfe0
Alina Komashchuk <akomashchuk24@gmail.com>
2022-07-01 00:02:12 +0300

    solution

--- src/components/Header/Header.jsx
+++
@@ -1,6 +0,0 @@
-// Don't forget to import the React library
-// Import a css file containig header styles
-
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
