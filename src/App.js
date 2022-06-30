0822e46629d289f42d4466de344dad2ece06ee0f
Ostap Salo <jlumon211@gmail.com>
2022-06-30 18:06:16 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <main className="app">
       <Header />
       <Article />
+      <Welcome />
     </main>
   );
 }
