1ed77efa0cb663e828f2f7679f103a03b63e56cc
viktorlyt <viktorlyt@gmail.com>
2022-07-01 12:43:31 +0300

    solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 import './Welcome.css';
 
 function Welcome() {
-  <section className="welcome">
-    <span className="welcome__text">Sticky Header!</span>
-  </section>;
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
 }
 
 export default Welcome;
