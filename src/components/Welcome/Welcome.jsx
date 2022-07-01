0191ba75d48be8807461675bac79033d7777391e
Andrew Khorsun <andrewkhorsun@gmail.com>
2022-07-01 14:04:43 +0300

    Fixed 01.07.22

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,17 @@
 // Don't forget to import the React library
 // Import a css file containig weclome styles
 
+import React from 'react';
+import './Welcome.css';
+
+function Welcome() {
+  (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
 // Create a Welcome function returning the HTML of welcome block
 
+export default Welcome;
 // Add a default export statement for Welcome component to use it in the other files
