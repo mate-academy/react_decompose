7a99cf8bdde6dcf6edd95b1d55ad179694820860
Mykhailo Parfeniuk <mypa@brighta.biz>
2022-07-03 13:21:18 +0300

    App decomposition has been made

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
-// Don't forget to import the React library
-// Import a css file containig header styles
+import React from 'react';
+import './Header.scss';
 
-// Create a Header function returning the HTML of header block
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
 
-// Add a default export statement for Header component to use it in the other files
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">About</a>
+        <a className="navigation__link" href="#services">Services</a>
+        <a className="navigation__link" href="#contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
