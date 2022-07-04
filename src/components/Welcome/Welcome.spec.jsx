4bbd17c34094216cffdbd83342a05361eb52f863
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-07-04 12:24:11 +0300

    fixed

--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import Welcome from './Welcome/Welcome';
+import Welcome from './Welcome';
 
 describe('Welcome component', () => {
   it('should contain Sticky Header', () => {
