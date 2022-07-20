4e7dec7787ef458cb2118bf223095a65a15e09f9
Aliud13 <lidiamishchuk@gmail.com>
2022-07-20 16:07:53 +0100

    solution

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
+import './App.css';
 
 function App() {
   return (
