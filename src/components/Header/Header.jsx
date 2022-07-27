3f530c4d2570d70fcff9d578395335b0453288bb
Denys Korotkoruchko <denenergii@gmail.com>
2022-07-27 17:15:21 +0300

    delete <></> and moved style

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,10 +3,8 @@ import './Header.css';
 import { Navigation } from '../Navigation/Navigation';
 
 export const Header = () => (
-  <>
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <Navigation />
-    </header>
-  </>
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
 );
