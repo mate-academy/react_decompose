d469f60bf10b684ee1152053b2db64845447d581
Oleg Vetrov <oleg94038@gmail.com>
2022-07-27 17:09:13 +0300

    Changed media-queries

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <main className="app">
       <Welcome />
-      <Header />
+      <Header siteName="Site Name!" />
       <Article />
     </main>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
 
-const Header = () => (
+const Header = props => (
   <header className="header">
-    <h1 className="header__title">Site Name</h1>
+    <h1 className="header__title">{props.siteName}</h1>
     <Navigation />
   </header>
 );
