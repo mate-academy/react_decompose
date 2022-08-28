75d43b15272081ed4f309b85fd0f1db779491263
Nikita Havryliuk <chinter.games1@gmail.com>
2022-08-28 20:14:40 +0300

    added header

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
     <main className="app">
-      <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>
-      <header className="header">
-        <h1 className="header__title">Site Name</h1>
-        <nav className="navigation">
-          <a className="navigation__link" href="#about">About</a>
-          <a className="navigation__link" href="#services">Services</a>
-          <a className="navigation__link" href="#contact">Contact</a>
-        </nav>
-      </header>
+      <Welcome />
+      <Header />
       <article className="article">
         <h1 className="article__title">Headline</h1>
         <p className="article__paragraph">
