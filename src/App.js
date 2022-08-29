36f27ff7c23f018cde944fd358a7b3d38d02c2cb
Sasha Kolomiiets <kinbean444@gmail.com>
2022-08-29 17:07:42 +0300

    fixed funcs

--- src/App.js
+++ src/App.js
@@ -4,14 +4,12 @@ import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import './Header.css';
 import Nav from '../Nav/Nav';
 
-function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <Nav />
-    </header>
-  );
-}
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Nav />
+  </header>
+);
 
 export default Header;
