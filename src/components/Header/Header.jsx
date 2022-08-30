b96fc8b3e914912cac502bbbcb500796e23b89ae
Andriy Kachalkin <kachalkin1306@gmail.com>
2022-08-30 17:44:53 +0300

    fixed NavigationCss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
-import '../Navigation/Navigation.css';
 
-function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <Navigation />
-    </header>
-  );
-}
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
 
 export default Header;
