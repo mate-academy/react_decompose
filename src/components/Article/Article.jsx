39635050dbaff23dd8d55fd87f1bf6c7446f7b62
Ganna Krainiuk <anya.krainiuk@gmail.com>
2022-09-05 12:52:59 +0300

    classes were added and fixed styles

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Article.css';
 
 const Article = () => (
-  <article>
+  <article className="article">
     <h1 className="article__title">Headline</h1>
     <p className="article__paragraph">
       In elementum lorem eget est euismod ornare. Phasellus sit amet
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 const Header = () => (
-  <header>
+  <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
   </header>
