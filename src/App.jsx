8db0f9b9b44a64f7f604bc993a1686661c75319b
Andrii Dunets <davbiznesman@gmail.com>
2022-09-06 08:51:57 +0300

    welcome component done

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
     <main className="app">
-      <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>
+      <Welcome />
       <Header />
       <Article />
     </main>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,3 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
 import React from 'react';
 import './Header.css';
 
