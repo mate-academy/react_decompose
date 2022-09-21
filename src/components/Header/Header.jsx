169f7e6db4202eec997777b9ea884dc38f1c8926
Serhii Vovk <gear.fq@gmail.com>
2022-09-21 17:31:23 +0300

    React decompose is done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
 // import the React library here
 // import a css file containig article styles
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
 
 // Create a Header function returning the HTML of header block
 
