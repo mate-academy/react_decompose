7fbf5df6b512b90c4d0c91855a7b08c0a55bd419
Kamil Witek <kamilprogramuje@gmail.com>
2022-09-21 20:32:48 +0200

    App changed to arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
