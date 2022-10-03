bbb3a56aab7f7721a59bdb5426b6672304e5c3ac
Ihor Andrianov <andrianoia@gmail.com>
2022-10-03 16:30:37 +0300

    fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.css';
-import './components/Welcome/Welcome.css';
-import './components/Header/Header.css';
-import './components/Article/Article.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
