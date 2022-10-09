7517ee160da6221e7c326a49d508a69c9706a292
Andrew Skoryk <andrewtimelessone@gmail.com>
2022-10-09 19:50:02 +0300

    feat: all componets were added

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from 'react';
 import './App.css';
 
-import './components/Article'
-// Move each BEM block to a separate component (file) and import them here
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
     <main className="app">
-      <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>
-      <header className="header">
-        <h1 className="header__title">Site Name</h1>
-        <nav className="navigation">
-          <a className="navigation__link" href="#about">About</a>
-          <a className="navigation__link" href="#services">Services</a>
-          <a className="navigation__link" href="#contact">Contact</a>
-        </nav>
-      </header>
+      <Welcome />
+      <Header />
       <Article />
     </main>
   );
