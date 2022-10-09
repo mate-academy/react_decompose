39af9c4ccae981dca4b2ab6eb80e67a70d8d1aa8
Ilya Maksymov <ivashthestampede01@gmail.com>
2022-10-09 19:18:16 +0300

    Split app into components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
-// import the React library here
-// import a css file containig article styles
+import React from 'react';
+import './Header.css';
 
-// Create a Header function returning the HTML of header block
+import Nav from '../Nav/Nav';
 
-// Add a default export statement for Header component to use it in the other files
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Nav />
+  </header>
+);
+
+export default Header;
