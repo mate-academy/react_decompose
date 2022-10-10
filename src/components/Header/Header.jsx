79c7400a8086bba392d2cc3e99966acda0095fc2
Vlad Harkusha <sivari888@gmail.com>
2022-10-10 19:25:53 +0300

    app component decomposed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
-// import the React library here
-// import a css file containig article styles
 
-// Create a Header function returning the HTML of header block
+import React from 'react';
+import { Navigation } from '../Navigation/Navigation';
+import './Header.css';
 
-// Add a default export statement for Header component to use it in the other files
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+
+    <Navigation />
+  </header>
+);
