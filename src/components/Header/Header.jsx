2360db1cf045035bf7d9112d1a6c8a643491d941
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-10-11 10:15:04 +0300

    fixes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,18 +5,17 @@
 
 // Add a default export statement for Header component to use it in the other files
 import React from 'react';
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <nav className="navigation">
-        <a className="navigation__link" href="#about">About</a>
-        <a className="navigation__link" href="#services">Services</a>
-        <a className="navigation__link" href="#contact">Contact</a>
-      </nav>
-    </header>
+    <>
+      <header className="header">
+        <h1 className="header__title">Site Name</h1>
+        <Navigation />
+      </header>
+    </>
   );
 }
 
