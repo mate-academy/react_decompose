ba9c8a2e0c94012cb1e3230b8631d4c6a46fd764
Roman Starovoit <rstarovoit@gmail.com>
2022-10-13 15:28:24 +0300

    Navigation moved 2 the separate component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,11 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
-
 import React from 'react';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    <nav className="navigation">
-      <a className="navigation__link" href="#about">About</a>
-      <a className="navigation__link" href="#services">Services</a>
-      <a className="navigation__link" href="#contact">Contact</a>
-    </nav>
+    <Navigation />
   </header>
 );
 
