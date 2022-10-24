2020cf32b35e7b99c9deef0b31ff713945856247
Maksym Storozhuk <hitthemax111@gmail.com>
2022-10-24 20:26:41 +0300

    Added few improvements to code

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,14 @@ import './Header.css';
 function Header() {
   return (
     <header className="header">
-      <h1 className="header__title">Site Name</h1>
+      <h1 className="header__title">
+        Site Name
+      </h1>
       <nav className="navigation">
         <a className="navigation__link" href="#about">About</a>
+
         <a className="navigation__link" href="#services">Services</a>
+
         <a className="navigation__link" href="#contact">Contact</a>
       </nav>
     </header>
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -5,7 +5,9 @@ import './Welcome.css';
 function Welcome() {
   return (
     <section className="welcome">
-      <span className="welcome__text">Sticky Header!</span>
+      <span className="welcome__text">
+        Sticky Header!
+      </span>
     </section>
   );
 }
