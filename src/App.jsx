4cb25c9ebd2e47e295969a4c463f4105262c8ff7
Yevhenii Haidukov <jeniagaidukov@gmail.com>
2022-11-02 20:20:59 +0200

    react decompose done v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import Article from './components/Article/Article';
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 function App() {
   return (
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      <Navigation />
     </header>
   );
 }
