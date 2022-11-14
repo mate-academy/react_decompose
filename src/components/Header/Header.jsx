19bc81e4e1b12e7e4ba47057c29915f18bd688c4
postoronnimV <vadimkrvts03@gmail.com>
2022-11-14 15:58:56 +0200

    added solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,19 @@
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
+function Header() {
+  return (
+    <div>
+      <header className="header">
+        <h1 className="header__title">Site Name</h1>
+        <Navigation />
+      </header>
+    </div>
+  );
+}
+
+export default Header;
 // import the React library here
 // import a css file containig article styles
 
