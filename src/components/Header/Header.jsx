0080d2f1ec2b328038965dea46692191629aac53
Bohdan Stepanets <number1xy@gmail.com>
2022-11-14 23:42:54 +0200

    Make React decompose

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { Navigation } from './Navigation/Navigation';
+import Navigation from './Navigation/Navigation';
 import './Header.css';
 
-export const Header = () => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
   </header>
 );
+
+export default Header;
