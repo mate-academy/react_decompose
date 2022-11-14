1903712024ed2b03896e03cd854fd8f35d946122
Bohdan Stepanets <number1xy@gmail.com>
2022-11-14 23:33:44 +0200

    Make React decompose

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 
 import { Welcome } from './components/Welcome/Welcome';
 import { Header } from './components/Header/Header';
-import { Article } from './components/Article/Article';
+import Article from './components/Article/Article';
 
 function App() {
   return (
