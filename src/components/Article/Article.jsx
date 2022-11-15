92c7ce31f74db4760699594e4a58e3de407b078f
Anna Shapovalovaemail <3809.kasabian.00@gmail.com>
2022-11-15 11:57:52 +0200

    v04 - h1 came back

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -4,7 +4,7 @@ import './Article.css';
 function Article() {
   return (
     <article className="article">
-      <h2 className="article__title">Headline</h2>
+      <h1 className="article__title">Headline</h1>
 
       <p className="article__paragraph">
         In elementum lorem eget est euismod ornare. Phasellus sit amet
