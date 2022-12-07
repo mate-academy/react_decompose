4a24e380ad5f03f2d29d27ac362e54c63f51a23e
Vlad Kurachenko <x0674016244@gmail.com>
2022-12-07 16:44:40 +0200

    Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -4,3 +4,71 @@
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
+import React from 'react';
+import './Article.css';
+
+function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod  ornare. Phasellus sit amet
+        pellentesque mauris. Aliquam quis  malesuada ex. Nullam eu aliquam
+        nibh. Mauris molestie, urna accumsan  ornare semper, augue nibh
+        posuere lorem, vitae feugiat sem magna  eget massa. Vivamus quis
+        tincidunt dolor. Fusce efficitur, orci  non vestibulum consequat,
+        lectus turpis bibendum odio, in  efficitur leo felis sed justo. Fusce
+        commodo iaculis orci, quis imperdiet  urna. Sed mollis facilisis lacus
+        non condimentum. Nunc efficitur massa   non neque elementum semper.
+        Vestibulum lorem arcu, tincidunt in   quam et, feugiat venenatis augue.
+        Donec sed tincidunt tellus, a  facilisis  magna. Proin sit amet  viverra
+        nibh, bibendum gravida felis. Vivamus   ut nunc id mauris posuere
+        pellentesque. Praesent tincidunt id   odio id feugiat.
+      </p>
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor,  tincidunt ut bibenduvitae,
+        fermentum ac quam. Aliquam pretium  tristique nibh quiiaculis. In et
+        cursus ex, eu aliquet ex. Proin  facilisis lacus sit amesapien
+        ultrices, ut vehicula arcu lobortis.  Vivamus molliipsum ut
+        hendrerit molestie. Morbi lacinia,  sapien eu dictudignissim, tellus
+        tortor congue magna, sit amet bibendum  libero nisi imassa.
+      </p>
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel loborti  eu, fringilla siamet dolor.
+        Cras congue, massa nec sagittis mollis,  dui feliultrices magna,
+        tincidunt finibus lorem quam in sem.  Morbi odio turpispulvinar sit
+        amet vulputate quis, ultricies eu  libero. Donec amaximus neque, nec
+        maximus nibh. Morbi rhoncus convallis  urna, accumsaporta lorem
+        hendrerit in. Cras eget nisl dui. Morbi  faucibus niseget ipsum
+        semper vulputate. Mauris nec tincidunt  lectus. Aenean ami consequat
+        velit dignissim consectetur. Fusce  placerat ac ipsum aeleifend.
+        Aenean quis faucibus ex.
+      </p>
+      <p className="article__paragraph">
+        Cras egestas tempor nibh, a fermentum  lorem sollicitudinon. Nulla
+        facilisi. In at elit id leo tristique  condimentum. Doneat est
+        nulla. Mauris egestas magna ut laoreet  pretium. Seultrices suscipit
+        vestibulum. Lorem ipsum dolor sit amet,  consectetuadipiscing elit.
+        Fusce id sapien eros. Vivamus viverra  ultricies gravidaNam urna
+        nibh, blandit a vulputate at, vehicula  non nulla. Aeneaut nulla
+        leo. Praesent in ullamcorper est.
+      </p>
+      <p className="article__paragraph">
+        Pellentesque habitant morbi tristique  senectus et netuet malesuada
+        fames ac turpis egestas. Phasellus  bibendum nec arcu elobortis.
+        Nam convallis faucibus ante sed porta.  Nullam uconvallis elit, quis
+        venenatis nunc. Curabitur sed sem eget  velit condimenturutrum in et
+        orci. Nunc non suscipit eros.  Suspendisse porta sem vejusto commodo
+        dictum. Aliquam erat ligula, fringilla  nec suscipit sedporta vitae
+        turpis. Vestibulum rhoncus placerat  nulla vitasuscipit. Curabitur
+        consectetur ex ut odio tristique  vehicula. Ut ligultortor,
+        tincidunt quis sodales vitae, ornare a  turpis. Proin siamet finibus
+        enim. Fusce tempus a neque vitae  tempor. Aenean rutrumlibero
+        iaculis interdum vulputate, dui eros  vehicula nisi, ainterdum enim
+        lacus eu diam.
+      </p>
+    </article>
+  );
+}
+
+export default Article;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,3 +4,16 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+    </header>
+  );
+}
+
+export default Header;
