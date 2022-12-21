f8767b51b23e190a0d83b579a83e88b90f88915a
Vladimir Arkanov <vladimirarkanov@icloud.com>
2022-12-21 17:49:46 +0200

    first commit, all jsx elements were splited to files and connectd

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,16 @@
-// import the React library here
-// import a css file containig article styles
+import React from 'react';
+
+import './Welcome.css';
+
+function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
+
+export default Welcome;
 
 // Create a Welcome function returning the HTML of welcome block
 
