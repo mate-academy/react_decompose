46993367323817f89250aec84fbebbda685a5bd5
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-22 09:01:23 +0200

    moved Welcome component into separate file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
 function App() {
   return (
     <main className="app">
-      <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>
+      <Welcome />
       <Header />
       <Article />
     </main>
