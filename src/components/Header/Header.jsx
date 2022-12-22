a414711a06c90f00f1e4c98756ed3543d7b19afa
Dima Kolesnyk <kolesnyk.dmytro.victorovych@gmail.com>
2022-12-22 14:22:39 +0200

    code separated by components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,17 @@
-// import the React library here
-// import a css file containig article styles
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
