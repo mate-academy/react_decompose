9901c0f407137bdd07b1aa4f802863df906fd540
Kareem Ashkar <kareemalachkar@gmail.com>
2022-12-27 00:50:01 +0300

    final

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,17 @@
 // import the React library here
+import React from 'react';
 // import a css file containig article styles
-
+import './Welcome.css';
 // Create a Welcome function returning the HTML of welcome block
-
+function Welcome() {
+  return (
+    <main className="app">
+      <section className="welcome">
+        <span className="welcome__text">Sticky Header!</span>
+      </section>
+    </main>
+  );
+}
 // Add a default export statement for Welcome component to use it in the other files
+
+export default Welcome;
