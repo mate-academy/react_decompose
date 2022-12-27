d6684882051a6b8308fd8a60967547e65e5551ae
Ala El Achkar <iamalaaashkar97@gmail.com>
2022-12-27 20:10:31 +0300

    converted to arrow function

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+const App = () => {
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>;
+};
 
 export default App;
