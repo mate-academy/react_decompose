d62efed1890339a43d0977ca7454eae56f93ae08
Kavster92 <kav.athi1992@googlemail.com>
2022-12-28 12:52:53 +0100

    created arrow functions for all regular functions

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,11 @@ import React from 'react';
 import Nav from '../Navigation/Navigation';
 import './Header.css';
 
-function Header() {
-  return (
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <Nav />
-    </header>
-  );
-}
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Nav />
+  </header>
+);
 
 export default Header;
