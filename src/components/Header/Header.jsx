7b25e29656aa60fb552bca6c2fe2fc9cd805b130
Vlad Kurachenko <x0674016244@gmail.com>
2023-01-04 12:26:00 +0200

    Solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,13 @@
 
 import React from 'react';
-import Nav from '../Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <Nav />
+      <Navigation />
     </header>
   );
 }
