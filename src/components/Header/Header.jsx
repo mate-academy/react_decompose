c9acc6bfba06aae5fb6f9105c8d1aa1c4016b4ce
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2023-01-04 14:51:54 +0100

    lint error2

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { Navigation } from '../Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => (
   <header className="header">
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Navigation.css';
 
-export const Navigation = () => (
+const Navigation = () => (
   <nav className="navigation">
-    <a className="navigation__link" href="#about">About</a>
-    <a className="navigation__link" href="#services">Services</a>
-    <a className="navigation__link" href="#contact">Contact</a>
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
   </nav>
 );
+
+export default Navigation;
