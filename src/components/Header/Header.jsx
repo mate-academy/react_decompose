7cb51110d47db63674cc4d40dce96ae17f53db8e
OleksandrOse <oselskyi3@gmail.com>
2023-01-23 17:29:11 +0200

    fix remarks

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,12 +3,10 @@ import './Header.css';
 import Navigation from '../Navigation/Navigation';
 
 const Header = () => (
-  <>
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <Navigation />
-    </header>
-  </>
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
 );
 
 export default Header;
