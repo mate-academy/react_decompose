abe1dbf249d5f203dab2e9287eb9c5244c9db01a
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-23 15:05:49 +0200

    removed redundant braces and change named export

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/Welcome/Welcome';
-import Header from './components/Header/Header';
-import Article from './components/Article/Article';
+import { Welcome } from './components/Welcome/Welcome';
+import { Header } from './components/Header/Header';
+import { Article } from './components/Article/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
-
-export default App;
+export const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './Welcome.css';
 
-const Welcome = () => (
-  (
-    <section className="welcome">
-      <span className="welcome__text">Sticky Header!</span>
-    </section>
-  )
+export const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
 );
-
-export default Welcome;
