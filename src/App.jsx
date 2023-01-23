eb6380a5fc4a577e286650f57c63bb1ea59c2b32
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-23 17:21:09 +0200

    upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
