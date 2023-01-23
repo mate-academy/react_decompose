8c30678279e45f5b0bff1a619c4da89814330fa9
bart6man <igor.musson.55@gmail.com>
2023-01-23 12:50:42 +0200

    Troubleshooting

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Welcome.css';
 
-export const Welcome = () => {
+export const Welcome = () => (
   <section className="welcome">
     <span className="welcome__text">Sticky Header!</span>
-  </section>;
-};
+  </section>
+);
