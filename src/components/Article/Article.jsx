664fbd3e5ad96ea77ebc1099a37c4ccf694539d5
Yurii Demkiv <demkiv123@gmail.com>
2023-01-23 11:22:09 +0200

    Added default export

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -4,7 +4,7 @@ import './Article.css';
 
 // Add a default export statement for Article component to use it in the other files
 
-export const Article = () => (
+const Article = () => (
   <article className="article">
     <h1 className="article__title">Headline</h1>
     <p className="article__paragraph">
@@ -65,3 +65,5 @@ export const Article = () => (
     </p>
   </article>
 );
+
+export default Article;
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,8 +7,10 @@ import './Welcome.css';
 
 // Add a default export statement for Welcome component to use it in the other files
 
-export const Welcome = () => (
+const Welcome = () => (
   <section className="welcome">
     <span className="welcome__text">Sticky Header!</span>
   </section>
 );
+
+export default Welcome;
