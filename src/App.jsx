ce2187eef76b919772f20179ba69c6c6fd71de15
Tetiana.Boiar <taniavasylchyk@gmail.com>
2023-01-23 17:02:20 +0200

    got rid of unnecessary empty lines

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
-
 import './App.css';
 
 function App() {
