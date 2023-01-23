97a6b65cdc6a58fbc5bfa056bd680fc8ba9bfa8d
Ihor Mariash <imariash86@gmail.com>
2023-01-23 22:27:20 +0200

    arrow function and named export were used, comments were removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
-import Welcome from './components/Welcome/Welcome';
-import Header from './components/Header/Header';
-import Article from './components/Article/Article';
+import { Welcome } from './components/Welcome/Welcome';
+import { Header } from './components/Header/Header';
+import { Article } from './components/Article/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
-
-export default App;
+export const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
