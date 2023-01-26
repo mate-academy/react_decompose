17c97642dd6eeb465f15510c1f1b04af57289d85
Vadivel-Babu <vadivelbabu1996@gmail.com>
2023-01-26 11:43:19 +0200

    solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,14 @@
 // import the React library here
-import React from 'react';
+import React from "react";
+import Navigation from "../Navigation/Navigation";
 // import a css file containig article styles
-import './Header.css';
+import "./Header.css";
 // Create a Header function returning the HTML of header block
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <nav className="navigation">
-        <a className="navigation__link" href="#about">
-          About
-        </a>
-        <a className="navigation__link" href="#services">
-          Services
-        </a>
-        <a className="navigation__link" href="#contact">
-          Contact
-        </a>
-      </nav>
+      <Navigation />
     </header>
   );
 }
