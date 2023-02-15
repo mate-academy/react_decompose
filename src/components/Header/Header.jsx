7fe7d85eab9f8f4cf96ad5fa0f3933a468da157c
Peti51 <galpeter31@gmail.com>
2023-02-15 10:50:45 +0100

    commit message

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,5 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
 import React from 'react';
-import './Header.css';
+import './header.css';
 
 const Header = () => (
   <header className="header">
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,12 +1,5 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
-
 import React from 'react';
-import './Welcome.css';
+import './welcome.css';
 
 const Welcome = () => (
   <section className="welcome">
