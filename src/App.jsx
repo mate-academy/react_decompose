7f8ea5434b7b218153d1634ea8c5bcd9762ad8ea
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-22 14:22:37 +0200

    Default comments removed as requested

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
