7131b15b305f92edc120301d7b4c46b1ced5d7ed
Sergei Nikolaienko <serjexdeath@gmail.com>
2023-02-22 09:33:21 +0200

    Redundant fragments removed

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -8,11 +8,9 @@ import React from 'react';
 import './Welcome.css';
 
 const Welcome = () => (
-  <>
     <section className="welcome">
       <span className="welcome__text">Sticky Header!</span>
     </section>
-  </>
 );
 
 export default Welcome;
