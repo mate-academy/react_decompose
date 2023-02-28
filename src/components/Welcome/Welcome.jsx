86dc3807a3091572c8ed43dc6422ff663f3c9891
Kate Matviiv <katyaya56@gmail.com>
2023-02-28 19:05:17 +0200

    Solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,3 +4,14 @@
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
+import React from 'react';
+import '../../App.css';
+import './Welcome.css';
+
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
+
+export default Welcome;
