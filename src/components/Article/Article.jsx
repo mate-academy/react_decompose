62abbf3ae4afa19bcefe46da9cdb634522f5862e
Kate Matviiv <katyaya56@gmail.com>
2023-02-28 21:30:27 +0200

    Silution 4

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -5,7 +5,6 @@
 
 // Add a default export statement for Article component to use it in the other files
 import React from 'react';
-import '../../App.css';
 import './Article.css';
 
 const Article = () => (
