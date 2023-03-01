e38d077b726a2817da70c1d1e027abae2e2fbb9e
Olena Hapon <olenagapon90gmail.com>
2023-03-01 09:59:45 +0100

    Maked changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,6 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
 import React from 'react';
 import './Header.css';
-import Navigation from '../Navagation/Navigation';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
