a287c4cada7662f002117f3e1ade3406869f4c26
Kate <novak5683@gmail.com>
2023-03-01 13:45:52 +0100

    comments removed

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,10 +1,3 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create an Article function returning the HTML of article block
-
-// Add a default export statement for Article component to use it in the other files
-
 import React from 'react';
 import './Article.css';
 
