add9b5e947dc7e5e1ececa8378621abe5a70fae8
ctk12 <chetankatwa12@gmail.com>
2023-03-02 17:47:46 +0530

    Added React Decompose

--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import Welcome from './Welcome';
+import { Welcome } from './Welcome';
 
 describe('Welcome component', () => {
   beforeEach(() => {
