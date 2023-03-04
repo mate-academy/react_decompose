e3cf8c844e90d51c2d86612f1163c0281f2e7fee
Jaxxxon78 <jaxxxon78@ukr.net>
2023-03-04 14:03:56 +0200

    once again

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
