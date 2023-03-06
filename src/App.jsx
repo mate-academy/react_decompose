d70bb16cfd370c7fe339313843e03c2c6788630f
christian m wolf <108151633+devWizHub@users.noreply.github.com>
2023-03-06 12:28:35 +0100

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
 import { Welcome } from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
 function App() {
   return (
-    <>
-      <main className="app" />
+    <main className="app">
       <Welcome />
       <Header />
-
       <Article />
-    </>
-
+    </main>
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
-// import a css file containig article styles
 import './Navigation.css';
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
 
 export const Navigation = () => (
   <nav className="navigation">
