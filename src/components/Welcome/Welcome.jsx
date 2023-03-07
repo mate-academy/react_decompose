43d253c754c512dd158ad9f6346ca58aa6c2ec30
Anna Akhalmosulishvili <akhala33@gmail.com>
2023-03-07 19:16:45 +0200

    solution fixed

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,3 @@
-// import the React library here
-// import a css file containig article styles
-
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
-
 import React from 'react';
 import './Welcome.css';
 
