88f8d89a55b3f9751e0ae01488f1c079d3376ad4
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-03-09 18:09:30 +0200

    reactDecompose task was implement

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,12 @@
-// import the React library here
-// import a css file containig article styles
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation';
 
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
