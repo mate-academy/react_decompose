61fdf57532a7a22caa49d7fac67c1041391006c4
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-03-09 19:07:32 +0200

    Solution react_decompose

---
+++ src/components/Header/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">About</a>
+    <a className="navigation__link" href="#services">Services</a>
+    <a className="navigation__link" href="#contact">Contact</a>
+  </nav>
+);
+
+export default Navigation;
