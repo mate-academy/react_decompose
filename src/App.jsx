97973aa7f92d57afdacfa2b3ff1be187e66a7407
Nazarov Oleksiy <112445970+nazarovoleksiy@users.noreply.github.com>
2023-03-09 19:28:02 +0200

    Fixed CSS files

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,6 @@ import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
-import './components/Welcome/Welcome.css';
-import './components/Header/Header.css';
-import './components/Header/Navigation/Navigation.css';
-import './components/Article/Article.css';
-
 function App() {
   return (
     <main className="app">
