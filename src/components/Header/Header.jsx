d567d0829df125cd63684523da03b9343f8d9c1d
kulakostas <92936150+kulakostas@users.noreply.github.com>
2023-03-10 10:57:23 +0200

    fixed lint errors

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import './Header.css';
-import Navigation from "../Navigation/Navigation";
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
@@ -8,7 +8,7 @@ function Header() {
       <h1 className="header__title">Site Name</h1>
       <Navigation />
     </header>
-  )
+  );
 }
 
 export default Header;
