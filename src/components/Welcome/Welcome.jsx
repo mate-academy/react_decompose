2c54af0332ccc51a781177c831d928516c9ced52
Oleh Chernenko <oleh.chernenko97@gmail.com>
2023-03-11 20:02:17 +0200

    bugs fixed

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import './Welcome.css';
 
 // Move each BEM block to a separate component (file) and import them here
-export const Welcome = () => (
+const Welcome = () => (
   <section className="welcome">
     <span className="welcome__text">Sticky Header!</span>
   </section>
 );
+
+export default Welcome;
