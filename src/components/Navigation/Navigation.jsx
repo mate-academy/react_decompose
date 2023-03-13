e51702bce7d2f3ad612a7a5e005e7e99ec4604df
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-03-13 10:35:54 +0100

    save changes

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Navigation.css';
+
+export const Navigation = () => (
+  <>
+    <a className="navigation__link" href="#about">About</a>
+    <a className="navigation__link" href="#services">Services</a>
+    <a className="navigation__link" href="#contact">Contact</a>
+  </>
+);
