a01af2dcf9220993d66f18cb637c1b48100d984c
Anna <arkushenko98@gmail.com>
2023-03-13 12:03:16 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import './App.css';
-import './components/Welcome/Welcome.css';
-import './components/Header/Header.css';
-import './components/Navigation/Navigation.css';
-import './components/Article/Article.css';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
