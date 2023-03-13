e7d1ccab2779fa197ebb24ce3bd587beec3d02a0
Andrii Kuznetsov <andreykuzmich91@gmail.com>
2023-03-13 14:05:34 +0200

    all fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Article } from './components/Article/Article';
 import { Header } from './components/Header/Header';
-import { Navigation } from './components/Navigation/Navigation';
 import { Welcome } from './components/Welcome/Welcome';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
