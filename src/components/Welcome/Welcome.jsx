08b21f9534a939eaac1921c2da68afcf2bbcd001
Stanislav Vdovychenko <vdovychenkostas@gmail.com>
2023-03-16 20:07:46 +0200

    error fixes

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,7 @@
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
-import React from "react";
+import React from 'react';
 import './Welcome.css';
 
 const Welcome = () => (
