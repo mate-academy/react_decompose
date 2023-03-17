1295c4ba1f54d06629fa99a62c87f93860a3904d
haduigon <haduigon@gmail.com>
2023-03-17 20:18:25 +0200

    second commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,12 @@
-import React from "react";
-import Navi from '../Navi/Navi';
-import '../Header/Header.css'
-
+import React from 'react';
+import Navigation from '../Navigation/Navigation';
+import './Header.css';
 
 const Header = () => (
   <header className="header">
-  <h1 className="header__title">Site Name</h1>
-  <Navi />
-</header>
-)
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
 
 export default Header;
-
