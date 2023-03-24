66a281d54e0b196e52f6d7a357b2588348e3f44b
GUSILLUS <zebela2002gmail.com>
2023-03-24 22:57:53 +0200

    push Navigation component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
-import Navigation from '../Navigation/Navigation';
 import './Header.css';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">About</a>
+    <a className="navigation__link" href="#services">Services</a>
+    <a className="navigation__link" href="#contact">Contact</a>
+  </nav>
+);
 
 const Header = () => (
   <header className="header">
