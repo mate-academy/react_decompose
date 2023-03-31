919cb1aae9947ff9d9151ad5e46d86302b64fc3e
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-03-31 22:53:56 +0300

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
+
 import './App.css';
 
 function App() {
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+
 import Navigation from '../Navigation/Navigation';
+
 import './Header.css';
 
 const Header = () => (
