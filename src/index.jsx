69dbc194db18732ef237b138f962b88776d81943
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-04-07 22:17:01 +0300

    hello react world!

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Welcome from './components/Welcome/Welcome';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <App />
+    <Welcome />
+    <Header />
+    <Article />
+  </div>,
   document.getElementById('root'),
 );
