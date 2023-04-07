a9a8fd0feb0034e38011c4954dc55d2199abe003
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-04-07 12:26:36 +0300

    improved my codestyle

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 import './App.css';
 
-function App() {
-  return (
-    <main className="app">
-      <Welcome />
-      <Header />
-      <Article />
-    </main>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
