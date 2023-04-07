a6af4ff1e7149a291aa154cc33341f4eaeae22a9
Aleksandr <alexnovaklawyer@gmail.com>
2023-04-07 15:30:37 +0300

    unnecessary text removed

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -10,8 +10,3 @@ function Welcome() {
 }
 
 export default Welcome;
-// import a css file containig article styles
-
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
