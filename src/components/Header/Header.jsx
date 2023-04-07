78e344c9eaa4b8a39c7282208391f8097a927caa
Aleksandr <alexnovaklawyer@gmail.com>
2023-04-07 16:29:36 +0300

    identations added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+
       <Navigation />
     </header>
   );
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,9 @@ import './Welcome.css';
 function Welcome() {
   return (
     <section className="welcome">
-      <span className="welcome__text">Sticky Header!</span>
+      <span className="welcome__text">
+        Sticky Header!
+      </span>
     </section>
   );
 }
