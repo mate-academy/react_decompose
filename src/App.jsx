e90a979ebca029157d099c907848e7d74df907f5
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-04-09 02:33:08 +0300

    deleted comments in the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 import Welcome from './components/Welcome/Welcome';
