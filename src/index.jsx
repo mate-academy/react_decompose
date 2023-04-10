c8fcf123e4212672eb0de91ee388867efc5e2d21
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-04-10 12:05:24 +0300

    fied hierarchy

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Welcome from './components/Welcome/Welcome';
-import Article from './components/Article/Article';
-import Header from './components/Header/Header';
 
 ReactDOM.render(
   <div>
     <App />
-    <Welcome />
-    <Header />
-    <Article />
   </div>,
   document.getElementById('root'),
 );
