1cb4918a3a59f5900e60e4f7d5302be81d024f81
kq333 <afugowski@yandex.com>
2023-04-25 21:25:42 +0200

    solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,20 @@
+/* eslint-disable react/react-in-jsx-scope */
 // import the React library here
 // import a css file containig article styles
 
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+
+export const Header = () => (
+  <div>
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">About</a>
+        <a className="navigation__link" href="#services">Services</a>
+        <a className="navigation__link" href="#contact">Contact</a>
+      </nav>
+    </header>
+  </div>
+);
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,6 +1,15 @@
+/* eslint-disable react/react-in-jsx-scope */
 // import the React library here
 // import a css file containig article styles
 
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
+
+export const Welcome = () => (
+  <div>
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  </div>
+);
