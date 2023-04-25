a2a0b4c5a3231c9f5f20f55537f48d751f2f73a7
Daniel <henio09@wp.pl>
2023-04-25 13:15:39 +0200

    pulled navigation from header to separate file

--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { mount } from '@cypress/react';
 import Welcome from './Welcome';
 
-describe.only('Welcome component', () => {
+describe('Welcome component', () => {
   beforeEach(() => {
     mount(<Welcome />);
   });
