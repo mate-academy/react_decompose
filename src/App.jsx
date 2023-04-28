3577187b738e455178b7fac6836eef3b0867a312
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-04-28 06:43:42 +0200

    fixed files structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
-import Header from './components/Header/Header';
-import Welcome from './components/Welcome/Welcome';
+import Header from './components/Navigation/Header/Header';
+import Welcome from './components/Navigation/Welcome/Welcome';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
