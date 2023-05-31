f289d7c4180aaafe09b2165eec28ba16efff8df2
Bohdan <bandurabogdan@gmail.com>
2023-05-31 14:48:10 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
-// Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
