b824735369fff71e05913ad073bcd0b0a3b415f8
Andrii Mashutikov <mashutikov.a.o@gmail.com>
2023-06-05 12:14:20 +0300

    Fixed comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Header.css';
-import Nav from '../Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <Nav />
+      <Navigation />
     </header>
   );
 }
