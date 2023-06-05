546b6dcd81bfa3ead90968ccbd34b5335bec9f97
Vadym Vitruk <vadym_v@ukr.net>
2023-06-05 12:38:11 +0300

    fixed solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,12 +4,10 @@ import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
-    <>
-      <header className="header">
-        <h1 className="header__title">Site Name</h1>
-        <Navigation />
-      </header>
-    </>
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
   );
 }
 
