f1ec7e64435a0e83cfcf035a16d11a67f35e64fb
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-06-05 12:50:12 +0300

    remove tests, fixed code in header and app files

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import './App.css';
 
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Article from './components/Article/Article';
 
 function App() {
   return (
     <main className="app">
       <Welcome />
-      <Header>
-        <Navigation />
-      </Header>
+      <Header />
       <Article />
     </main>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
-const Header = ({ children }) => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    {children}
+    <Navigation />
   </header>
 );
 
