38043325ec1464740f4dfb1a8a8d61ed4b400503
Olha Sheliakina <134736240+OlhaSheliakina@users.noreply.github.com>
2023-06-28 22:31:40 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import './App.css';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
-import Navigation from './components/Navigation/Navigation';
 
 const App = () => (
   <main className="app">
