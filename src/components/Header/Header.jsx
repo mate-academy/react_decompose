83528eafe036a29e71553e2b89f33290f20cb7f7
Ilia Osoben <osoben17@gmail.com>
2023-07-04 12:33:47 +0300

    react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
-// import a css file containig article styles
 
-// Create a Header function returning the HTML of header block
+import Navigations from '../Navigation/Navigation';
+import './Header.css';
 
-// Add a default export statement for Header component to use it in the other files
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigations />
+  </header>
+);
+
+export default Header;
