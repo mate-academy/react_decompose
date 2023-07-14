ca242e91a9cbdb3004340b9f19fa9c5dafe2a255
hnezbrytskyi <Heorhii.Nezbrytskyi@god.dev>
2023-07-14 17:09:14 +0300

    homework is done

--- src/components/Article/Article.spec.jsx
+++ src/components/Article/Article.spec.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { mount } from '@cypress/react';
-import TestdArticle from './Article';
+import Article from './Article';
 
 describe('Article component', () => {
   beforeEach(() => {
-    mount(<TestdArticle />);
+    mount(<Article />);
   });
 
   it('should contain Headline', () => {
