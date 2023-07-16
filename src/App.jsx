c55627bf19e9044d250e2f4d58512b87ca87847a
Serhii <gashysh@protonmail.com>
2023-07-16 13:33:37 +0300

    bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import WelcomeComp from './components/Welcome/Welcome';
 import HeaderComp from './components/Header/Header';
 import ArticleComp from './components/Article/Article';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
 
 const App = () => (
   <main className="app">
