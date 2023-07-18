57d0fc47c25eef5e7b730f65cced996be3e352cd
Dmytro Tymchenko <zav.pro_kr@ukr.net>
2023-07-18 13:36:20 +0300

    DecoTest1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.scss';
 // Move each BEM block to a separate component (file) and import them here
-import { Article } from './components/Article/Article';
-import { Header } from './components/Header/Header';
-import { Welcome } from './components/Welcome/Welcome';
-import { Navigation } from './components/Navigation/Navigation';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import Navigation from './components/Navigation/Navigation';
 
 const App = () => (
   <div>
