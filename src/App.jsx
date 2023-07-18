fc51c83bd7817a53da999a022ab1bcf6baec8c54
Dmytro Tymchenko <zav.pro_kr@ukr.net>
2023-07-18 13:48:38 +0300

    DecoTestFinal

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.scss';
-// Move each BEM block to a separate component (file) and import them here
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
