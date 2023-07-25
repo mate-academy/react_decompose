8f7fd2e8fda97f7db3ceb7325c2000f9dbd8ea5a
iv4ek <ivaskovila@gmail.com>
2023-07-25 13:18:13 +0200

    Update 1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,10 +4,10 @@ const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <nav className="navigation">
-    <a className="navigation__link" href="#about">About</a>
-    <a className="navigation__link" href="#services">Services</a>
-    <a className="navigation__link" href="#contact">Contact</a>
-  </nav>
+      <a className="navigation__link" href="#about">About</a>
+      <a className="navigation__link" href="#services">Services</a>
+      <a className="navigation__link" href="#contact">Contact</a>
+    </nav>
   </header>
 );
 
