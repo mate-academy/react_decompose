631ae65e319ee30615549d6908494f3e89d72d4e
Hanna-Balabukha <anna.aniskovets1992@ukr.net>
2023-07-28 15:22:19 +0300

    'done'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import './App.scss';
-import { Article } from './components/Article/Article';
-// Move each BEM block to a separate component (file) and import them here
+import { Welcome } from './components/Welcome/Welcome';
 import { Header } from './components/Header/Header';
+import { Article } from './components/Article/Article';
 
 export const App = () => (
   <div>
-    <section className="welcome">
-      <span className="welcome__text">Sticky Header!</span>
-    </section>
+    <Welcome />
     <Header />
     <Article />
   </div>
