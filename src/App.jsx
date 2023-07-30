392e5c7528483620ef82e7deee0cb0aa317a0c2a
RomanenkoStud <pavlo.romanenko@nure.ua>
2023-07-30 19:30:50 +0300

    header fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Article from './components/Article/Article';
 
 const App = () => (
   <main className="app">
     <Welcome />
-    <Header>
-      <Navigation />
-    </Header>
+    <Header />
     <Article />
   </main>
 );
