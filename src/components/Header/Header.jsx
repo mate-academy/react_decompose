e2b8007da881c98eff31cae3bdef47898fdec048
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-08-01 14:42:01 +0200

    add task solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,3 +3,12 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+
+import { Navigation } from '../Navigation/Navigation';
+
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
