12a43973bc402daa432fa7691a5b96bc4fbba774
Ksenia Skrebtsova <121960247+KseniaSkr@users.noreply.github.com>
2023-08-04 17:31:19 +0300

    React Decompose

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,4 +1,11 @@
 // import a css file containig article styles
+import './Welcome.css';
+
+export const Welcome = () => (
+  <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+)
 
 // Create a Welcome function returning the HTML of welcome block
 
