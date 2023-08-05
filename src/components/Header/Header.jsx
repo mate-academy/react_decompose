f400e271e107d82fc90a726b74be09071683bf96
Ihor Verbivskyi <i.verba2992@gmail.com>
2023-08-05 14:30:12 +0300

    decompose

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
-// import a css file containig article styles
+import './Header.css';
 
-// Create a Header function returning the HTML of header block
+import { Navigation } from '../Navigation/Navigation';
 
-// Add a default export statement for Header component to use it in the other files
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
