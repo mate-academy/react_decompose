5d72721ea6855a2303f2eacf045466efe10389aa
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-10 21:13:20 +0200

    comments adressed

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,11 @@ import './App.css';
 import { Header } from './components/Header/Header';
 import { Article } from './components/Article/Article';
 import { Welcome } from './components/Welcome/Welcome';
-import { Navigation } from './components/Navigation/Navigation';
-
-// Move each BEM block to a separate component (file) and import them here
 
 const App = () => (
   <main className="app">
     <Welcome />
-    <Header>
-      <Navigation />
-    </Header>
+    <Header />
     <Article />
   </main>
 );
