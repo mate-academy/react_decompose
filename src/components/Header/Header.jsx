6a1165573afac141231565ead07e39b65ff04aae
Anna Shevtsova <aniashevtsova@gmail.com>
2023-08-10 13:46:16 +0200

    all tests pass

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,15 @@
+import './Header.css';
+import { Navigation } from '../Navigation/Navigation';
+
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
+
 // import a css file containig article styles
 
 // Create a Header function returning the HTML of header block
