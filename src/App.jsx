51a78831f94111ef12d088925eb3038c0837c33a
Evelina Moore <luna.rybnikova@gmail.com>
2023-08-16 22:27:12 +0300

    fix task

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import Welcome from './components/Welcome/Welcome';
-import Header from './components/Header/Header';
-import ArticleA from './components/Article/Article';
-import './App.css';
+import React from "react";
+import Welcome from "./components/Welcome/Welcome";
+import Header from "./components/Header/Header";
+import ArticleA from "./components/Article/Article";
+import "./App.css";
 // Move each BEM block to a separate component (file) and import them here
 
 const App = () => (
