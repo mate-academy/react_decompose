6eaafdea9b7ceda7a1fee70a61a94ba1c68268bf
Konrad <konrad.domalewski@gmail.com>
2023-08-17 11:59:58 +0200

    adding review changes

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,9 +1,3 @@
-// import a css file containig article styles
-
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
-import React from 'react';
 import './Welcome.css';
 
 export default function Welcome() {
