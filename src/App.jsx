41f1ebd3bc9de9800b7e754850515c8592b313fd
Daryna Pidlutska <daryna.pidlutska@gmail.com>
2023-08-21 22:02:05 +0200

    fix code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable import/no-named-as-default */
-
 import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,7 @@
-/* eslint-disable import/no-named-as-default */
-
 import './Header.css';
 import Navigation from '../Navigation/Navigation';
 
-export const Header = () => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
