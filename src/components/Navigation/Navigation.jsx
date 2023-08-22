64f353620e4faba823b0b131015f7312fd75cf16
KlymSudakov <sudakov.klym@gmail.com>
2023-08-22 09:16:59 +0300

    add task solution

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+/* eslint linebreak-style: ["error", "windows"] */
+
+import './Navigation.css';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
+  </nav>
+);
+
+export default Navigation;
