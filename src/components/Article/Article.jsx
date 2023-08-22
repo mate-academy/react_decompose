1a9d03db5fe9729c04bb580e4d3310a423e35bf9
AndrMar1939 <advo737@gmail.com>
2023-08-22 10:01:03 +0300

    add solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -2,9 +2,7 @@ import './Article.css';
 
 const Article = () => (
   <article className="article">
-    <h1 className="article__title">
-      Headline
-    </h1>
+    <h1 className="article__title">Headline</h1>
 
     <p className="article__paragraph">
       In elementum lorem eget est euismod ornare. Phasellus sit amet
