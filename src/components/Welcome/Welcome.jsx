83f30347d0189afacceaa37c62b84f510a68a9b2
Artur Shynkaryk <mun9je@gmail.com>
2023-08-22 18:53:14 +0300

    fix

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,14 +4,14 @@
 
 // Add a default export statement for Welcome component to use it in the other files
 
-import './Header.css';
+import './Welcome.css';
 
-function Welcome ( ) {
-    return (
-        <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>  
-    )
+function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
 }
 
 export default Welcome;
