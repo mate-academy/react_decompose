64028bc7428d9b9194f12f48b31547cde05bd2c5
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-08-23 16:57:43 +0300

    fix problem

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,13 +1,11 @@
 import './Navigation.css';
 
 const Navogation = () => (
-  <header className="header">
-    <nav className="navigation">
-      <a className="navigation__link" href="#about">About</a>
-      <a className="navigation__link" href="#services">Services</a>
-      <a className="navigation__link" href="#contact">Contact</a>
-    </nav>
-  </header>
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">About</a>
+    <a className="navigation__link" href="#services">Services</a>
+    <a className="navigation__link" href="#contact">Contact</a>
+  </nav>
 );
 
 export default Navogation;
