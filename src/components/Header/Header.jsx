d1b722d6bd1ac4192eceee61be18315982a69bef
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2023-08-23 16:30:24 +0300

    add component navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,10 @@
 import './Header.css';
+import Navogation from '../Navigation/Navigation';
 
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    <nav className="navigation">
-      <a className="navigation__link" href="#about">About</a>
-      <a className="navigation__link" href="#services">Services</a>
-      <a className="navigation__link" href="#contact">Contact</a>
-    </nav>
+    <Navogation />
   </header>
 );
 
