4a6c73851cbf1187ddb1872505a37e4ef7487a30
Oksana Maruniak <syper.ksenka@gmail.com>
2023-09-01 17:45:34 +0300

    changed Welcome place

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
 
-// Move each BEM block to a separate component (file) and import them here
-
+import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
-import Welcome from './components/Welcome/Welcome';
 
 const App = () => (
   <main className="app">
+    <Welcome />
     <Header />
     <Article />
-    <Welcome />
   </main>
 );
 
