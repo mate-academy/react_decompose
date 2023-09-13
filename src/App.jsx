6f14cfa87aa11d0570425af54867b7b286821430
Vova Zinchenko <zincenkomain17@gmail.com>
2023-09-13 20:25:22 +0300

    overFix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import './App.css';
-import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
 
-function App() {
-  return (
-    <main className="app">
-      <Header />
-      <Welcome />
-      <Article />
-    </main>
-  );
-}
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
