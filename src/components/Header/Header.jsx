414a3ea5403f046a566c0685f01aa8c07e88cf22
Kriss Kozak <misskrissretro@gmail.com>
2023-09-21 09:34:45 +0300

    c:/Users/DELL/Kriss/projects/js_code/react_decompose/src/App.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 // import a css file containig article styles
+
 import './Header.css';
 // Create a Header function returning the HTML of header block
+
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
