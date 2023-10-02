d0fe5e0bf9143f8e431ccd7f0368c9f218acfbcf
Denys <denzzlle171@gmail.com>
2023-10-03 00:03:15 +0300

    add solution react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,5 @@
-import './Header.css';
-import Navigation from '../Navigation/Navigation';
+// import a css file containig article styles
 
-const Header = () => (
-  <header className="header">
-    <h1 className="header__title">Site Name</h1>
-    <Navigation />
-  </header>
-);
+// Create a Header function returning the HTML of header block
 
-export default Header;
+// Add a default export statement for Header component to use it in the other files
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,9 +1,5 @@
-import './Welcome.css';
+// import a css file containig article styles
 
-const Welcome = () => (
-  <section className="welcome">
-    <span className="welcome__text">Sticky Header!</span>
-  </section>
-);
+// Create a Welcome function returning the HTML of welcome block
 
-export default Welcome;
+// Add a default export statement for Welcome component to use it in the other files
