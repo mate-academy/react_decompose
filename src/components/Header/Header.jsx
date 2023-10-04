a500eecc7c7cafd9e85d92cc15149cf2dd9beb73
YevhenKharko <yevhenkharko@gmail.com>
2023-10-04 16:57:33 +0300

    fixed issues with my node

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import './Header.css';
-import '../Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => {
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-      <Navigation />
-</header>
-}
+    <Navigation />
+  </header>;
+};
 
 export default Header;
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => (
     <a className="navigation__link" href="#about">About</a>
     <a className="navigation__link" href="#services">Services</a>
     <a className="navigation__link" href="#contact">Contact</a>
-</nav>
-)
+  </nav>
+);
 
 export default Navigation;
