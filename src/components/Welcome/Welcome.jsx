93793e5e382dba97281330ea59803253576a2f50
Stostyy <anatoliy.shostal@gmail.com>
2023-10-11 12:28:53 +0300

    fix task solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,13 +1,11 @@
-import React from "react";
+import React from 'react';
 
-import "./Welcome.css";
+import './Welcome.css';
 
-const Welcome = () => {
-  return(
-    <section className="welcome">
-      <span className="welcome__text">Sticky Header!</span>
-    </section>
-  )
-}
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
 
 export default Welcome;
