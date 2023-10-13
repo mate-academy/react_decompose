3c2fd568e16a6d41d643e9c5b8ee165552ae9eca
andrew gidrovich <mr.androvich55555@gmail.com>
2023-10-13 17:52:56 +0300

    add task solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,12 @@
 // import a css file containig article styles
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 // Create a Header function returning the HTML of header block
 export const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    <nav className="navigation">
-      <a className="navigation__link" href="#about">About</a>
-      <a className="navigation__link" href="#services">Services</a>
-      <a className="navigation__link" href="#contact">Contact</a>
-    </nav>
+
+    <Navigation />
   </header>
 );
 // Add a default export statement for Header component to use it in the other files
