cf654ab460c08c2e85b10217e011d142a2a26b2b
Sasha Green <aleksa146311@gmail.com>
2023-10-13 15:12:22 +0300

    fix export

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
-import { Navigation } from '../Navigation/Navigation';
+import Navigation from '../Navigation/Navigation';
 
-export const Header = () => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
   </header>
 );
+
+export default Header;
