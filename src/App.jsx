18587ab84e21d4a764765080ab17e238ba22299d
Olena Shapkun <shapkunkh@gmail.com>
2023-10-26 11:59:19 +0300

    react decompose solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
-import './components/Article/Article.css';
 import Welcome from './components/Welcome/Welcome';
-import './components/Welcome/Welcome.css';
 import Header from './components/Header/Header';
-import './components/Header/Header.css';
 
 const App = () => (
   <main className="app">
