f2e4016b156888fb0bfeee23491cb6dbc39eb0dc
bohdanliutyi <liutyibohdan13@gmaail.com>
2023-11-06 17:49:59 +0200

    fixed test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import './App.css';
+
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
-import './App.css';
 // Move each BEM block to a separate component (file) and import them here
 
 const App = () => (
--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,6 +1,5 @@
-// import a css file containig article styles
 import './Article.css';
-// Create an Article function returning the HTML of article block
+
 function Article() {
   return (
     <article className="article">
