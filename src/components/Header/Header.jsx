a6d381094ee4794a45dd64729d09c121b74ec754
Ivan <fanmetalheavy@gmail.com>
2023-11-12 23:03:02 +0200

    added decomposition components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,3 +3,14 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+import MyNavigation from '../Navigation/Navigation';
+import './Header.css';
+
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <MyNavigation />
+  </header>
+);
+
+export default Header;
