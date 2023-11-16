868677480b68fe58ccfcf3cce86356075d2b3229
Viktor Shaparenko <vkshadyafk@gmail.com>
2023-11-16 16:45:07 +0200

    fix mistake

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
-import { Navigation } from '../navigation/Navigation';
+import Navigation from '../navigation/Navigation';
 
-export const Header = () => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
   </header>
 );
+
+export default Header;
--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,9 +1,11 @@
 import './Navigation.css';
 
-export const Navigation = () => (
+const Navigation = () => (
   <nav className="navigation">
     <a className="navigation__link" href="#about">About</a>
     <a className="navigation__link" href="#services">Services</a>
     <a className="navigation__link" href="#contact">Contact</a>
   </nav>
 );
+
+export default Navigation;
