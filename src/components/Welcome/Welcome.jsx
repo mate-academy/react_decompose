25dbf9f1d447ad13a95078becceb7c310eebfa5d
Danylo-Chumakov <danil26032003@gmail.com>
2023-11-23 16:24:18 +0200

    add task solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,13 @@
-// import a css file containig article styles
+// import a css file containing welcome styles
+import React from 'react';
+import './Welcome.css';
 
-// Create a Welcome function returning the HTML of welcome block
+// Create a Welcome function returning the HTML of the welcome block
+export const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
 
-// Add a default export statement for Welcome component to use it in the other files
+// Add a default export statement for the Welcome component to use it in other files
+export default Welcome;
