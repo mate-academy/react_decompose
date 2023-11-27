e69487a0074da62872c07716755de3c2ebfbeff8
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2023-11-27 14:26:22 +0100

    fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
-// import a css file containig article styles
 import './Header.css';
-// Create a Header function returning the HTML of header block
+
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
@@ -12,5 +11,4 @@ const Header = () => (
   </header>
 );
 
-// Add a default export statement for Header component to use it in the other files
 export default Header;
