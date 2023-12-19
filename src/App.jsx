fd691199960da0b54b47250f41976be971f17145
Yuliia Kyrychenko <julina.wow@gmail.com>
2023-12-19 16:43:37 +0200

    vo3

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import './App.css';
-import './components/Welcome/Welcome.css';
-import './components/Header/Header.css';
-import './components/Navigation/Navigation.css';
-import './components/Article/Article.css';
 import { Article } from './components/Article/Article';
 import { Header } from './components/Header/Header';
 import { Welcome } from './components/Welcome/Welcome';
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import { Navigation } from '../Navigation/Navigation';
+import './Header.css';
 
 export const Header = () => (
   <header className="header">
