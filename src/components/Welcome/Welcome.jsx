d071e375ab91fb89b023763ebde728374986938b
Vladorius <lutsyk710@gmail.com>
2023-12-30 16:31:05 +0100

    fix some launch trouble

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,5 @@
 // import a css file containig article styles
-import './Welcome.css'
+import './Welcome.css';
 // Create a Welcome function returning the HTML of welcome block
 const Welcome = () => (
   <section className="welcome">
@@ -7,4 +7,5 @@ const Welcome = () => (
   </section>
 );
 // Add a default export statement for Welcome component to use it in the other files
+
 export default Welcome;
