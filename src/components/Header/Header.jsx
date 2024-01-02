f1e2cad0c76ecbf5f651eb752c513183b079edd2
mrsvolodya <vovamursky@gmail.com>
2024-01-02 18:12:39 +0200

    import-task

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,16 @@
+import './Header.css';
+import Navigation from './navigation/Navigation';
 // import a css file containig article styles
+function Header() {
+  return (
+    <>
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </>
+  );
+}
 
+export default Header;
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,11 @@
-// import a css file containig article styles
+import './Welcome.css';
 
-// Create a Welcome function returning the HTML of welcome block
+function Welcome() {
+  return (
+    <>
+      <span className="welcome__text">Sticky Header!</span>
+    </>
+  );
+}
 
-// Add a default export statement for Welcome component to use it in the other files
+export default Welcome;
