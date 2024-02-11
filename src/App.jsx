d922209299ebc2b4da3627067ae2ee24bf327696
Anastasiia Chaikovska <nastyaasd03mail.ru@gmail.com>
2024-02-11 19:38:59 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,6 @@ import { CreateWelcome } from './components/Welcome/Welcome';
 import { CreateHeader } from './components/Header/Header';
 import { CreateArticle } from './components/Article/Article';
 
-// Move each BEM block to a separate component (file) and import them here
-
 const App = () => (
   <main className="app">
     <CreateWelcome />
