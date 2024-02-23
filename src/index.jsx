e1193524dff737abc241e906a0c8683e88b78aa1
Vladislav <vladislavzavadskij@MBP>
2024-02-23 22:38:28 +0200

    commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,7 @@
-import { createRoot } from 'react-dom/client';
-
+import ReactDOM from 'react-dom';
 import App from './App';
 
-createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.render(
+  <App />,
+  document.getElementById('root'),
+);
