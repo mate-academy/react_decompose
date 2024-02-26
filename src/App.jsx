ffc91785242187c30754436c126bba2feb4afd81
Dmytro Moshkivskyi <dmosh@ukr.net>
2024-02-26 21:56:54 +0200

    react_decompose by Dimoshua

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
-import { Article } from './components/Article';
-import { Header } from './components/Header';
-import { Navigation } from './components/Navigation';
-import { Welcome } from './components/Welcome';
 
-export const App = () => (
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+
+const App = () => (
   <main className="app">
     <Welcome />
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,11 @@
-// import a css file containig article styles
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
-// Create a Header function returning the HTML of header block
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
 
-// Add a default export statement for Header component to use it in the other files
-
-<header className="header">
-  <h1 className="header__title">Site Name</h1>
-
-  <nav className="navigation">
-    <a className="navigation__link" href="#about">
-      About
-    </a>
-
-    <a className="navigation__link" href="#services">
-      Services
-    </a>
-
-    <a className="navigation__link" href="#contact">
-      Contact
-    </a>
-  </nav>
-</header>;
+export default Header;
