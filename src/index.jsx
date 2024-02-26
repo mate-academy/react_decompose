c0bcb1c97439f999ba4b5fcc78d838252a65144c
Maksym <max2112gereta@gmail.com>
2024-02-26 16:12:31 -0500

    second commit

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client';
 
-import Navigation from './components/Navigation/Navigation';
+import Navigation from './App';
 
 createRoot(document.getElementById('root')).render(<Navigation />);
