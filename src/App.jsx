83b430adcb3ac49014b7c87fcc5e9dc994215259
Zubkovskiy Bohdan <zubkovvsbogdan@gmail.com>
2024-03-03 12:11:36 +0200

    fix export

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import './App.css';
 
-import { Welcome } from './components/Welcome/Welcome';
-import { Header } from './components/Header/Header';
-import { Article } from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 const App = () => (
   <main className="app">
