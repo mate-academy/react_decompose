a543ff4449602269fdbab1c90ef83ec6c17fbf85
Елена Карнаух <elenakarnauh@MacBook-Pro-Elena.local>
2024-03-04 19:00:05 +0200

    add changed files

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,14 @@
+import { Navigation } from './Navigation/Navigation';
+import './Header.css';
+
+export const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
+
+export default Header;
 // import a css file containig article styles
 
 // Create a Header function returning the HTML of header block
