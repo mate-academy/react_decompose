a451f60680cacce4d94d65f453ce95c8ea5f4c6a
Vitalii <stage2903@gmail.com>
2024-03-05 15:02:44 +0200

    new solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
 const App = () => (
