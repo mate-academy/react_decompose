baa6a55ac2909ed65f060b80674fa4156d319bdf
Max <gcrazy007@gmail.com>
2024-03-12 17:47:54 +0200

    add task solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,9 +1,9 @@
-import './Welcome.css'
+import './Welcome.css';
 
 export const Welcome = () => (
-<section className="welcome">
-<span className="welcome__text">Sticky Header!</span>
-</section>
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
 );
 
 export default Welcome;
