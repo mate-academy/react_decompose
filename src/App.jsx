56d61e8999da65c64fb0e23827be872936bbcc24
Erik Hulinian <gulinianerik2@gmail.com>
2024-03-18 20:44:52 +0200

    task

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import { Header } from './components/Header/Header';
-import { Article } from './components/Article/Article';
-import { Welcome } from './components/Welcome/Welcome';
-import { Navigation } from './components/Navigation/Navigation';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Navigation from './components/Navigation/Navigation';
 import './App.css';
 
 const App = () => (
