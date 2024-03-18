70c3acfbdd591f95447ef84a10b93984a44defea
Erik Hulinian <gulinianerik2@gmail.com>
2024-03-18 21:09:16 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 import Welcome from './components/Welcome/Welcome';
-import Navigation from './components/Navigation/Navigation';
+
 import './App.css';
 
 const App = () => (
@@ -10,7 +10,6 @@ const App = () => (
     <Welcome />
     <Header />
     <Article />
-    <Navigation />
   </main>
 );
 
