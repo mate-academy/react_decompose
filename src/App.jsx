64ea17dc8ddc17320ff450a645e5913b8cd272d2
Maryna Karpova <mari.artarea@gmail.com>
2024-03-18 15:13:01 +0100

    try 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Welcome from './components/Welcome/Welcome';
 // Move each BEM block to a separate component (file) and import them here
 
@@ -10,7 +9,6 @@ const App = () => (
   <main className="app">
     <Welcome />
     <Header />
-    <Navigation />
     <Article />
   </main>
 );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 // import a css file containig article styles
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 // Create a Header function returning the HTML of header block
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      <Navigation />
     </header>
   );
 }
