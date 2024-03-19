1c0a03e13a38fbc8e1d5e504052d013bbed78afd
Wojciech <weswojciech@gmail.com>
2024-03-19 16:00:05 +0100

    fix demo link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
       <h1 className="header__title">Site Name</h1>
       <Navigation />
     </header>
-  )
+  );
 }
 
 // Add a default export statement for Header component to use it in the other files
