9a94374c493c18eb064def74735ce756bcaaa0b2
Wojciech <weswojciech@gmail.com>
2024-03-20 13:52:04 +0100

    solution

--- src/components/Welcome/Welcome.spec.jsx
+++ src/components/Welcome/Welcome.spec.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { mount } from '@cypress/react18';
+import { mount } from '@cypress/react';
 import TestedWelcome from './Welcome';
 
 describe('Welcome component', () => {
