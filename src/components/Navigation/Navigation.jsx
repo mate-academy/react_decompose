337d3ff2e9de503aff6856d8a358937058184657
Illia Vasulenko <vasulenko.illia@gmail.com>
2024-04-01 22:21:49 +0300

    add readme

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './Navigation.css';
+
 export const Nav = () => {
   return (
     <nav className="navigation">
@@ -14,5 +15,5 @@ export const Nav = () => {
         Contact
       </a>
     </nav>
-  )
-}
+  );
+};
