7a580049e3a2920966daceea013c56761952af9d
ivankolbukh <ivan.kolbukh@vitechteam.com>
2024-04-06 22:40:22 +0300

    react_decompose

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+// Move each BEM block to a separate component (file) and import them here
+
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
+
+export default App;
