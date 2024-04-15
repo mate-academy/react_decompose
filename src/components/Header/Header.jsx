be5ef4e10f9b686c2e6e6ddfacb92e18d9c35105
Greshch <greshchook@gmail.com>
2024-04-15 10:02:42 +0300

    3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 // import a css file containig article styles
+import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
 // Create a Header function returning the HTML of header block
@@ -6,7 +7,7 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <nav className="navigation">
+      {/* <nav className="navigation">
         <a className="navigation__link" href="#about">
           About
         </a>
@@ -16,7 +17,8 @@ function Header() {
         <a className="navigation__link" href="#contact">
           Contact
         </a>
-      </nav>
+      </nav> */}
+      <Navigation />
     </header>
   );
 }
