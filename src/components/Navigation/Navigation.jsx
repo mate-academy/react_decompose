eab1021a5292166e60aff4b55346acf03ca0449c
Nataliia <koshmaknatalia@gmail.com>
2024-04-15 21:41:04 -0600

    solution

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,19 @@
+import './Navigation.css';
+
+export default function Navigation() {
+  return (
+    <>
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">
+          About
+        </a>
+        <a className="navigation__link" href="#services">
+          Services
+        </a>
+        <a className="navigation__link" href="#contact">
+          Contact
+        </a>
+      </nav>
+    </>
+  );
+}
