1dbbd175dbb724ca5e91f224a0fa0ab056f02450
Karina Kovgan <karinakovgan3006@gmail.com>
2024-05-02 20:17:19 +0300

    Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,3 +3,16 @@
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
+import React from 'react';
+import './Article.css';
+
+const Article = () => (
+  <article className="article">
+    <h1 className="article__title">Headline</h1>
+    <p className="article__paragraph">
+      In elementum lorem eget est euismod ornare...
+    </p>
+  </article>
+);
+
+export default Article;
