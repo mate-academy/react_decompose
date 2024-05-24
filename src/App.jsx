945d99bf3a7aac11c36a89699214e5dd242b1f24
Igor Novikov <novikov.igor.20@gmail.com>
2024-05-24 16:12:13 +0300

    fixed bugs with navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
+import Navigation from './components/Navigation/Navigation';
 
 const App = () => (
   <main className="app">
     <Welcome />
     <Header />
+    <Navigation />
     <Article />
   </main>
 );
