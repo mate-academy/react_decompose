db21b81fc6e63e2036c89ae98e1ca7c5bf63f4e2
Oleksandr Sushko <alexandrsushko25@gmail.com>
2024-05-28 20:32:00 +0200

    git push

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,16 @@
-// import a css file containig header styles
+import { Navigation } from '../Navigation/Navigation';
+import './Header.css';
 
+export const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
