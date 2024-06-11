d86790e83b266efa606eb9b75ecc72620dc53271
Om1gSe <mr.om1gse15@gmail.com>
2024-06-11 14:22:48 +0300

    add solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client';
-
 import App from './App';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <>
+    <App />
+    <Welcome />
+    <Header />
+    <Article />
+  </>,
+);
