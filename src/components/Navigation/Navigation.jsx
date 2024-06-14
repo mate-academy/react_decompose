8f1dcc46620b22d1e2b82c58b81056eb0252fe76
Ivan Khomenko <gazebo88fire@gmail.com>
2024-06-14 16:11:25 +0300

    add task solution

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import './Navigation.css';
+
+export const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
+  </nav>
+);
+export default Navigation;
