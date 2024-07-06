86caf5c5a03e3fe5bc340f8af0db208987f327f1
akopp90 <62627921+akopp90@users.noreply.github.com>
2024-07-07 01:47:49 +0200

    task finished

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,15 @@
-// import a css file containig header styles
+// import a css file containig welcome styles
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
-// Create a Header function returning the HTML of header block
+// Create a Welcome function returning the HTML of welcome block
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigation />
+  </header>
+);
 
-// Add a default export statement for Header component to use it in the other files
+// Add a default export statement for Welcome component to use it in the other files
+export default Header;
