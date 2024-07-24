37f3d1934a12c8157028c16625cd8cbffeb21a58
Maksym <max.borovique@gmail.com>
2024-07-24 15:49:29 +0300

    added some solutions

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,8 +1,5 @@
-// import a css file containig article styles
-
-// Create an Article function returning the HTML of article block
-
-// Add a default export statement for Article component to use it in the other files
+import React from 'react';
+import './Article.css';
 
 export const Article = () => (
   <article className="article">
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,8 +1,5 @@
-// import a css file containig welcome styles
-
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
+import React from 'react';
+import './Welcome.css';
 
 export const Welcome = () => (
   <section className="welcome">
