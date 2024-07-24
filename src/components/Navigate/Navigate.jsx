28f8b843476359d27d75226f250b88ab25175e5f
RimmaBuzuluk <rimmysya2000@gmail.com>
2024-07-24 10:20:55 +0300

    add solution

---
+++ src/components/Navigate/Navigate.jsx
@@ -0,0 +1,15 @@
+const Navigate = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
+  </nav>
+);
+
+export default Navigate;
