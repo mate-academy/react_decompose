575e22f254e102f096bd53281e3c9e06bc8fcbbf
Your Name <ruslankurman13@gmail.com>
2024-08-12 16:19:39 +0300

    task solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import React from "react";
-import "./Header.css";
-import Navigation from "../Navigation/Navigation";
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => {
   return (
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Welcome.css';
+
 const Welcome = () => {
   return (
     <section className="welcome">
