a7dcf1ce105de3ccd6e511dae33208e82893b8e6
Sellermens <sellermens@Sellermenss-MacBook-Pro.local>
2024-09-08 00:07:17 +0300

    add task solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,14 @@
-// import a css file containig welcome styles
+// Import the CSS file containing welcome styles
+import './Welcome.css';
 
-// Create a Welcome function returning the HTML of welcome block
+// Create a Welcome function returning the HTML of the welcome block
+function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
 
-// Add a default export statement for Welcome component to use it in the other files
+// Add a default export statement for the Welcome component to use it in other files
+export default Welcome;
