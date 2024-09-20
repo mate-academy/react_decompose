c8409998eb784fc57ec1d611633a454fcc6f0446
Aman Mishra <39798790+mishra-aman@users.noreply.github.com>
2024-09-20 21:45:20 +0530

    -made necessary changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import Article from './components/Article/Article';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
     <div>
       <h1>Mate React App</h1>
+      <Header />
+      <Welcome />
+      <Article />
     </div>
   );
 }
