22860afb5f7c06928fec7ed49d45495c87a620c5
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2025-01-06 13:14:02 +0000

    add task solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import './Article.css';
 
-const Article = () => (
-  <article className="article">
-    <h1 className="article__title">Headline</h1>
-    <p className="article__paragraph">
-      This is a sample paragraph inside the article component.
-    </p>
-  </article>
-);
+function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod ornare. Phasellus sit amet
+        pellentesque mauris...
+      </p>
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae...
+      </p>
+      {/* Add all other paragraphs here */}
+    </article>
+  );
+}
 
 export default Article;
