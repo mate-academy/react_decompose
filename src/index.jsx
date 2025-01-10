e669f7be4b3c406f8c593adfb95c38c91c1c12ed
Varvara00 <varvara.galy171203@gmail.com>
2025-01-10 19:51:12 +0100

    finished task "react_decompose "

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,19 @@
 import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import Navigation from './components/Navigation/Navigation';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Welcome />
+      <Navigation />
+      <Article />
+    </div>
+  );
+}
 
 createRoot(document.getElementById('root')).render(<App />);
