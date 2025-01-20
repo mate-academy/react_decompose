bb8ab2a4a44a4f9b6a2239ac7f6b1c09864e22f3
Veronika Vasyliuk <verka0218@gmail.com>
2025-01-20 15:00:48 +0200

    fix solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Article from './components/Article/Article';
-import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 import './App.css';
 
 function App() {
