5083e08015858b5b6214978ae544aada381bd611
Valentyn <valentyn93beltsyk@gmail.com>
2025-01-30 15:33:34 +0200

    solution_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-import './components/Article/Article.css';
-import './components/Welcome/Welcome.css';
-import './components/Header/Header.css';
-
-import { Welcome } from './components/Welcome/Welcome';
-import { Header } from './components/Header/Header';
-import { Article } from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 // Move each BEM block to a separate component (file) and import them here
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,12 @@
-// import a css file containig welcome styles
+// import a css file containing welcome styles
+import './Welcome.scss';
 
 // Create a Welcome function returning the HTML of welcome block
-
 // Add a default export statement for Welcome component to use it in the other files
-export const Welcome = () => (
-  <section className="welcome">
-    <span className="welcome__text">Sticky Header!</span>
-  </section>
-);
+export default function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
