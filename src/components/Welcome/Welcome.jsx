70589e5f1c691ab5d2243f93a91da11023c7386b
LX-y-ka <alexandravirtualife@gmail.com>
2025-02-09 23:22:39 +0200

    add solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,4 +1,15 @@
 // import a css file containig welcome styles
+import './Welcome.css';
+
+function Welcome () {
+  return (
+    <section className="welcome">
+        <span className="welcome__text">Sticky Header!</span>
+      </section>
+  );
+}
+
+export default Welcome;
 
 // Create a Welcome function returning the HTML of welcome block
 
