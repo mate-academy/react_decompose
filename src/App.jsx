17d6cae4965504563f2cde285c474e416f4549ce
Heny <infowebet@gmail.com>
2025-02-10 10:54:32 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
-import { Header, Welcome, Article } from './components';
+// import { Header, Welcome, Article } from './components';
 
 function App() {
   return (
