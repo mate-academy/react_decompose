df514c24a952cf7e5bdced9f35115f9b58f1aebd
RishiPrasath <73206126+RishiPrasath@users.noreply.github.com>
2025-02-21 01:10:06 +0800

    Implement feature XYZ

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
     <main className="app">
-      <section className="welcome">
+      {/* <section className="welcome">
         <span className="welcome__text">Sticky Header!</span>
-      </section>
-      <header className="header">
+      </section> */}
+      <Welcome />
+
+      {/* <header className="header">
         <h1 className="header__title">Site Name</h1>
         <nav className="navigation">
           <a className="navigation__link" href="#about">
@@ -21,8 +27,10 @@ function App() {
             Contact
           </a>
         </nav>
-      </header>
-      <article className="article">
+      </header> */}
+      <Header />
+
+      {/* <article className="article">
         <h1 className="article__title">Headline</h1>
         <p className="article__paragraph">
           In elementum lorem eget est euismod ornare. Phasellus sit amet
@@ -80,7 +88,9 @@ function App() {
           interdum vulputate, dui eros vehicula nisi, at interdum enim lacus eu
           diam.
         </p>
-      </article>
+      </article> */}
+
+      <Article />
     </main>
   );
 }
