48eff1ef61ba9bf0dfa4c44ec8417f7ed24cd3f6
kami2693 <kamikaze2693@gmail.com>
2025-03-02 15:42:21 +0100

    solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,19 @@
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
+
 // import a css file containig header styles
 
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
