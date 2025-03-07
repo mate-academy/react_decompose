d63249315881cf18ade79506c8e64ece763c80e1
Vladyslav Kutsyn <vladislavkucn@gmail.com>
2025-03-07 15:30:30 +0100

    add soltuion 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
-import Navigation from './components/Navigation/Navigation';
-// Move each BEM block to a separate component (file) and import them here
 /* eslint-disable */
 
 function App() {
   return (
-    <>
+    <main className="app">
       <Header />
-      <Navigation />
       <Welcome />
       <Article />
-    </>
+    </main>
   );
 }
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      <Navigation />
     </header>
   );
 }
