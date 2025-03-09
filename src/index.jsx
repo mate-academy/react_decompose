a070ab5103941cfda375a1d28efb6f71f3b722d8
Ivan <tushchukivan@gmail.com>
2025-03-09 23:26:09 +0200

    add task solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,13 @@
+import React from "react";
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
 
 createRoot(document.getElementById('root')).render(<App />);
+
+const page = ReactDOM.createRoot(document.getElementById('page'));
+page.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
