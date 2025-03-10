277ef06a197884ab3e966e5bc5c9b9d16c41230f
Vasyl <priymak17ukrvaha@gmail.com>
2025-03-10 20:33:22 +0200

    fixed style order

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Welcome } from './components/Welcome/Welcome';
-import { Navigation } from './components/Navigation/Navigation';
 import { Article } from './components/Article/Article';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
