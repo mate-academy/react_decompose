eeb567d09da08b420bb3d9b0d4688fbfcfe0fa63
Nazar <nkinah93@gmail.com>
2025-03-14 18:42:59 +0200

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import { Welcome } from './components/Welcome/Welcome';
-import { Header } from './components/Header/Header';
-import { Article } from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Nav from './components/Nav/Nav';
 // Move each BEM block to a separate component (file) and import them here
-function App() {
+export default function App() {
   return (
     <main className="app">
       <Header />
+      <Nav />
       <Welcome />
       <Article />
     </main>
   );
 }
-
-export default App;
