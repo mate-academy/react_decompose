c9387176c411d39cab42706e2162995433ebabfc
PedroWeb10 <pdro_dev@yahoo.com>
2025-03-17 17:11:08 -0300

    solution React Decompose 2

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,12 +1,13 @@
 // import a css file containig article styles
 import React from 'react';
+import './Welcome.css';
 // Create an Article function returning the HTML of article block
 const Welcome = () => {
   return (
     <welcome>
-      <section className="welcome">
+      <div className="welcome">
         <span className="welcome__text">Sticky Header!</span>
-      </section>
+      </div>
     </welcome>
   );
 };
