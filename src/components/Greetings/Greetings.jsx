c5f93d04f9e6b495bede3e4914a569794a05aa83
Csillag61 <csillagok@gmail.com>
2025-03-31 21:09:19 -0400

    react_decompose

---
+++ src/components/Greetings/Greetings.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Greetings.css'; 
+
+const Greetings = () => {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+};
+
+
+
+export default Greetings;
