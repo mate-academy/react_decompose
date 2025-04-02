9b56b88a9867ab55998aed74f6603d5f113fd2e8
Grzegorz <grze.pra@gmail.com>
2025-04-02 20:33:24 +0200

    Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Article from './components/Article/Article';
+import Header from './components/Header/Header';
 import './App.css';
 // Move each BEM block to a separate component (file) and import them here
 
@@ -9,20 +10,7 @@ function App() {
       <section className="welcome">
         <span className="welcome__text">Sticky Header!</span>
       </section>
-      <header className="header">
-        <h1 className="header__title">Site Name</h1>
-        <nav className="navigation">
-          <a className="navigation__link" href="#about">
-            About
-          </a>
-          <a className="navigation__link" href="#services">
-            Services
-          </a>
-          <a className="navigation__link" href="#contact">
-            Contact
-          </a>
-        </nav>
-      </header>
+      <Header />
       <Article />
     </main>
   );
