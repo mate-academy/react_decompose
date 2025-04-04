55947a6d71595d6de7e55016bf370c43299e9728
Illia <hetsko.ilya@gmail.com>
2025-04-05 02:31:03 +0300

    fix task solution

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <main className="app">
       <Welcome />
       <Header />
+      <Navigation />
       <Article />
     </main>
   );
