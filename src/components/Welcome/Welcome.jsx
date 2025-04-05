6f215d99f33aa03d81a8034621252fba97173dd7
Eric Shankman <ershankman@gmail.com>
2025-04-04 21:54:50 -0400

    initial commit

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,10 @@
 // import a css file containig welcome styles
+import './Welcome.css';
 
 // Create a Welcome function returning the HTML of welcome block
+function Welcome() {
+  return <span className="welcome__text">Sticky Header!</span>;
+}
 
 // Add a default export statement for Welcome component to use it in the other files
+export default Welcome;
