97a44a585d32bc012869379948cc0abf936af67e
Ivan Huzhva <iwan_guzhwa@gmail.com>
2025-04-08 13:10:36 +0200

    add task solution v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import './App.css';
-// Move each BEM block to a separate component (file) and import them here
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
+import Navigation from './components/Navigation/Navigation';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
-  return <main className="app" />;
+  return (
+    <main className="app">
+      <Header />
+
+      <Welcome />
+
+      <Navigation />
+
+      <Article />
+    </main>
+  );
 }
 
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,5 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import Article from './components/Article/Article';
-import Navigation from './components/Navigation/Navigation';
-import Header from './components/Header/Header';
-import Welcome from './components/Welcome/Welcome';
 
-createRoot(document.getElementById('root')).render(
-  <App>
-    <Header />
-
-    <Welcome />
-
-    <Navigation />
-
-    <Article />
-  </App>,
-);
+createRoot(document.getElementById('root')).render(<App />);
