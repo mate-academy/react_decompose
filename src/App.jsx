743b91aed555b7ac5835cf4be1c13aed80025395
Olha Shtyfliuk <149540630+fwrwxx@users.noreply.github.com>
2025-04-15 19:23:25 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header/Header';
-import Article from './components/Article/Article';
-import Welcome from './components/Welcome/Welcome';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header/Header";
+import Article from "./components/Article/Article";
+import Welcome from "./components/Welcome/Welcome";
 
 function App() {
   return (
