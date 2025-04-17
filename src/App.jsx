c11b5b516f3b538ef8dd9d02c185409145bb9dc0
Mykhailo <kurochkinmy@gmail.com>
2025-04-17 15:08:00 +0300

    solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
 import './App.css';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 
-const App = () => <main className="app" />;
+const App = () => (
+  <main className="app">
+    <Welcome />
+    <Header />
+    <Article />
+  </main>
+);
 
 export default App;
