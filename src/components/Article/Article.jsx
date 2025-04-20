53645bb74b922a33d613cf46dd72ea11089f8ea2
Arber <1234arberzylyftari1234@gmail.com>
2025-04-20 15:19:06 +0200

    Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,3 +3,19 @@
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
+import './Article.css';
+
+function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__headline">Headline</h1>
+      <p className="article__paragraph">Paragraph 1</p>
+      <p className="article__paragraph">Paragraph 2</p>
+      <p className="article__paragraph">Paragraph 3</p>
+      <p className="article__paragraph">Paragraph 4</p>
+      <p className="article__paragraph">Paragraph 5</p>
+    </article>
+  );
+}
+
+export default Article;
