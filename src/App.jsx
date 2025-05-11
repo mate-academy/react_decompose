04eea7d4be08a6739e2f5588fe7ebe0c842ba370
Andrii Zakharov <zaharov.andrey.93@gmail.com>
2025-05-11 19:15:07 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Article from './components/Article/Article';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
