9fd974dc27920940473b487dc3c1e8c9a1f02f39
Jaroslava <lazyrka@gmail.com>
2025-05-13 21:45:43 +0300

    solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,11 @@
-// import a css file containig header styles
+import Navigation from '../Navigation/Navigation';
+import './Header.scss';
 
-// Create a Header function returning the HTML of header block
-
-// Add a default export statement for Header component to use it in the other files
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,9 @@
-// import a css file containig welcome styles
+import './Welcome.scss';
 
-// Create a Welcome function returning the HTML of welcome block
-
-// Add a default export statement for Welcome component to use it in the other files
+export default function Welcome() {
+  return (
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
+  );
+}
