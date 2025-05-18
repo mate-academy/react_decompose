86df49689ec496ee9d947375c40caf9dc4152bbe
Serhii bc2024-6 <tickhere135@gmail.com>
2025-05-18 20:50:55 +0300

    fix navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-  import React from 'react';
-  import './App.css';
-  import Article from './components/Article/Article';
-  import Header from './components/Header/Header';
-  import Navigation from './components/Navigation/Navigation';
-  import Welcome from './components/Welcome/Welcome';
+import React from 'react';
+import './App.css';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 
-  function App() {
-    return (
-      <main className="app">
-        <Welcome />
-        <Header />
-        <Navigation />
-        <Article />
-      </main>
-    );
-  }
+function App() {
+  return (
+    <main className="app">
+      <Welcome />
+      <Header />
+      <Article />
+    </main>
+  );
+}
 
-  export default App;
+export default App;
