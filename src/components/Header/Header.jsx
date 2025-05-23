7c9caef63e405914b77dc6986574a937c5821134
Maryna <doctor.who2016.dw@gmail.com>
2025-05-23 15:04:02 +0300

    add task solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,18 +4,22 @@ import './Header.css';
 
 function Header() {
   return (
-    <header className="header">
-      <h1 className="header__title">Site Name</h1>
-      <a className="navigation__link" href="#about">
-        About
-      </a>
-      <a className="navigation__link" href="#services">
-        Services
-      </a>
-      <a className="navigation__link" href="#contact">
-        Contact
-      </a>
-    </header>
+    <>
+      <header className="header">
+        <h1 className="header__title">Site Name</h1>
+      </header>
+      <nav className="navigation">
+        <a className="navigation__link" href="#about">
+          About
+        </a>
+        <a className="navigation__link" href="#services">
+          Services
+        </a>
+        <a className="navigation__link" href="#contact">
+          Contact
+        </a>
+      </nav>
+    </>
   );
 }
 // Add a default export statement for Header component to use it in the other files
