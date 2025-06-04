6f890400be5ca21d7b58cc487763fda1d76e7c61
Vlad Mitlosh <vlad.mitlosh@gmail.com>
2025-06-04 10:31:16 +0300

    solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -3,7 +3,6 @@
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
-import React from 'react';
 import './Article.css';
 
 const Article = () => {
