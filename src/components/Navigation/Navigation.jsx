2a7fdcb4b488abb8c0c598492b59898606ee1e55
Irina <weedfox053@gmail.com>
2025-06-06 22:17:26 +0300

    React Decompose

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,15 @@
+import '../Navigation/Navigation.css'
+
+function Navigation () {
+  return (
+    <nav className="navigation">
+      <ul className="navigation__list">
+        <li className="navigation__item">Home</li>
+        <li className="navigation__item">About</li>
+        <li className="navigation__item">Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navigation;
