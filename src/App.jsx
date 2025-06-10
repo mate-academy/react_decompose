62ece312484161937ca5855454b20450ebcd79f2
Ivan Ilnytskyy <ilniskyi2602@gmail.com>
2025-06-10 21:47:14 +0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
 import Article from './components/Article/Article';
 
 export default function App() {
   return (
     <div className="app">
-      <Header />
-      <Navigation />
       <Welcome />
+      <Header />
       <Article />
     </div>
   );
