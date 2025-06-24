3c0a73534b14a629ef0fbd020cbc30ebde0163ec
Jeny Berezhna <JenySmails24@gmail.com>
2025-06-24 22:38:51 +0100

    add task solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,22 @@
 // import a css file containig welcome styles
+import './Welcome.css';
 
 // Create a Welcome function returning the HTML of welcome block
+function Welcome() {
+  return (
+    <main className="app">
+      <section className="welcome">
+        <span className="welcome__text">Sticky Header!</span>
+        <div className="welcome">
+          <h1 className="welcome__title">Welcome to My Website</h1>
+          <p className="welcome__description">
+            This is a simple React component that welcomes users to the site.
+          </p>
+        </div>
+      </section>
+    </main>
+  );
+}
 
 // Add a default export statement for Welcome component to use it in the other files
+export default Welcome;
