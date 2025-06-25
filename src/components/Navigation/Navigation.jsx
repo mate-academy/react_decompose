f3addddebe46b375d34cb73122d2b4080fcc21c2
TheNash1996 <nash-morris@outlook.com>
2025-06-25 21:34:10 +0100

    Decompose App into Header, Welcome, Article components with styles

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+// src/components/Navigation/Navigation.js
+
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <ul>
+        <li><a href="#welcome">Welcome</a></li>
+        <li><a href="#article">Article</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
