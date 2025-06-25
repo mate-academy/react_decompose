ece97cefb9af0783005d500626f78af7e8d26999
Anatoli Yaschenko <yachenko.anatoliy37@gmail.com>
2025-06-25 08:11:02 +0300

    Підготовка до деплою

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,24 @@
-// import a css file containig article styles
+import './Article.css';
 
-// Create an Article function returning the HTML of article block
-
-// Add a default export statement for Article component to use it in the other files
+export default function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod ornare...
+      </p>
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum...
+      </p>
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel lobortis eu...
+      </p>
+      <p className="article__paragraph">
+        Cras egestas tempor nibh, a fermentum lorem...
+      </p>
+      <p className="article__paragraph">
+        Pellentesque habitant morbi tristique senectus...
+      </p>
+    </article>
+  );
+}
