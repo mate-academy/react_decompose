dc46b348e4e9b06dcf393db9b78cc626f290fcb0
Alessandro <Alessandrogabriel15160@gmail.com>
2025-06-25 22:12:10 -0300

    Add navigation links to Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './Header.css';
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      <nav className="header__nav">
+        <a href="#about" className="header__link">About</a>
+        <a href="#services" className="header__link">Services</a>
+        <a href="#contact" className="header__link">Contact</a>
+      </nav>
     </header>
   );
 }
