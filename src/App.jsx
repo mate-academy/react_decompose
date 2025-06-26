d2da0cc860d0d33a50064135b5776c72b6b78129
Alessandro <Alessandrogabriel15160@gmail.com>
2025-06-26 09:18:25 -0300

    d

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
