1acec2456f758c61efa32fdf3a0b9c1bef6ebce0
Alessandro <Alessandrogabriel15160@gmail.com>
2025-06-25 22:30:09 -0300

    Refactor Header to use Navigation component and fix styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,6 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <nav className="header__nav">
-        <a href="#about" className="header__link">About</a>
-        <a href="#services" className="header__link">Services</a>
-        <a href="#contact" className="header__link">Contact</a>
-      </nav>
     </header>
   );
 }
