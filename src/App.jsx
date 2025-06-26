34644f3fce04f8cf5609af3dd77624a066152f54
Alessandro <Alessandrogabriel15160@gmail.com>
2025-06-25 23:04:52 -0300

    rd

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,13 @@ import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
-import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <>
       <Header />
-      <Navigation />
       <Welcome />
       <section id="contact">
-        {/* conteúdo da seção Contact aqui */}
         Contact content
       </section>
       <main className="app">
