32d9e731578fa42645eced92375bf4480a3033bf
= <=>
2025-07-16 20:47:42 -0300

    Adicionado as funcionalidades

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,3 +3,15 @@
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
+
+import './Welcome.css';
+
+const Welcome = () => {
+  return (
+    <article className="welcome">
+      <p className="welcome__text">Sticky Header!</p>
+    </article>
+  );
+};
+
+export default Welcome;
