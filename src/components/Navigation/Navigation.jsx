db1896bb4a997fbaaa13810f6420dadaffe742ba
Andriï Zakhartchenko <andriizakhartchenko@outlook.fr>
2025-07-24 13:54:22 +0200

    save

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,17 +1,17 @@
-function Navigation(){
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a className="navigation__link" href="#about">
+        About
+      </a>
+      <a className="navigation__link" href="#services">
+        Services
+      </a>
+      <a className="navigation__link" href="#contact">
+        Contact
+      </a>
+    </nav>
+  );
+}
 
-            return(
-                <nav className="navigation">
-                    <a className="navigation__link" href="#about">
-                        About
-                    </a>
-                    <a className="navigation__link" href="#services">
-                        Services
-                    </a>
-                    <a className="navigation__link" href="#contact">
-                        Contact
-                    </a>
-                </nav>
-            );
-} 
 export default Navigation;
