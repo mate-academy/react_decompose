febe8f11916d20b4b46ae0526ca5386cf74a6041
inikiforov <ievgen.nikiforov@tedata.de>
2025-07-24 17:54:51 +0200

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 // Move each BEM block to a separate component (file) and import them here
-import Article from './components/Article/Article.jsx';
-import Header from './components/Header/Header.jsx';
-import Welcome from './components/Welcome/Welcome.jsx';
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
 
 function App() {
   return (
