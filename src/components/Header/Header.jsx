6523e50e59b66ad4db45f4b8b2e0ee53b737476b
Mike <mixakievchanen@gmail.com>
2025-07-26 13:01:50 +0300

    imports fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 // import a css file containig header styles
-
+import './Header.css';
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
