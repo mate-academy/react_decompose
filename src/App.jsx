d6023d903efdbf6d391322b994b5252d9dbf451f
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2025-07-30 16:56:03 +0300

    remove 'import Navigation' from App.jsx, improve DEMO link

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
+import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Article from './components/Article/Article';
-import './App.css';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
