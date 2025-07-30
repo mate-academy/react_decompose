996b09e39c2dbc275f91feeda75598f8f9418248
lvlashnin <lvlashnin@gmail.com>
2025-07-30 17:14:32 +0300

    decomposed App

---
+++ src/AppNew.jsx
@@ -0,0 +1,14 @@
+import Article from './components/Article/Article';
+import Header from './components/Header/Header';
+import Welcome from './components/Welcome/Welcome';
+import './App.css';
+
+export const AppNew = () => (
+  <div>
+    <Welcome />
+
+    <Header />
+
+    <Article />
+  </div>
+);
--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import { createRoot } from 'react-dom/client';
+import { AppNew } from './AppNew';
 
-import App from './App';
-
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(<AppNew />);
