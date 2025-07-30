338a794ba1bcc824775428c34efdf64117bf3cf2
DmitriyALEX <doiroffical@gmail.com>
2025-07-30 20:19:22 +0300

    fixed components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Welcome from './components/Welcome/Welcome';
-import Article from './components/Article/Article';
+import './App.css'
+import Header from './components/Header/Header'
+import Welcome from './components/Welcome/Welcome'
+import Article from './components/Article/Article'
 
 function App() {
   return (
     <main className="app">
-      <Welcome />
       <Header />
+      <Welcome />
       <Article />
     </main>
-  );
+  )
 }
 
-export default App;
+export default App
