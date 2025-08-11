068387dc09bd536f379dd7b94bd22499e8706c96
MTS13 <mtstaras@gmail.com>
2025-08-11 22:29:37 +0300

    add task solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,11 @@
 // import a css file containig header styles
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 // Create a Header function returning the HTML of header block
 const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
-    <nav className="navigation">
-      <a href="#about" className="navigation__link">
-        About
-      </a>
-      <a href="#services" className="navigation__link">
-        Services
-      </a>
-      <a href="#contact" className="navigation__link">
-        Contact
-      </a>
-    </nav>
+    <Navigation />
   </header>
 );
 
