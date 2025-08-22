55761ce86ddd02581f4c53cfb384d29460eb2406
Nadiia Chernygina <nadja.chernygina@gmail.com>
2025-08-22 16:34:15 +0300

    second solution in the tasck

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,23 @@
+// import a css file containig header styles
+
+// Create a Header function returning the HTML of header block
+
+// Add a default export statement for Header component to use it in the other files
+
+import './Navigation.css';
+
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
+  </nav>
+);
+
+export default Navigation;
