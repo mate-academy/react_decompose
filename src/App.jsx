40428a1c209aa405741e26b8c98d8a676ed14766
AdamPershyi <en0tadam@gmail.com>
2025-08-25 21:35:17 +0300

    task solution01

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import './App.css';
+// eslint-disable-next-line import/no-named-as-default
 import Welcome from './components/Welcome/Welcome';
+// eslint-disable-next-line import/no-named-as-default
 import Header from './components/Header/Header';
+// eslint-disable-next-line import/no-named-as-default
 import Article from './components/Article/Article';
+// eslint-disable-next-line import/no-named-as-default
+import Navigation from './components/Navigation/Navigation';
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
@@ -10,6 +15,7 @@ function App() {
     <main className="app">
       <Welcome />
       <Header />
+      <Navigation />
       <Article />
     </main>
   );
