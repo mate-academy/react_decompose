db135b5d1305fa367fa473d627f15505984a84ad
Mykhailo Doloh <dologmihajlo31@gmail.com>
2025-09-03 22:13:11 +0300

    add task

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,16 @@
-// import a css file containig header styles
+// src/components/Header/Header.jsx
 
-// Create a Header function returning the HTML of header block
+import React from 'react';
+import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
-// Add a default export statement for Header component to use it in the other files
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
