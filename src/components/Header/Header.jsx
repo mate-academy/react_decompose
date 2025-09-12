2add03ad9df997b9cbaae8049e4d49c56cfdd54e
jridiamal <amaljridi84@gmail.com>
2025-09-12 19:01:20 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,3 +3,20 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+import Header from './Header/Header';
+import Welcome from './Welcome/Welcome';
+import Navigation from './Navigation/Navigation';
+import Article from './Article/Article';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Welcome />
+      <Navigation />
+      <Article />
+    </>
+  );
+}
+
+export default App;
