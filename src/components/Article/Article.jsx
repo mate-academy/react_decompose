93dd872cca3341ab7e340d3abf5363849771a701
Andriy <andrijs373@gmail.com>
2025-09-20 15:28:51 +0300

    add task solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -6,7 +6,7 @@
 import './Article.css';
 
 const Article = () => (
-  <article>
+  <article className="article">
     <h1 className="article__title">Headline</h1>
     <p className="article__paragraph">
       In elementum lorem eget est euismod ornare. Phasellus sit amet
