0ce0934fe4f9b9f1097e551cca6d50d79f2b6613
Oleh Pshyk <pshykol@gmail.com>
2025-09-30 17:23:06 +0300

    add task solution

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,20 @@
+import './Navigation.css';
+import React from 'react';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a className="navigation__link" href="#about">
+        About
+      </a>
+      <a className="navigation__link" href="#services">
+        Services
+      </a>
+      <a className="navigation__link" href="#contact">
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+export default Navigation;
