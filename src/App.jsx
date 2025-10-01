7f0b6c43679d112f610f305b9cc395b201f50d03
Hannah Olbrich <hannaholbrich@Hannahs-MacBook-Pro.local>
2025-10-01 18:29:32 +0100

    Add solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 // Move each BEM block to a separate component (file) and import them here
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
 
@@ -10,7 +9,6 @@ function App() {
   return (
     <main className="app">
       <Header />
-      <Navigation />
       <Welcome />
       <Article />
     </main>
