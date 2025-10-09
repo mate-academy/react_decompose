71985f7982dbc8257c2209b7ef8469b635d3abec
Anton <nimachuka@gmail.com>
2025-10-09 20:13:38 +0200

    all tests passed

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,4 +1,35 @@
-// import a css file containig article styles
+import React from 'react';
+import './Article.css';
+
+const Article = () => {
+  return (
+    <article className="article">
+      <h1>Headline</h1>
+      <p className="article__paragraph">
+        This is a sample article content. It provides information about a
+        specific topic and is designed to engage readers.
+      </p>
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod ornare. Phasellus sit amet
+        pellentesque mauris. Aliquam quis malesuada ex.
+      </p>
+      <p className="article__paragraph">
+        Nullam eu aliquam nibh. Mauris molestie, urna accumsan ornare semper,
+        augue nibh posuere lorem, vitae feugiat sem magna eget massa.
+      </p>
+      <p className="article__paragraph">
+        Vivamus quis tincidunt dolor. Fusce efficitur, orci non vestibulum
+        consequat, lectus turpis bibendum odio.
+      </p>
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor. Cras
+        congue, massa nec sagittis mollis.
+      </p>
+    </article>
+  );
+};
+
+export default Article;
 
 // Create an Article function returning the HTML of article block
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,3 +3,26 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <nav>
+        <a href="#about" className="navigation__link">
+          About
+        </a>
+        <a href="#services" className="navigation__link">
+          Services
+        </a>
+        <a href="#contact" className="navigation__link">
+          Contact
+        </a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
