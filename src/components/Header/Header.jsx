d912ada5904fb9f1c2ffd17161ecf5a0e8b974b8
Mykola Huryn <mukola197022@gmail.com>
2025-10-15 17:17:49 +0200

    decompose

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,3 +3,15 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+
+import './Header.css';
+import Navigtion from '../Navigation/Navigation';
+
+const Header = () => (
+  <header className="header">
+    <h1 className="header__title">Site Name</h1>
+    <Navigtion />
+  </header>
+);
+
+export default Header;
