b1717f70297a1879f80d99e3068cc4cac7be4e14
gabemaldito <gabrielraphacosta@gmail.com>
2025-10-20 15:47:37 -0700

    novo

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,15 @@
 // import a css file containig article styles
+// import a css file containig article styles
 
+function Article() {
+  return (
+    <article>
+      {/* Your article content here */}
+    </article>
+  );
+}
+
+export default Article;
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,15 @@
 // import a css file containig welcome styles
+// import a css file containig welcome styles
 
+function Welcome() {
+  return (
+    <section>
+      {/* Your welcome content here */}
+    </section>
+  );
+}
+
+export default Welcome;
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
