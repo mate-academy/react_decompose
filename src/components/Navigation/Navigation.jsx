39dfca5424fa8e6c901abd8e6dac5f22d75df42f
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-23 07:51:02 -0300

    React-Decompose

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+// import a css file containig navigation styles
+
+import './Navigation.css';
+
+function Navigation() {
+  return (
+    <nav className="navigation">
+      <a className="navigation__link" href="#about">
+        About
+      </a>
+      <a className="navigation__link" href="#services">
+        Services
+      </a>
+      <a className="navigation__link" href="#contact">
+        Contact
+      </a>
+    </nav>
+  );
+}
+
+export default Navigation;
