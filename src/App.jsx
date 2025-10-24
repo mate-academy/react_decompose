067b0682daedea3a19d30f331d7c0df1d69b2e80
Denys <yolm9yom@gmail.com>
2025-10-24 22:19:20 +0300

    add task solution

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
 import './App.css';
-// eslint-disable-next-line import/no-named-as-default
 import Welcome from './components/Welcome/Welcome';
-// eslint-disable-next-line import/no-named-as-default
 import Header from './components/Header/Header';
-// eslint-disable-next-line import/no-named-as-default
 import Article from './components/Article/Article';
-// Move each BEM block to a separate component (file) and import them here
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,7 @@
 import Navigation from '../Navigation/Navigation';
 import './Header.css';
 
-export const Header = () => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Navigation />
