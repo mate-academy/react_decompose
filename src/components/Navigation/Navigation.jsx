08daa6a1b0895b5d643d89225291b77794eefdb1
Eduardo Santos Ferreira <eduardoferreira.gplus@gmail.com>
2025-10-24 21:37:46 -0300

    Task solution

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import './Navigation.css';
+
+// Create a Navigation function returning the HTML of navigation block
+const Navigation = () => (
+  <nav className="navigation">
+    <a className="navigation__link" href="#about">
+      About
+    </a>
+    <a className="navigation__link" href="#services">
+      Services
+    </a>
+    <a className="navigation__link" href="#contact">
+      Contact
+    </a>
+  </nav>
+);
+
+export default Navigation;
