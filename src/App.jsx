dec62bc546d8c15f48c28e52722f9aad2f79c8fd
Olha Trokhymchuk <trochymtschuk.ol@gmail.com>
2025-10-27 20:23:10 +0200

    add some changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import './App.css';
 import Article from './components/Article/Article';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Welcome from './components/Welcome/Welcome';
 
 // Move each BEM block to a separate component (file) and import them here
@@ -10,10 +9,9 @@ import Welcome from './components/Welcome/Welcome';
 function App() {
   return (
     <main className="app">
-      <Article />
-      <Header />
-      <Navigation />
       <Welcome />
+      <Header />
+      <Article />
     </main>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 const Header = () => {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      <Navigation />
     </header>
   );
 };
