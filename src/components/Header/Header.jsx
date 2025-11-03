ec2d1634dcfdc962c77e7cd0939fbffd51e1cc81
Roman <romanpopovuch07@gmail.com>
2025-11-03 16:39:01 +0200

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
 import './Header.css';
+import Navigation from '../Navigation/Navigation';
 
 export default function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-
-      <nav className="navigation">
-        <a href="#about" className="navigation__link">
-          About
-        </a>
-        <a href="#services" className="navigation__link">
-          Services
-        </a>
-        <a href="#contact" className="navigation__link">
-          Contact
-        </a>
-      </nav>
+      <Navigation />
     </header>
   );
 }
---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navigation.css';
+
+export default function Navigation() {
+  return (
+    <nav className="navigation">
+      <a href="#about" className="navigation__link">
+        About
+      </a>
+      <a href="#services" className="navigation__link">
+        Services
+      </a>
+      <a href="#contact" className="navigation__link">
+        Contact
+      </a>
+    </nav>
+  );
+}
