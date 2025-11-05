fd79498a01e3cd66d20c2d5904826591897e9242
Микола. <basovmikola04@gmail.com>
2025-11-05 22:05:28 +0200

    implement task

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,3 +3,14 @@
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+    </header>
+  );
+}
+
+export default Header;
