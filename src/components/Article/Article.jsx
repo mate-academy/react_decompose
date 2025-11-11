79bff205eab5bf2490e20f4a9072ac5c55ef0e11
Alisson <alissonramos539@gmail.com>
2025-11-11 20:26:26 -0300

    Adding Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,43 @@
-// import a css file containig article styles
+import React from 'react';
+import '../../App.css';
 
-// Create an Article function returning the HTML of article block
+function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
 
-// Add a default export statement for Article component to use it in the other files
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod ornare. Phasellus sit amet
+        pellentesque mauris. Aliquam quis malesuada ex. Nullam eu aliquam
+        nibh. Mauris molestie, urna accumsan ornare semper, augue nibh posuere
+        lorem, vitae feugiat sem magna eget massa. Vivamus quis tincidunt
+        dolor. Fusce efficitur, orci non vestibulum consequat, lectus turpis
+        bibendum odio, in efficitur leo felis sed justo. Fusce commodo iaculis
+        orci, quis imperdiet urna. Sed mollis facilisis lacus non condimentum.
+      </p>
+
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae,
+        fermentum ac quam. Aliquam pretium tristique nibh quis iaculis.
+      </p>
+
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor.
+        Cras congue, massa nec sagittis mollis, dui felis ultrices magna,
+        tincidunt finibus lorem quam in sem.
+      </p>
+
+      <p className="article__paragraph">
+        Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
+        facilisi. In at elit id leo tristique condimentum.
+      </p>
+
+      <p className="article__paragraph">
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Phasellus bibendum nec arcu eu lobortis.
+      </p>
+    </article>
+  );
+}
+
+export default Article;
