5cb19777d9d37ba2abba1b5a1460055f759ec48c
Iryna <Yurchak.Iryna2017@gmail.com>
2025-11-12 15:52:33 +0200

    impruved

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,7 +4,7 @@ import './Welcome.css';
 export default function Welcome() {
   return (
     <section className="welcome">
-      <span className="welcome__text">Sticky Header!</span>
+      <span className="welcome__text"> Sticky Header!</span>
     </section>
   );
 }
