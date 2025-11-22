7274d23248e5655a2dbe1fdc0752dacc3757ea98
Elber-Kevenny <vieirabeb@gmail.com>
2025-11-21 23:26:09 -0300

    add task solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,15 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Navigation from './components/Navigation/Navigation';
+import Article from './components/Article/Article';
 
-createRoot(document.getElementById('root')).render(<App />);
+createRoot(document.getElementById('root')).render(
+  <App />,
+  <Welcome />,
+  <Header />,
+  <Article />,
+  <Navigation />,
+);
