2a30d7fd85cebe1f7fb6c24dd31db555766376ac
Elber-Kevenny <vieirabeb@gmail.com>
2025-11-21 23:43:04 -0300

    add new solution

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,5 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import Welcome from './components/Welcome/Welcome';
-import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
-import Article from './components/Article/Article';
 
-createRoot(document.getElementById('root')).render(
-  <App />,
-  <Welcome />,
-  <Header />,
-  <Article />,
-  <Navigation />,
-);
+createRoot(document.getElementById('root')).render(<App />);
