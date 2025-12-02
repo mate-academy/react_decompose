77219ecd9fc1350a12d9f8bcb86ea4459a412a38
Tymofii <zrozik@MacBook-Air-Tymofii.local>
2025-12-02 15:42:37 +0200

    added navigation, welcome, header, article components

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
+  { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
+  pluginReact.configs.flat.recommended,
+]);
