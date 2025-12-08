5dc01f2c64e4f4ca514110d18c8b98a9e5e1581e
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-12-08 23:50:10 +0200

    Decompose react

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
-// import a css file containig header styles
+import React from 'react';
+import './Header.css';
 
-// Create a Header function returning the HTML of header block
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+    </header>
+  );
+}
 
-// Add a default export statement for Header component to use it in the other files
+export default Header;
