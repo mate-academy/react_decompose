7856d90ead56d2157a3b088f2ef3cb18db637341
horbenkoOleksandr <horbenko.oleksandr.v@gmail.com>
2025-12-09 00:08:46 +0200

    React-decompose

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './Header.css';
 
-function Header() {
+function Header({ children }) {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
+      {children}
     </header>
   );
 }
