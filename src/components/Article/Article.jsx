f1bf31a76eef7d4cd8edbca48f37ee260f2b50c3
Stas <oleksii.stanislav@student.uzhnu.edu.ua>
2025-12-17 19:19:35 +0200

    Fix

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,7 +1,7 @@
 // import a css file containig article styles
 import './Article.css';
 // Create an Article function returning the HTML of article block
-export const Article = () => (
+const Article = () => (
   <article className="article">
     <h1 className="article__title">Headline</h1>
     <p className="article__paragraph">
@@ -60,4 +60,5 @@ export const Article = () => (
   </article>
 );
 // Add a default export statement for Article component to use it in the other files
+
 export default Article;
