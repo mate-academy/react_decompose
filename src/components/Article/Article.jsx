c436c7aeed780328bcfc502c60efb70265071e58
Vlad Zhovtani <zovtanyy@gmail.com>
2025-12-25 15:30:33 +0200

    done3

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,7 +1,7 @@
 // import a css file containig article styles
 import './Article.css';
 // Create an Article function returning the HTML of article block
-export const Article = () => {
+const Article = () => {
   return (
     <article className="article">
       <h1 className="article__title">Headline</h1>
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -2,7 +2,7 @@
 import './Welcome.css';
 
 // Create a Welcome function returning the HTML of welcome block
-export const Welcome = () => {
+const Welcome = () => {
   return (
     <section className="welcome">
       <span className="welcome__text">Sticky Header!</span>
