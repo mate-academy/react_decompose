5cfc7bf7f5c59ddebe93d76175278fcdb409bfab
Tetiana Sobolieva <tetiana.sobolieva.s@gmail.com>
2025-12-29 09:25:18 +0200

    deleted unnessesary imports css

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Article from './components/Article/Article';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
