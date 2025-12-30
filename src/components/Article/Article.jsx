8e6d6ee11688b155cdd314b0f9928ea7d9e7d0af
Vasyl Levitskyi <leva1990vale@gmail.com>
2025-12-30 09:44:31 +0200

    add solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -12,25 +12,16 @@ function Article() {
       <h1 className="article__title">Headline</h1>
 
       <p className="article__paragraph">
-        In elementum lorem eget est euismod ornare. Phasellus sit amet
-        pellentesque mauris. Aliquam quis malesuada ex.
+        In elementum lorem eget est euismod ornare.
       </p>
+      <p className="article__paragraph">In ac nisi lacus. Fusce est dolor.</p>
 
       <p className="article__paragraph">
-        In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae,
-        fermentum ac quam.
+        Donec arcu elit, euismod vel lobortis.
       </p>
-
+      <p className="article__paragraph">Cras egestas tempor nibh.</p>
       <p className="article__paragraph">
-        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor.
-      </p>
-
-      <p className="article__paragraph">
-        Cras egestas tempor nibh, a fermentum lorem sollicitudin non.
-      </p>
-
-      <p className="article__paragraph">
-        Pellentesque habitant morbi tristique senectus et netus.
+        Pellentesque habitant morbi tristique.
       </p>
     </article>
   );
