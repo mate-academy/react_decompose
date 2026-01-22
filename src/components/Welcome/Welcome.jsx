5f82c7c079aaf7e0da905b84f2bd38fc799e71c0
Samuel Lazarin <samuel.lazarin12@gmail.com>
2026-01-22 18:17:57 -0300

    add task solution test one

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -4,9 +4,10 @@ import './Welcome.css';
 function Welcome() {
   return (
     <section className="welcome">
-        <span className="welcome__text">Sticky Header!</span>
-      </section>
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
   );
 }
+
 // Add a default export statement for Welcome component to use it in the other files
 export default Welcome;
