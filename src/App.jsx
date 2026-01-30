1ba9745351d808964d1a093eab24176b0378aebe
Olga Wukelan <olga.wukelan@gmail.com>
2026-01-30 21:48:33 +0400

    fix navig

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,13 @@ import React from 'react';
 import './App.css';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 import Article from './components/Article/Article';
-
 
 function App() {
   return (
     <main className="app">
       <Welcome />
       <Header />
-      <Navigation />
       <Article />
     </main>
   );
