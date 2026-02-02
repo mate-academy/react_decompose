4fd592a2d711fcd67f637ce1047ea726324ae310
Andrey <inokatre08@gmail.com>
2026-02-02 19:36:29 +0100

    Solution1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import './Header.css';
 import Nav from '../Navigation/Navigation';
 
-export const Header = () => (
+const Header = () => (
   <header className="header">
     <h1 className="header__title">Site Name</h1>
     <Nav />
