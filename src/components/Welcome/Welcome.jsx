5d56bba7aa399b91a7b27cee3ad78d1143fb8020
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-02-07 16:24:29 +0100

    i have do decompose

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,5 @@
 // import a css file containig welcome styles
-import './Welcome.css'
+import './Welcome.css';
 
 // Create a Welcome function returning the HTML of welcome block
 const Welcome = () => {
@@ -7,8 +7,8 @@ const Welcome = () => {
     <section className="welcome">
       <span className="welcome__text">Sticky Header!</span>
     </section>
-  )
-}
+  );
+};
 
 // Add a default export statement for Welcome component to use it in the other files
 export default Welcome;
