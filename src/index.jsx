49f0f5eb9860d601b54bce1718053d2558f459e0
shymkivvasyl <shymkiv.vasyl333@gmail.com>
2026-02-07 18:40:13 +0100

    www

--- src/index.jsx
+++ src/index.jsx
@@ -4,4 +4,5 @@ import App from './App';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
+
 root.render(<App />);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// eslint-disable-next-line import/extensions
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
