b7e2f7401301eaabaeefb972cfaedd1a2b086f25
unknown <dkuz6323@gmail.com>
2026-02-12 19:43:14 +0200

    fix bugs

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -6,7 +6,7 @@
 
 import './Article.css';
 
-export const Article = () => (
+const Article = () => (
   <article className="article">
     <h1 className="article__title">Headline</h1>
     <p className="article__paragraph">
@@ -64,3 +64,5 @@ export const Article = () => (
     </p>
   </article>
 );
+
+export default Article;
