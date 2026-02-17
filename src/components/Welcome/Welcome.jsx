fbd118945138cf3da7bc7beb073b1d4a55e057fe
Dima Sidenko <sidenkodima2202@gmail.com>
2026-02-17 20:27:22 +0100

    add solution

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,11 @@
 // import a css file containig welcome styles
-
+import './Welcome.css';
 // Create a Welcome function returning the HTML of welcome block
-
+const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
 // Add a default export statement for Welcome component to use it in the other files
+
+export default Welcome;
