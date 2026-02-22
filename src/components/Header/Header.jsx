a0c892a490520e7d2a6ddf4357f83429ae5bfaa0
Artem Nosachenko <nosachenko.art@icloud.com>
2026-02-22 13:28:40 +0100

    add task

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
-// import a css file containig header styles
+import './Header.css';
 
+export default function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+    </header>
+  );
+}
 // Create a Header function returning the HTML of header block
 
 // Add a default export statement for Header component to use it in the other files
