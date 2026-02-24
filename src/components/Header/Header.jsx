fd33951b23ad39e2f36122856750a3f269ce405d
Anna Rodina <tavoshka@gmail.com>
2026-02-24 15:55:57 +0200

    Fixed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-      <Navigation />;
+      <Navigation />
     </header>
   );
 }
