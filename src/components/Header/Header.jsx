48175d572a6003583b8e7f2d130a285ba0b3adbd
Tiago <o-seu-email-do-github@exemplo.com>
2026-02-24 13:20:56 +0000

    feat: decompose App into Welcome, Header, and Article components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,21 @@
 // import a css file containig header styles
-import React from "react";
+import React from 'react';
 
 // Create a Header function returning the HTML of header block
-import "./Header.css";
+import './Header.css';
 
 // Add a default export statement for Header component to use it in the other files
 const Header = () => {
-  return(
+  return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
       <a className="navigation__link" href="#about">
         About
       </a>
-       <a className="navigation__link" href="#services">
+      <a className="navigation__link" href="#services">
         Services
       </a>
-       <a className="navigation__link" href="#contact">
+      <a className="navigation__link" href="#contact">
         Contact
       </a>
     </header>
