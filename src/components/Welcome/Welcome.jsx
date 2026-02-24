39090e79626b02c5ebfa75a1d6198ab939344bc0
Aleks Pashkevych <alekspashkevych@gmail.com>
2026-02-24 18:55:11 +0200

    Tasks looks working

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -3,9 +3,9 @@ import './Welcome.css';
 // Create a Welcome function returning the HTML of welcome block
 export const Welcome = () => {
   return (
-     <section className="welcome">
-            <span className="welcome__text">Sticky Header!</span>
-          </section>
+    <section className="welcome">
+      <span className="welcome__text">Sticky Header!</span>
+    </section>
   );
 };
 // Add a default export statement for Welcome component to use it in the other files
