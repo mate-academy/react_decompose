43ead9976a485a3b9ca79b5b2ccdea85cae02bc3
joicyareis <jdcampchaves@gmail.com>
2026-03-19 20:23:53 +1100

    React Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,21 @@
-// import a css file containig article styles
+import './Article.css'; // import a css file containig article styles
 
 // Create an Article function returning the HTML of article block
 
 // Add a default export statement for Article component to use it in the other files
+
+function Article() {
+  return (
+    <article className="article">
+      <h1>Headline</h1>
+
+      <p className="article__paragraph"> Paragrafo 1</p>
+      <p className="article__paragraph"> Paragrafo 2</p>
+      <p className="article__paragraph"> Paragrafo 3</p>
+      <p className="article__paragraph"> Paragrafo 4</p>
+      <p className="article__paragraph"> Paragrafo 5</p>
+    </article>
+  );
+}
+
+export default Article;
