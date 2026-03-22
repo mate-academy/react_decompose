9d28d298e1b0640260c48a183056830c6d01dd6b
Natalia Humenyk <gumenuknatalia53@gmail.com>
2026-03-22 21:44:44 +0200

    Solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import Navigation from '../Navigation/Navigation';
 // Create a Header function returning the HTML of header block
 const Header = () => {
   return (
-    <header className="header">
+    <header className="header" style={{ backgroundColor: 'red' }}>
       <h1 className="header__title">Site Name</h1>
       <Navigation />{' '}
     </header>
