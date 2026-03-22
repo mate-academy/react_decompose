e7c43c8b1fabbf1d00a335a3b1163c50fcb18de6
nataSokolnikova <167069028+nataSokolnikova@users.noreply.github.com>
2026-03-22 15:51:47 +0200

    Load sol

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,5 +1,12 @@
 // import a css file containig welcome styles
+import './Welcome.scss';
 
+// Create an Article function returning the HTML of article block
+export const Welcome = () => (
+  <section className="welcome">
+    <span className="welcome__text">Sticky Header!</span>
+  </section>
+);
 // Create a Welcome function returning the HTML of welcome block
 
 // Add a default export statement for Welcome component to use it in the other files
