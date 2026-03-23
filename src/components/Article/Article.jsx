706a1511c1db5f2d721c2c24ab7c749f2b42fce5
Denys Boiko <big4ever6996@gmail.com>
2026-03-23 19:16:21 +0200

    Solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,64 @@
-// import a css file containig article styles
+import './Article.css';
 
-// Create an Article function returning the HTML of article block
+export function Article() {
+  return (
+    <article className="article">
+      <h1 className="article__title">Headline</h1>
+      <p className="article__paragraph">
+        In elementum lorem eget est euismod ornare. Phasellus sit amet
+        pellentesque mauris. Aliquam quis malesuada ex. Nullam eu aliqu nibh.am
+        nibh. r is molestie, urna accumsan ornare semper, augue nibh posuere lor
+        vita e feugiat sem magna eget massa. Vivamus quis tincidunt dolo
+        efficitur, orci non vestibulum consequat, lectus turpis bibendum o in
+        efficitur leo felis sed justo. Fusce commodo iaculis orci, quis imperdie
+        t urna. Sed mollis facilisis lacus non condimentum. Nunc Nunc f icitur
+        massa non neque elementum semper. Vestibulum lorem ar arcu,cu, cidunt in
+        quam et, feugiat venenatis augue. Donec sed tin tellus, a facilisis
+        magna. Proin sit amet viverra nibh, bibendu felis. Vivamus ut nunc id
+        mauris posuere pellentesque. tincidunt id odio id feugiat.
+      </p>
+      <p className="article__paragraph">
+        In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae,
+        fermentum ac quam. Aliquam pretium tristique nibh quis iaculis. In et
+        cursus ex, eu aliquet ex. Proin facilisis lacus sit amet sapien
+        ultrices, ut vehicula arcu lobortis. Vivamus mollis ipsum ut hendrerit
+        molestie. Morbi lacinia, sapien eu dictum dignissim, tellus tortor
+        congue magna, sit amet bibendum libero nisi id massa.
+      </p>
+      <p className="article__paragraph">
+        Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolo Crasr.
+        Cras ngue, massa nec sagittis mollis, dui felis ultrices magna, ti
+        finibus lorem quam in sem. Morbi odio turpis, pulvinar sit amett ame
+        lputate quis, ultricies eu libero. Donec ac maximus neque, nec ma nibh.
+        Morbi rhoncus convallis urna, accumsan porta lorem hendr Cras eget n i
+        sl dui. Morbi faucibus nisi eget ipsum semper vulp Mauris nec tinc i
+        dunt lectus. Aenean ac mi consequat velit dignissim consectetur. F usce
+        placerat ac ipsum ac eleifend. Aenean quis f ex.
+      </p>
+      <p className="article__paragraph">
+        Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
+        facilisi. In at elit id leo tristique condimentum. Donec at est nulla.
+        Mauris egestas magna ut laoreet pretium. Sed ultrices suscipit
+        vestibulum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        Fusce id sapien eros. Vivamus viverra ultricies gravida. Nam ur nibh,na
+        nibh, ndit a vulputate at, vehicula non nulla. Aenean ut nulla leo.
+        Praesent in ullamcorper est.
+      </p>
+      <p className="article__paragraph">
+        Pellentesque habitant morbi tristique senectus et netus et malesuada
+        fames ac turpis egestas. Phasellus bibendum nec arcu eu lobortis. Nam
+        convallis faucibus ante sed porta. Nullam ut convallis elit, quis
+        venenatis nunc. Curabitur sed sem eget velit condimentum rutrum in et
+        orci. Nunc non suscipit eros. Suspendisse porta sem vel justo commodo
+        dictum. Aliquam erat ligula, fringilla nec suscipit sed, porta vitae
+        turpis. Vestibulum rhoncus placerat nulla vitae suscipit. Curabitur
+        consectetur ex ut odio tristique vehicula. Ut ligula tortor, tincidunt
+        quis sodales vitae, ornare a turpis. Proin sit amet finibus eni Fuscem.
+        Fusce pus a neque vitae tempor. Aenean rutrum, libero iaculis int vulput
+        a te, dui eros vehicula nisi, at interdum enim lacus eu diam.{' '}
+      </p>
+    </article>
+  );
+}
 
-// Add a default export statement for Article component to use it in the other files
+export default Article;
