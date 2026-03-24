71696d0e6ad340ffc6ee24e10d203da569704599
Alex Kh. <sanchez.primal@gmail.com>
2026-03-24 14:00:54 +0100

    add solution

---
+++ src/components/Navigation/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-restricted-exports */
+
+export { default } from './Navigation';
+// export * as default from './Navigation'; // wierd
+// export * from './Navigation'; // also doesn't re-export default
