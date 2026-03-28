e64db122ae1480bb0bace50f35d0d43b57779c93
Taras Pidkuimukha <taraspidkuimukha@gmail.com>
2026-03-28 16:40:33 +0100

    fix: Navigation part added, article-title styles added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
+import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <main className="app">
         <Welcome />
         <Article />
+        <Navigation />
       </main>
     </>
   );
