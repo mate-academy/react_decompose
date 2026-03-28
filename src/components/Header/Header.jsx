9cc2952b59bc1594af41a89daa259a4d335130d5
Miftah Adem <miftahaadem@gmail.com>
2026-03-29 00:16:59 +0300

    Modified Header.css

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import './Header.css';
 // Create a Header function returning the HTML of header block
 function Header() {
   return (
-    <header className='header'>
+    <header className="header">
       <h1 className="header__title">Site Name</h1>
       <Navigation />
     </header>
