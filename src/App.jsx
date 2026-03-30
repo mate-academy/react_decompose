521a897f01a010fc53037cb0a890d21f2e6e431f
itpoverenov@gmail.com <itpoverenov@gmail.com>
2026-03-30 23:18:59 +0200

    t

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Welcome from './components/Welcome/Welcome';
 import Article from './components/Article/Article';
+
 // Move each BEM block to a separate component (file) and import them here
 
 function App() {
