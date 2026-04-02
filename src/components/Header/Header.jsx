d0f5ea400cd5c2b0449dd832a0b266d89ee7de12
Tetiana Topchii <tetianatopchii@MacBook-Air-Tetiana.local>
2026-04-02 20:09:51 +0200

    added navigation

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <header className="header">
       <h1 className="header__title">Site Name</h1>
-     <Navigation />
+      <Navigation />
     </header>
   );
 };
