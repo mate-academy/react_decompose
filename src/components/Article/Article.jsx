ba8d7b4c5c7df658725694b3458c66902b921883
valerij <valerijkolpacov@gmail.com>
2026-04-09 17:59:52 +0300

    Solution09_04

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,5 +1,37 @@
-// import a css file containig article styles
+import React from 'react';
+import './Article.css';
 
-// Create an Article function returning the HTML of article block
+const Article = () => (
+  <article className="article">
+    <h1 className="article__title">Headline</h1>
+    <p className="article__paragraph">
+      In elementum lorem eget est euismod ornare. Phasellus sit amet
+      pellentesque mauris. Aliquam quis malesuada ex. Nullam eu aliquam nibh.
+      nibh. molestie, urna accumsan ornare semper, augue nibh posuere lorem
+      vitae f eugiat sem magna eget massa.
+    </p>
+    <p className="article__paragraph">
+      In ac nisi lacus. Fusce est dolor, tincidunt ut bibendum vitae, ac quam. A
+      liquam pretium tristique nibh quis iaculis. In et cursus ex aliquet ex. P
+      r oin facilisis lacus sit amet sapien ultrices, ut arcu lobortis.
+    </p>
+    <p className="article__paragraph">
+      Donec arcu elit, euismod vel lobortis eu, fringilla sit amet dolor. Cras
+      Cras e, massa nec sagittis mollis, dui felis ultrices magna, tinc finibus
+      l o rem quam in sem. Morbi odio turpis, pulvinar sit amet vulp quis,
+      ultricie s eu libero.
+    </p>
+    <p className="article__paragraph">
+      Cras egestas tempor nibh, a fermentum lorem sollicitudin non. Nulla
+      facilisi. In at elit id leo tristique condimentum. Donec at est nulla.
+      Mauris egestas magna ut laoreet pretium.
+    </p>
+    <p className="article__paragraph">
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+      fames rpis egestas. Phasellus bibendum nec arcu eu lobortis. Nam conva
+      faucibus ante sed porta. Nullam ut convallis elit.
+    </p>
+  </article>
+);
 
-// Add a default export statement for Article component to use it in the other files
+export default Article;
