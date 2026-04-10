e39c5e01a1c78f3d638a008c6bf3a73bc39c053d
yevkrasnenkov <krasnenkov.evgen@gmail.com>
2026-04-09 22:24:31 -0400

    no nav in app

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import './App.css';
 import Article from './components/Article/Article';
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
-import Navigation from './components/Navigation/Navigation';
 
 function App() {
   return (
     <main className="app">
-      <Navigation />
       <Welcome />
       <Header />
       <Article />
