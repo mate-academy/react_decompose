c8a120a5b05140e981f2e4d38f8ff3c687401be5
Maksym <maxim.golya@gmail.com>
2026-05-06 22:15:32 +0300

    solution

--- src/components/Article/Article.jsx
+++ src/components/Article/Article.jsx
@@ -1,9 +1,5 @@
 /* eslint-disable prettier/prettier */
-// import a css file containig article styles
 
-// Create an Article function returning the HTML of article block
-
-// Add a default export statement for Article component to use it in the other files
 import './Article.css';
 
 function Article() {
