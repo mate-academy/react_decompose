5531603092ad9b0b6aa13a312343053e7dbe8cc4
PavloMolytovnyk <pavlomolytovny@icloud.com>
2026-05-07 21:29:40 +0300

    Change1

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 import './App.css';
-import './components/Welcome/Welcome.css'
-import './components/Header/Header.css'
-import './components/Article/Article.css'
+import './components/Welcome/Welcome.css';
+import './components/Header/Header.css';
+import './components/Article/Article.css';
 
-import  Welcome from './components/Welcome/Welcome';
-import  Header from './components/Header/Header';
-import  Article from './components/Article/Article';
+import Welcome from './components/Welcome/Welcome';
+import Header from './components/Header/Header';
+import Article from './components/Article/Article';
 
 function App() {
   return (
