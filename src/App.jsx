88e12aeae8b61a70966c7c92cb5d117656864338
Natalia Lunkevych <gadyka08@gmail.com>
2026-05-07 18:54:45 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
+// Імпортуємо компоненти (використовуємо default import, як зазначено у твоїх файлах)
 import Welcome from './components/Welcome/Welcome';
 import Header from './components/Header/Header';
 import Article from './components/Article/Article';
-import Navigation from './components/Navigation/Navigation';
-// Move each BEM block to a separate component (file) and import them here
 
+// Головний компонент додатка
 function App() {
   return (
     <main className="app">
-      <Welcome />
+      {/* 1. Заголовок (Navigation вже знаходиться всередині Header.jsx) */}
       <Header />
-      <Navigation />
+
+      {/* 2. Блок привітання */}
+      <Welcome />
+
+      {/* 3. Основна стаття */}
       <Article />
     </main>
   );
