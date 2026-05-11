51cd3f0a5f452d37231da163c493c44018654b60
Sergey Ivanov <ivanovsereza26@gmail.com>
2026-05-11 08:29:56 +0300

    add task solution

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,17 @@
+import Navigation from '../Navigation/Navigation';
+import './Header.css';
+
+function Header() {
+  return (
+    <header className="header">
+      <h1 className="header__title">Site Name</h1>
+      <Navigation />
+    </header>
+  );
+}
+
+export default Header;
+
 // import a css file containig header styles
 
 // Create a Header function returning the HTML of header block
